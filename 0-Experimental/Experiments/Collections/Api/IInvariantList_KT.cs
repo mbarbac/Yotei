8a3d022053cf5999e65be89ea40ee86610b04a0d
Moises Barba <mbarbac@hotmail.com>
2024-01-06 18:09:33 +0100

    xxx

--- 0-Experimental/Experiments/Collections/Api/IInvariantList_KT.cs
+++ 0-Experimental/Experiments/Collections/Api/IInvariantList_KT.cs
@@ -1,4 +1,4 @@
-namespace Experiments;
+namespace Experiments.Collections;
 
 // ========================================================
 /// <summary>
--- 0-Experimental/Experiments/Collections/Tests/Test_Template.cs
+++ 0-Experimental/Experiments/Collections/Tests/Test_Template.cs
@@ -1,86 +1,86 @@
-namespace Experiments;
+namespace Experiments.Collections;
 
 // ========================================================
-////[Enforced]
+//[Enforced]
 public static partial class Test_Template
 {
     //[Enforced]
-    [Fact]
-    public static void Test_Create_Empty() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Create_Empty() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Create_Single() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Create_Single() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Create_Multiple() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Create_Multiple() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Create_Multiple_With_Duplicates() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Create_Multiple_With_Duplicates() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Change_Settings() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Change_Settings() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Contains() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Contains() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Find_Key() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Find_Key() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Find_Predicate() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Find_Predicate() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Clone() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Clone() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_GetRange() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_GetRange() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Replace() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Replace() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Add() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Add() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_AddRange() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_AddRange() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Insert() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Insert() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_InsertRange() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_InsertRange() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_RemoveAt() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_RemoveAt() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_RemoveRange() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_RemoveRange() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Remove_Item() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Remove_Item() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Remove_Predicate() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Remove_Predicate() ** TODO
 
     //[Enforced]
-    [Fact]
-    public static void Test_Clear() { /*TODO*/ }
+    //[Fact]
+    //public static void Test_Clear() ** TODO
 }
