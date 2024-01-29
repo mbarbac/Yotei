e4b3493b3a0631219d21f23de30db61bf538d1b1
Moises Barba <mbarbac@hotmail.com>
2024-01-29 10:21:31 +0100

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/1.Common/Code/CommandInfoBuider.cs
@@ -0,0 +1,23 @@
+﻿namespace Yotei.ORM.Code;
+
+// ========================================================
+/// <summary>
+/// Represents a builder for the information needed to execute a database command.
+/// </summary>
+public class CommandInfoBuilder
+{
+    StringBuilder _Text;
+    ParameterListBuilder _Parameters;
+
+    /// <summary>
+    /// The engine this instance is associated with.
+    /// </summary>
+    public IEngine Engine => _Parameters.Engine;
+
+    /// <summary>
+    /// The text of the command.
+    /// </summary>
+    public string Text => _Text.ToString();
+
+    public IParameterList Parameters => null;
+}
