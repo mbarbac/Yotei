a10b13ebd2ad83a512f119ecc7182bf0bce2e141
Moises Barba <mbarbac@hotmail.com>
2024-02-09 20:23:56 +0100

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Api/IMetadataEntry.cs
@@ -0,0 +1,18 @@
+﻿namespace Yotei.ORM.Records;
+
+// ========================================================
+/// <summary>
+/// Represents a metadata entry.
+/// </summary>
+public partial interface IMetadataEntry
+{
+    /// <summary>
+    /// The collection of tag names by which this instance is known.
+    /// </summary>
+    [WithGenerator] IMetadataTag Tag { get; }
+
+    /// <summary>
+    /// The value carried by this instance.
+    /// </summary>
+    [WithGenerator] object? Value { get; }
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Code/MetadataEntry.cs
@@ -0,0 +1,42 @@
+﻿namespace Yotei.ORM.Records.Code;
+
+// ========================================================
+/// <inheritdoc cref="IMetadataEntry"/>
+[WithGenerator]
+public sealed partial class MetadataEntry : IMetadataEntry
+{
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <param name="value"></param>
+    public MetadataEntry(IMetadataTag tag, object? value)
+    {
+        Tag = tag;
+        Value = value;
+    }
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="source"></param>
+    MetadataEntry(MetadataEntry source)
+    {
+        Tag = source.Tag;
+        Value = source.Value;
+    }
+
+    /// <inheritdoc/>
+    public override string ToString() => $"{Tag.DefaultName} = '{Value.Sketch()}'";
+
+    /// <inheritdoc/>
+    public IMetadataTag Tag
+    {
+        get => _Tag;
+        init => _Tag = value.ThrowWhenNull();
+    }
+    IMetadataTag _Tag = default!;
+
+    /// <inheritdoc/>
+    public object? Value { get; init; }
+}
