82447e23ed5ca292e6175416331ac5a18b65494e
Moises Barba <mbarbac@hotmail.com>
2024-03-01 19:49:13 +0100

    xxx

---
+++ 0-Experimental/Experiments/Utils/__.cs
@@ -0,0 +1,12 @@
+namespace Experiments.Utils;
+
+// ========================================================
+//[Enforced]
+//public static class Test_
+//{
+//    //[Enforced]
+//    [Fact]
+//    public static void Test_()
+//    {
+//    }
+//}
