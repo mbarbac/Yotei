28fa7b47e91ccaf6f522114719a3734c90ce2207
Moises Barba <mbarbac@hotmail.com>
2024-07-08 19:34:51 +0200

    xxx

---
+++ 1-Yotei.Tools/Yotei.Tools.Tests/Tests_SystemEx/Test_Sketch_Extensions.cs
@@ -0,0 +1,445 @@
+namespace Yotei.Tools.Tests;
+
+// ========================================================
+//[Enforced]
+public static class Test_Sketch_Extensions
+{
+    const string NAMESPACE = "Yotei.Tools.Tests";
+    const string CLASSNAME = nameof(Test_Sketch_Extensions);
+
+    // -----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Null_Object()
+    {
+        object? source = null;
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("", name);
+
+        options = options with { NullStr = "NULL" };
+        name = source.Sketch(options); Assert.Equal("NULL", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Object) NULL", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.Object) NULL", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("NULL", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Null_String()
+    {
+        string? source = null;
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("", name);
+
+        options = options with { NullStr = "NULL" };
+        name = source.Sketch(options); Assert.Equal("NULL", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(String) NULL", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.String) NULL", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("NULL", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Null_Others()
+    {
+        int? source = null;
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("", name);
+
+        options = options with { NullStr = "NULL" };
+        name = source.Sketch(options); Assert.Equal("NULL", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Nullable) NULL", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeArguments = true } };
+        name = source.Sketch(options); Assert.Equal("(Nullable<Int32>) NULL", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.Nullable<System.Int32>) NULL", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("NULL", name);
+    }
+
+    // -----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Regular_String()
+    {
+        var source = "Hello";
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("Hello", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(String) Hello", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.String) Hello", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("Hello", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Regular_Guid()
+    {
+        var source = new Guid(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("00000001-0002-0003-0405-060708090a0b", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Guid) 00000001-0002-0003-0405-060708090a0b", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.Guid) 00000001-0002-0003-0405-060708090a0b", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("00000001-0002-0003-0405-060708090a0b", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Regular_Int()
+    {
+        var source = 5;
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("5", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Int32) 5", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.Int32) 5", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("5", name);
+    }
+
+    // -----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Double()
+    {
+        var source = 1234.567;
+
+        var options = SketchOptions.False with { Provider = CultureInfo.InvariantCulture };
+        var name = source.Sketch(options); Assert.Equal("1234.567", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Double) 1234.567", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.Double) 1234.567", name);
+
+        // Default...
+        options = SketchOptions.Default with { Provider = CultureInfo.InvariantCulture };
+        name = source.Sketch(options); Assert.Equal("1234.567", name);
+
+        options = options with { Provider = CultureInfo.GetCultureInfo("es-ES") };
+        name = source.Sketch(options); Assert.Equal("1234,567", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Decimal()
+    {
+        var source = new decimal(7.5);
+
+        var options = SketchOptions.False with { Provider = CultureInfo.InvariantCulture };
+        var name = source.Sketch(options); Assert.Equal("7.5", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Decimal) 7.5", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.Decimal) 7.5", name);
+
+        // Default...
+        options = SketchOptions.Default with { Provider = CultureInfo.InvariantCulture };
+        name = source.Sketch(options); Assert.Equal("7.5", name);
+
+        options = options with { Provider = CultureInfo.GetCultureInfo("es-ES") };
+        name = source.Sketch(options); Assert.Equal("7,5", name);
+    }
+
+    // ----------------------------------------------------
+
+    [Flags] public enum MyEnum { One = 1, Two = 2, Three = 4 }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Enum_Simple()
+    {
+        var source = MyEnum.One;
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("One", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("MyEnum.One", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeHost = true } };
+        name = source.Sketch(options); Assert.Equal($"{CLASSNAME}.MyEnum.One", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("One", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Enum_Flags()
+    {
+        var source = MyEnum.One | MyEnum.Two | MyEnum.Three;
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("One | Two | Three", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("MyEnum.One | MyEnum.Two | MyEnum.Three", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeHost = true } };
+        name = source.Sketch(options); Assert.Equal($"{CLASSNAME}.MyEnum.One | {CLASSNAME}.MyEnum.Two | {CLASSNAME}.MyEnum.Three", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("One | Two | Three", name);
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Array_Empty()
+    {
+        var source = Array.Empty<char>();
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("[]", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Char[]) []", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.Char[]) []", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("[]", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Array_Populated()
+    {
+        var source = new int[] { 1, 2, 3 };
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("[1, 2, 3]", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Int32[]) [1, 2, 3]", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options); Assert.Equal("(System.Int32[]) [1, 2, 3]", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("[1, 2, 3]", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Dictionary()
+    {
+        var source = new Dictionary<string, int>() { { "James", 50 }, { "Maria", 25 } };
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("{James = 50, Maria = 25}", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(Dictionary) {James = 50, Maria = 25}", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeArguments = true } };
+        name = source.Sketch(options); Assert.Equal("(Dictionary<String, Int32>) {James = 50, Maria = 25}", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options);
+        Assert.Equal("(System.Collections.Generic.Dictionary<System.String, System.Int32>) {James = 50, Maria = 25}", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("{James = 50, Maria = 25}", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Other_Collections()
+    {
+        var source = new List<string>() { "James", "Maria" };
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("[James, Maria]", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(List) [James, Maria]", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeArguments = true } };
+        name = source.Sketch(options); Assert.Equal("(List<String>) [James, Maria]", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeNamespace = true } };
+        name = source.Sketch(options);
+        Assert.Equal("(System.Collections.Generic.List<System.String>) [James, Maria]", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("[James, Maria]", name);
+    }
+
+    // ----------------------------------------------------
+
+    public class A
+    {
+        public string Name = "James";
+        readonly int Age = 50;
+        readonly static string Org = "MI6";
+        protected string GetString() => $"({Name}, {Age}, {Org})";
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Shape()
+    {
+        var source = new A();
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("", name);
+
+        options = options with { UseShape = true };
+        name = source.Sketch(options); Assert.Equal("{ Name = James }", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(A) { Name = James }", name);
+
+        options = options with { UsePrivateMembers = true };
+        name = source.Sketch(options); Assert.Equal("(A) { Name = James, Age = 50 }", name);
+
+        options = options with { UseStaticMembers = true };
+        name = source.Sketch(options); Assert.Equal("(A) { Name = James, Age = 50, Org = MI6 }", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("{ Name = James }", name);
+    }
+
+    // ----------------------------------------------------
+
+    public class B : A { public override string ToString() => $"B: {GetString()}"; }
+    public class C : B { }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Overriden_ToString()
+    {
+        var source = new B();
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("B: (James, 50, MI6)", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(B) B: (James, 50, MI6)", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("B: (James, 50, MI6)", name);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Overriden_Base_ToString()
+    {
+        var source = new C();
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("B: (James, 50, MI6)", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(C) B: (James, 50, MI6)", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("B: (James, 50, MI6)", name);
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Dynamics()
+    {
+        var source = new ExpandoObject(); // dynamics do not bind against extension methods!
+        dynamic obj = source;
+        obj.Name = "James";
+        obj.Age = 50;
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("{ [Name, James], [Age, 50] }", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options); Assert.Equal("(ExpandoObject) { [Name, James], [Age, 50] }", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("{ [Name, James], [Age, 50] }", name);
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Anonymous()
+    {
+        var source = new { Name = "James", Age = 50 };
+
+        var options = SketchOptions.False;
+        var name = source.Sketch(options); Assert.Equal("{ Name = James, Age = 50 }", name);
+
+        options = options with { EasyOptions = options.EasyOptions with { UseTypeName = true } };
+        name = source.Sketch(options);
+        Assert.EndsWith("{ Name = James, Age = 50 }", name);
+        Assert.Contains("AnonymousType", name);
+
+        // Default...
+        options = SketchOptions.Default;
+        name = source.Sketch(options); Assert.Equal("{ Name = James, Age = 50 }", name);
+    }
+}
---
+++ 1-Yotei.Tools/Yotei.Tools/SystemEx/SketchOptions.cs
@@ -0,0 +1,104 @@
+﻿namespace Yotei.Tools;
+
+// ========================================================
+/// <summary>
+/// Provides options for the <see cref="SketchExtensions"/> methods.
+/// </summary>
+public record SketchOptions
+{
+    /// <summary>
+    /// The options to use with type or reflection elements.
+    /// </summary>
+    public EasyNameOptions EasyOptions
+    {
+        get => _EasyOptions;
+        init => _EasyOptions = value.ThrowWhenNull();
+    }
+    EasyNameOptions _EasyOptions = EasyNameOptions.Default;
+
+    /// <summary>
+    /// The string used to represent null values.
+    /// </summary>
+    public string NullStr
+    {
+        get => _NullStr;
+        init => _NullStr = value.ThrowWhenNull();
+    }
+    string _NullStr = default!;
+
+    /// <summary>
+    /// If not null, the format specification to use.
+    /// </summary>
+    public string? Format { get; init; }
+
+    /// <summary>
+    /// If not null, the format provider to use.
+    /// </summary>
+    public IFormatProvider? Provider { get; init; }
+
+    /// <summary>
+    /// If true uses the source's shape, if such would be needed.
+    /// </summary>
+    public bool UseShape { get; init; }
+
+    /// <summary>
+    /// If true enforces including private members for objects' shapes.
+    /// </summary>
+    public bool UsePrivateMembers { get; init; }
+
+    /// <summary>
+    /// If true enforces including static members for objects' shapes.
+    /// </summary>
+    public bool UseStaticMembers { get; init; }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Initializes a new default instance with all its properties set to <c>false</c> or
+    /// <c>null</c>, except <see cref="NullStr"/> with a value of 'NULL'.
+    /// </summary>
+    public SketchOptions()
+    {
+        EasyOptions = EasyNameOptions.False;
+        NullStr = "NULL";
+        Format = null;
+        Provider = null;
+        UseShape = true;
+        UsePrivateMembers = false;
+        UseStaticMembers = false;
+    }
+
+    /// <summary>
+    /// A shared default instance.
+    /// </summary>
+    public static SketchOptions Default { get; } = new();
+
+    /// <summary>
+    /// A shared instance with its options set to <c>false</c> or <c>null</c>.
+    /// </summary>
+    public static SketchOptions False { get; } = new()
+    {
+        EasyOptions = EasyNameOptions.False,
+        NullStr = "",
+        Format = null,
+        Provider = null,
+        UseShape = false,
+        UsePrivateMembers = false,
+        UseStaticMembers = false,
+    };
+
+    /// <summary>
+    /// A shared instance with its options set to <c>true</c>, except the <see cref="Format"/>
+    /// and <see cref="Provider"/> ones, and <see cref="NullStr"/> with a value of 'NULL'.
+    /// </summary>
+    public static SketchOptions True { get; } = new()
+    {
+        EasyOptions = EasyNameOptions.True,
+        NullStr = "NULL",
+        Format = null,
+        Provider = null,
+        UseShape = true,
+        UsePrivateMembers = true,
+        UseStaticMembers = true,
+    };
+}
