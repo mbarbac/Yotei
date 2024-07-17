8e050f0b53f6fe704f1a19a0846d50bfc1d7128a
Moises Barba <mbarbac@hotmail.com>
2024-07-17 19:52:47 +0200

    xxx

---
+++ 0-Experimental/Experimental/Language/Test_RuntimeCompiler.cs
@@ -0,0 +1,27 @@
+﻿//using Microsoft.CSharp;
+//using System.CodeDom.Compiler;
+
+using System.CodeDom.Compiler;
+using Microsoft.CodeDom.Providers.DotNetCompilerPlatform;
+
+namespace Experimental.Language;
+
+// =========================================================
+//[Enforced]
+public static class Test_RuntimeCompiler
+{
+    /*
+    //[Enforced]
+    [Fact]
+    public static void Test_()
+    {
+        var provider = new CSharpCodeProvider();
+        var parameters = new CompilerParameters { GenerateInMemory = true };
+        var code = "public class RuntimeCode { public int Add(int a, int b) { return a + b; } }";
+        var results = provider.CompileAssemblyFromSource(parameters, code);
+        var instance = results.CompiledAssembly.CreateInstance("RuntimeCode");
+        var method = instance!.GetType().GetMethod("Add");
+        var result = (int)method!.Invoke(instance, [1, 2])!;
+        Assert.Equal(3, result);
+    }*/
+}
