7a1bd0f2ed223b60dcfddbdf84df29f0a6c6d387
Moises Barba <mbarbac@hotmail.com>
2024-08-15 19:54:37 +0200

    xxx

--- 2-Yotei.ORM/Yotei.ORM/Code/Identifier.cs
+++ 2-Yotei.ORM/Yotei.ORM/Code/Identifier.cs
@@ -1,4 +1,6 @@
-﻿namespace Yotei.ORM.Code;
+﻿using Yotei.ORM.Internal;
+
+namespace Yotei.ORM.Code;
 
 // ========================================================
 public static class Identifier
@@ -12,14 +14,16 @@ public static class Identifier
     /// <param name="engine"></param>
     /// <param name="value"></param>
     /// <param name="reduce"></param>
+    /// <param name="trim"></param>
     /// <returns></returns>
-    public static List<string?> GetParts(IEngine engine, string? value, bool reduce = true)
+    public static List<string?> GetParts(
+        IEngine engine, string? value, bool reduce = true, bool trim = true)
     {
         engine.ThrowWhenNull();
 
         var parts = engine.UseTerminators
-            ? GetPartsWithTerminators(engine, value)
-            : GetPartsNoTerminators(value);
+            ? GetPartsWithTerminators(engine, value, trim)
+            : GetPartsNoTerminators(value, trim);
 
         if (reduce)
         {
@@ -33,18 +37,91 @@ public static class Identifier
         return parts;
     }
 
+    // -----------------------------------------------------
+
     /// <summary>
     /// Invoked when the engine uses no terminators.
     /// </summary>
-    static List<string?> GetPartsNoTerminators(string? value)
+    static List<string?> GetPartsNoTerminators(string? value, bool trim)
     {
-        throw null;
+        value = value.NullWhenEmpty(trim);
+        if (value is null) return [];
+
+        if (value.Contains('.'))
+        {
+            var items = value.Split('.');
+            var parts = items.Select(x => ValidateNoTerminators(x, trim)).ToList();
+            return parts;
+        }
+        else
+        {
+            var parts = new List<string?>(1) { ValidateNoTerminators(value, trim) };
+            return parts;
+        }
     }
+
+    /// <summary>
+    /// Invoked to validate a part when no terminators are used.
+    /// </summary>
+    static string? ValidateNoTerminators(string? value, bool trim)
+    {
+        value = value.NullWhenEmpty(trim);
+
+        if (value is not null)
+        {
+            if (value.Contains(' ')) throw new ArgumentException(
+            "Identifier parts cannot contain spaces.").WithData(value);
+
+            if (value.Contains('.')) throw new ArgumentException(
+                "Identifier parts cannot contain dots.").WithData(value);
+        }
+        return value;
+    }
+
+    // -----------------------------------------------------
 
     /// <summary>
     /// Invoked when the engine uses terminators.
     /// </summary>
-    static List<string?> GetPartsWithTerminators(IEngine engine, string? value)
+    static List<string?> GetPartsWithTerminators(IEngine engine, string? value, bool trim)
+    {
+        // Trivial case...
+        value = value.NullWhenEmpty(trim);
+        if (value is null) return [];
+
+        // Tokenizing...
+        var tokenizer = new StrWrappedTokenizer(engine.LeftTerminator, engine.RightTerminator);
+        var token = tokenizer.Tokenize(value);
+
+        // No parts...
+        if (token is IStrTokenText text)
+        {
+            var parts = GetPartsNoTerminators(text.Payload, trim);
+            return parts;
+        }
+
+        // Single part...
+        if (token is IStrTokenWrapped wrapped)
+        {
+            var part = wrapped.Payload.ToString();
+            var parts = new List<string?>(1) { ValidateWithTerminators(engine, part, trim) };
+            return parts;
+        }
+
+        // Chained parts...
+        if (token is IStrTokenChain chain)
+        {
+            throw null;
+        }
+
+        // Unknown token...
+        throw new ArgumentException("Unknown token found.").WithData(value);
+    }
+
+    /// <summary>
+    /// Invoked to validate a part when terminators are used.
+    /// </summary>
+    static string? ValidateWithTerminators(IEngine engine, string? value, bool trim)
     {
         throw null;
     }
