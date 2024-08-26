4cb58a36008ecfdc6686224c56db6689d0219e74
Moises Barba <mbarbac@hotmail.com>
2024-08-26 18:43:23 +0200

    xxx

---
+++ 3-Yotei.ORM.Engines/Yotei.ORM.Relational.FakeDB/DTOs/RegionDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Yotei.ORM.Relational.FakeDB;
+
+// ========================================================
+public class RegionDTO
+{
+    public string? Id { get; set; } = null;
+    public string? Name { get; set; } = null;
+    public string? ParentId { get; set; } = null;
+    public object? RowVersion { get; set; } = null;
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"Id:{Id ?? "-"}");
+        if (Name != null) sb.Append($", Name:{Name}");
+        if (ParentId != null) sb.Append($", ParentId:{ParentId}");
+        if (RowVersion != null) sb.Append($", RowVersion:{RowVersion.Sketch()}");
+        return sb.ToString();
+    }
+}
