e0cf9c36c8366e8267226032d24c73ff24428de2
Moises Barba <mbarbac@hotmail.com>
2024-11-13 20:19:06 +0100

    xxx

---
+++ 1-Yotei.Tools/Yotei.Tools/Exceptions/NotFoundException.cs
@@ -0,0 +1,26 @@
+﻿namespace Yotei.Tools;
+
+// ========================================================
+/// <summary>
+/// Represents a failed attempt of finding an object.
+/// </summary>
+public class NotFoundException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    public NotFoundException() { }
+
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    /// <param name="message"></param>
+    public NotFoundException(string message) : base(message) { }
+
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="inner"></param>
+    public NotFoundException(string message, Exception inner) : base(message, inner) { }
+}
