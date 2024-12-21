203824ad60b40c8dfd8ef8c30fb90aebf940763d
Moises Barba <mbarbac@hotmail.com>
2024-12-21 19:21:50 +0100

    xxx

---
+++ 1-Yotei.Tools.LambdaParser/Yotei.Tools.LambdaParser/Nodes/LambdaNodeIndexed.cs
@@ -0,0 +1,36 @@
+﻿namespace Yotei.Tools;
+
+// ========================================================
+/// <summary>
+/// Represents a dynamic get indexed operation.
+/// </summary>
+[DebuggerDisplay("{ToDebugString()}")]
+public class LambdaNodeIndexed : LambdaNodeHosted
+{
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    /// <param name="host"></param>
+    /// <param name="indexes"></param>
+    public LambdaNodeIndexed(LambdaNode host, IEnumerable<LambdaNode> indexes) : base(host)
+    {
+        LambdaIndexes = ValidateLambdaArguments(indexes, canBeEmpty: false);
+        LambdaParser.Print($"- New: {ToDebugString()}");
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+         => $"{LambdaHost}[{string.Join(", ", LambdaIndexes.Select(x => x.ToString()))}]";
+
+    /// <inheritdoc/>
+    public override LambdaNodeIndexed Clone() => new(
+        LambdaHost.Clone(),
+        LambdaIndexes.Select(x => x.Clone()).ToImmutableList());
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// The not null and not empty collection of indexes used for the indexed get operation.
+    /// </summary>
+    public IImmutableList<LambdaNode> LambdaIndexes { get; }
+}
