d4221527f5e3d0ea00fd929889dee7f37266d023
Moises Barba <mbarbac@hotmail.com>
2024-12-29 18:07:15 +0100

    xxx

--- 0-Development/Runner/Program.cs
+++ 0-Development/Runner/Program.cs
@@ -1,8 +1,5 @@
 ﻿using static Yotei.Tools.Diagnostics.ConsoleEx;
 using static System.ConsoleColor;
-using Runner.Tester;
-using Runner.Artifacts;
-using Runner.Builder;
 
 namespace Runner;
 
@@ -12,20 +9,6 @@ namespace Runner;
 /// </summary>
 internal class Program
 {
-    public static readonly string FatSeparator = "****************************************";
-    public static readonly string SlimSeparator = "-------------------------";
-    public static TimeSpan Timeout = TimeSpan.FromMinutes(2);
-
-    public static RequestList Includes = [];
-    public static RequestList Excludes = [];
-
-    public static string NuGetRepoSource = @"https://api.nuget.org/v3/index.json";
-    public static string LocalRepoSource = "Local";
-    public static string LocalRepoPath = @"C:\Dev\Packages";
-
-    // Note that "Local" is a label that through configuration maps to a concrete location or
-    // path on disk. The later is used when deleting artifacts, the former when building nuget
-    // packages.
 
     // ----------------------------------------------------
 
@@ -42,80 +25,6 @@ internal class Program
         // Explicit includes and excludes, in order...
         //Includes.Add(new("Yotei.Tools.LambdaParser.Tests", "Test_NameParser", null));
 
-        // Main menu...
-        var done = -1; do
-        {
-            WriteLine(true);
-            WriteLine(true, Green, FatSeparator);
-            WriteLine(true, Green, "Main Menu:");
-            WriteLine(true);
-
-            done = Menu.Run(
-                Green, Timeout,
-                new MenuEntry("Exit"),
-                new MenuTester(breakOnError: true),
-                new MenuArtifacts(),
-                new MenuBuilder());
-        }
-        while (done > 0);
-    }
-
-    // ----------------------------------------------------
-
-    /// <summary>
-    /// Returns the path to the solution's directory.
-    /// </summary>
-    /// <returns></returns>
-    static internal string GetSolutionDirectory()
-    {
-        var path = AppContext.BaseDirectory;
-        var dir = new DirectoryInfo(path);
-
-        while (true)
-        {
-            var files = dir.GetFiles("*.sln");
-            if (files.Length > 0) return dir.FullName;
-
-            dir = dir.Parent!;
-            if (dir == null) Environment.FailFast("Cannot find solution's directory.");
-        }
-    }
-
-    // ----------------------------------------------------
-
-    /// <summary>
-    /// Edits the given directory path, returning either a valid one, or an empty one if allowed,
-    /// or null in case of cancellation of pressing the [Escape] key.
-    /// </summary>
-    /// <param name="path"></param>
-    /// <param name="description"></param>
-    /// <param name="okEmpty"></param>
-    /// <returns></returns>
-    static internal string? EditDirectory(string path, string? description = null, bool okEmpty = false)
-    {
-        path ??= string.Empty;
-        description ??= "Directory";
-
-        while (true)
-        {
-            Write(true, Green, $"{description}: ");
-
-            var valid = EditLine(Timeout, path, out path!);
-            if (!valid) return null;
-
-            if (path.Length == 0 && okEmpty) return string.Empty;
-            try
-            {
-                var dir = new DirectoryInfo(path);
-                if (dir.Exists) return dir.FullName;
-
-                Console.CursorTop -= 1;
-                Console.CursorLeft = 0;
-                Write(Green, $"{description}: ");
-                WriteLine(Red, "<Invalid>");
-            }
-            catch (ArgumentException) { }
-            catch (FileNotFoundException) { }
-        }
+        ReadLine();
     }
 }
