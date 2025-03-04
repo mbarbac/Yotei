f290c3d863e49c875b4dabbbeedad818157a877f
Moises Barba <mbarbac@hotmail.com>
2025-03-04 19:23:36 +0100

    xxx

---
+++ 1-Yotei.Tools.Generators/Yotei.Tools.BaseGenerator/Base/Nodes/NodeExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Yotei.Tools.BaseGenerator;
+
+// ========================================================
+internal static class NodeExtensions
+{
+    /// <summary>
+    /// Gets the candidate associated with the given node, if any.
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IValidCandidate? GetCandidate(this INode node) => node.ThrowWhenNull() switch
+    {
+        TypeNode item => item.Candidate,
+        PropertyNode item => item.Candidate,
+        FieldNode item => item.Candidate,
+        MethodNode item => item.Candidate,
+
+        _ => null
+    };
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Returns the list of attributes captured for the given node, through its associated
+    /// candidate, if any, or returns an empty list otherwise. Attributes are only those that
+    /// have been specified by the tree generator, not any other ones.
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static List<AttributeData> GetCapturedAttributes(
+        this INode node)
+        => node.GetCandidate()?.Attributes.ToList() ?? [];
+}
---
+++ 1-Yotei.Tools.Generators/Yotei.Tools.BaseGenerator/Roslyn/AccessibilityExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Yotei.Tools.BaseGenerator;
+
+// ========================================================
+internal static class AccessibilityExtensions
+{
+    /// <summary>
+    /// Returns the C# string representation of the specified accessibility. May return null
+    /// if no accessibility is applicable.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="addspace"></param>
+    /// <returns></returns>
+    public static string? ToCSharpString(this Accessibility value, bool addspace = false)
+    {
+        var item = value switch
+        {
+            Accessibility.Private => "private", // 1
+            Accessibility.ProtectedAndInternal => "protected", // 2
+            Accessibility.Protected => "protected", // 3
+            Accessibility.ProtectedOrInternal => "protected", // 5
+            Accessibility.Public => "public", // 6
+
+            // 0: NotApplicable => null
+            // 2: ProtectedAndFriend => "protected"
+            // 4: Friend => null
+            // 4: Internal => null
+            // 5: ProtectedOrFriend => "protected"
+
+            _ => null
+        };
+
+        if (item != null && addspace) item += ' ';
+        return item;
+    }
+}
