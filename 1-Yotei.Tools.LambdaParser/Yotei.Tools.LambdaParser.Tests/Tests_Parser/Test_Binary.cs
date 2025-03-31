af0565bd63901860a9baa7ccfd59f2ff0730bbc1
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-03-31 19:04:27 +0200

    xxx

--- 1-Yotei.Tools.LambdaParser/Yotei.Tools.LambdaParser.Tests/Tests_Parser/Test_Binary.cs
+++ 1-Yotei.Tools.LambdaParser/Yotei.Tools.LambdaParser.Tests/Tests_Parser/Test_Binary.cs
@@ -195,7 +195,7 @@ public static class Test_Binary
         Assert.Equal("(x.Alpha And x)", node.ToString());
     });
 
-    [Enforced]
+    //[Enforced]
     [Fact]
     public static void Parse_SingleAnd_Dynamic_Dynamic() => Repeater.Repeat(() =>
     {
@@ -209,5 +209,103 @@ public static class Test_Binary
         WriteLine($"> Result: {node}");
         item = Assert.IsType<LambdaNodeBinary>(node);
         Assert.Equal("(x.Alpha And x.Beta)", node.ToString());
+    });
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_RegularAnd_Argument_Constant() => Repeater.Repeat(() =>
+    {
+        Func<dynamic, object> func;
+        LambdaNode node;
+        LambdaNodeBinary item;
+
+        WriteLine();
+        func = x => x && 7;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeBinary>(node);
+        Assert.Equal("(x And '7')", node.ToString()); // Not AndAlso :(
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_RegularAnd_Argument_Argument() => Repeater.Repeat(() =>
+    {
+        Func<dynamic, object> func;
+        LambdaNode node;
+        LambdaNodeBinary item;
+
+        WriteLine();
+        func = x => x && x;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeBinary>(node);
+        Assert.Equal("(x And x)", node.ToString()); // Not AndAlso :(
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_RegularAnd_Argument_Dynamic() => Repeater.Repeat(() =>
+    {
+        Func<dynamic, object> func;
+        LambdaNode node;
+        LambdaNodeBinary item;
+
+        WriteLine();
+        func = x => x && x.Alpha;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeBinary>(node);
+        Assert.Equal("(x And x.Alpha)", node.ToString());
+
+        WriteLine();
+        func = x => x.Alpha & x;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeBinary>(node);
+        Assert.Equal("(x.Alpha And x)", node.ToString());
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_RegularAnd_Dynamic_Dynamic() => Repeater.Repeat(() =>
+    {
+        Func<dynamic, object> func;
+        LambdaNode node;
+        LambdaNodeBinary item;
+
+        WriteLine();
+        func = x => x.Alpha && x.Beta;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeBinary>(node);
+        Assert.Equal("(x.Alpha And x.Beta)", node.ToString());
+
+        WriteLine();
+        func = x => x.Delta.Alpha && x.Beta.Delta;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeBinary>(node);
+        Assert.Equal("(x.Delta.Alpha And x.Beta.Delta)", node.ToString());
+    });
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_Binary_AddAssign_Resolves_To_Setter() => Repeater.Repeat(() =>
+    {
+        Func<dynamic, object> func;
+        LambdaNode node;
+        LambdaNodeSetter item;
+
+        WriteLine();
+        func = x => x.Alpha += x.Beta;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeSetter>(node);
+        Assert.Equal("(x.Alpha = (x.Alpha AddAssign x.Beta))", node.ToString());
     });
 }
--- 1-Yotei.Tools.LambdaParser/Yotei.Tools.LambdaParser.Tests/Tests_Parser/Test_Constant.cs
+++ 1-Yotei.Tools.LambdaParser/Yotei.Tools.LambdaParser.Tests/Tests_Parser/Test_Constant.cs
@@ -45,4 +45,52 @@ public static class Test_Constant
         item = Assert.IsType<LambdaNodeValue>(node);
         Assert.Equal("'any'", node.ToString());
     });
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_With_Cast_Constant() => Repeater.Repeat(() =>
+    {
+        Func<dynamic, object> func;
+        LambdaNode node;
+        LambdaNodeValue item;
+
+        WriteLine();
+        func = x => (string?)null!;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeValue>(node);
+        Assert.Equal("'NULL'", node.ToString());
+
+        WriteLine();
+        func = x => (int)10.5!;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeValue>(node);
+        Assert.Equal("'10'", node.ToString());
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_With_Cast_Dynamic() => Repeater.Repeat(() =>
+    {
+        Func<dynamic, object> func;
+        LambdaNode node;
+        LambdaNodeConvert item;
+
+        WriteLine();
+        func = x => (string?)x!;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeConvert>(node);
+        Assert.Equal("((String) x)", node.ToString());
+
+        WriteLine();
+        func = x => (int)x.Delta.Beta.Alpha;
+        node = LambdaParser.Parse(func).Result;
+        WriteLine($"> Result: {node}");
+        item = Assert.IsType<LambdaNodeConvert>(node);
+        Assert.Equal("((Int32) x.Delta.Beta.Alpha)", node.ToString());
+    });
 }
