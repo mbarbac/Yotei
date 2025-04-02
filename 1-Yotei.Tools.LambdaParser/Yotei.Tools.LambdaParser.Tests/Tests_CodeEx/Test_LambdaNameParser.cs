d29a0e5c2d14cc54a42e98824e930b7128ccd0e3
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-04-02 14:52:13 +0200

    xxx

---
+++ 1-Yotei.Tools.LambdaParser/Yotei.Tools.LambdaParser.Tests/Tests_CodeEx/Test_LambdaNameParser.cs
@@ -0,0 +1,191 @@
+﻿using static Yotei.Tools.Diagnostics.DebugEx;
+using static System.ConsoleColor;
+
+namespace Yotei.Tools.Tests;
+
+// ========================================================
+//[Enforced]
+public static class Test_LambdaNameParser
+{
+    //[Enforced]
+    [Fact]
+    public static void Parse_Standard_Empty() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => x, out var parts, out var arg);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Empty(name);
+        Assert.Empty(parts);
+        Assert.Equal("x", arg.LambdaName);
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_Standard_Empty_Multipart() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => x.x.x, out var parts, out var arg);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Empty(name);
+        Assert.Equal(2, parts.Length);
+        Assert.True(parts.All(x => x is null || x.Length == 0));
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_Standard_SinglePart() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => x.Alpha, out var parts, out var arg);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal("Alpha", name);
+        Assert.Single(parts);
+        Assert.Equal("Alpha", parts[0]);
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_Standard_MultiPart() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => x.Alpha.Beta.Delta, out var parts, out var arg);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal("Alpha.Beta.Delta", name);
+        Assert.Equal(3, parts.Length);
+        Assert.Equal("Alpha", parts[0]);
+        Assert.Equal("Beta", parts[1]);
+        Assert.Equal("Delta", parts[2]);
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_Standard_Complex_MultiPart() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => x.x.Alpha.Beta, out string[] parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal(".Alpha.Beta", name);
+        Assert.Equal(3, parts.Length);
+        Assert.Empty(parts[0]);
+        Assert.Equal("Alpha", parts[1]);
+        Assert.Equal("Beta", parts[2]);
+
+        name = LambdaNameParser.Parse(x => x.Alpha.x.Beta, out parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal("Alpha..Beta", name);
+        Assert.Equal(3, parts.Length);
+        Assert.Equal("Alpha", parts[0]);
+        Assert.Empty(parts[1]);
+        Assert.Equal("Beta", parts[2]);
+
+        name = LambdaNameParser.Parse(x => x.Alpha.Beta.x, out parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal("Alpha.Beta.", name);
+        Assert.Equal(3, parts.Length);
+        Assert.Equal("Alpha", parts[0]);
+        Assert.Equal("Beta", parts[1]);
+        Assert.Empty(parts[2]);
+    });
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_Constant_Null() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => null!, out string[] parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Empty(name);
+        Assert.Single(parts);
+        Assert.True(parts.All(x => x.Length == 0));
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_Constant_String_SinglePart() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => string.Empty, out string[] parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Empty(name);
+        Assert.Single(parts);
+        Assert.True(parts.All(x => x.Length == 0));
+
+        name = LambdaNameParser.Parse(x => " ", out parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Empty(name);
+        Assert.Single(parts);
+        Assert.True(parts.All(x => x.Length == 0));
+
+        name = LambdaNameParser.Parse(x => " Alpha ", out parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal("Alpha", name);
+        Assert.Single(parts);
+        Assert.Equal("Alpha", parts[0]);
+    });
+
+    //[Enforced]
+    [Fact]
+    public static void Parse_Constant_String_MultiPart() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => " . . . ", out string[] parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Empty(name);
+        Assert.Equal(4, parts.Length);
+        Assert.True(parts.All(x => x.Length == 0));
+
+        name = LambdaNameParser.Parse(x => " Alpha . Beta . Delta ", out parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal("Alpha.Beta.Delta", name);
+        Assert.Equal(3, parts.Length);
+        Assert.Equal("Alpha", parts[0]);
+        Assert.Equal("Beta", parts[1]);
+        Assert.Equal("Delta", parts[2]);
+
+        name = LambdaNameParser.Parse(x => " . Beta . Delta ", out parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal(".Beta.Delta", name);
+        Assert.Equal(3, parts.Length);
+        Assert.Empty(parts[0]);
+        Assert.Equal("Beta", parts[1]);
+        Assert.Equal("Delta", parts[2]);
+
+        name = LambdaNameParser.Parse(x => " Alpha . . Delta ", out parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal("Alpha..Delta", name);
+        Assert.Equal(3, parts.Length);
+        Assert.Equal("Alpha", parts[0]);
+        Assert.Empty(parts[1]);
+        Assert.Equal("Delta", parts[2]);
+
+        name = LambdaNameParser.Parse(x => " Alpha . Beta . ", out parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Equal("Alpha.Beta.", name);
+        Assert.Equal(3, parts.Length);
+        Assert.Equal("Alpha", parts[0]);
+        Assert.Equal("Beta", parts[1]);
+        Assert.Empty(parts[2]);
+    });
+
+    [Enforced]
+    [Fact]
+    public static void Parse_Constant_String_And_Numeric() => Repeater.Repeat(() =>
+    {
+        var name = LambdaNameParser.Parse(x => x("James")("00")(7), out string[] parts);
+        WriteLine($"Name: '{name}'");
+
+        Assert.Empty(name);
+        Assert.Equal(4, parts.Length);
+        Assert.True(parts.All(x => x.Length == 0));
+    });
+}
--- 1-Yotei.Tools.LambdaParser/Yotei.Tools.LambdaParser/CodeEx/LambdaNameParser.cs
+++ 1-Yotei.Tools.LambdaParser/Yotei.Tools.LambdaParser/CodeEx/LambdaNameParser.cs
@@ -8,7 +8,7 @@ public class LambdaNameParser
 {
     /// <summary>
     /// Parses the given dynamic lambda expression into the dot-separated names that expression
-    /// resolves into. If all parts are null or empty, then returns an empty string.
+    /// resolves into. If all parts are empty, then returns an empty string.
     /// </summary>
     /// <param name="expression"></param>
     /// <returns></returns>
@@ -17,7 +17,7 @@ public class LambdaNameParser
     /// <summary>
     /// Parses the given dynamic lambda expression into the dot-separated names that expression
     /// resolves into, and returns in the out arguments the collection of parts names. If all
-    /// parts are null or empty, then returns an empty string, and an empty array of parts.
+    /// parts are empty, then returns an empty string, and an empty array of parts.
     /// </summary>
     /// <param name="expression"></param>
     /// <param name="parts"></param>
@@ -30,7 +30,7 @@ public class LambdaNameParser
     /// <summary>
     /// Parses the given dynamic lambda expression into the dot-separated names that expression
     /// resolves into, and returns the  dynamic argument used for the expression. If all parts
-    /// are null or empty, then returns an empty string.
+    /// are empty, then returns an empty string.
     /// </summary>
     /// <param name="expression"></param>
     /// <param name="parts"></param>
@@ -43,8 +43,8 @@ public class LambdaNameParser
     /// <summary>
     /// Parses the given dynamic lambda expression into the dot-separated names that expression
     /// resolves into, and returns in the out arguments the collection of parts names, and the
-    /// dynamic argument used for the expression. If all parts are null or empty, then returns
-    /// an empty string, and an empty array of parts.
+    /// dynamic argument used for the expression. If all parts are empty, then returns an empty
+    /// string, and an empty array of parts.
     /// </summary>
     /// <param name="expression"></param>
     /// <param name="parts"></param>
@@ -53,6 +53,44 @@ public class LambdaNameParser
     public static string Parse(
         Func<dynamic, object> expression, out string[] parts, out LambdaNodeArgument arg)
     {
-        throw null;
+        expression.ThrowWhenNull();
+
+        var parser = LambdaParser.Parse(expression);
+        if (parser.DynamicArguments.Length != 1) throw new ArgumentException(
+            "Dynamic lambda name parser requires one and only one dynamic argument.")
+            .WithData(parser, nameof(expression));
+
+        var named = new LambdaNameParser();
+        named.Parser = parser;
+        named.Argument = arg = parser.DynamicArguments[0];
+
+        named.OnParse(parser.Result, true);
+
+        parts = named.List.ToArray();
+        return parts.All(x => x.Length == 0) ? string.Empty : string.Join('.', parts);
     }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Private constructor.
+    /// </summary>
+    private LambdaNameParser() { }
+
+    /// <summary>
+    /// The parser used by this instance.
+    /// </summary>
+    LambdaParser Parser { get; set; } = default!;
+
+    /// <summary>
+    /// The dynamic argument used to invoked the expression parsed.
+    /// </summary>
+    LambdaNodeArgument Argument { get; set; } = default!;
+
+    /// <summary>
+    /// The list of parsed parts.
+    /// </summary>
+    List<string> List { get; } = [];
+
+    // ----------------------------------------------------l
 }
