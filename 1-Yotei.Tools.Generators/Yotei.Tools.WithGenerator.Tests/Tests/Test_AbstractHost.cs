5a3d5de79d34a2ff98446a33dd6defb6625cc539
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-04-07 20:20:26 +0200

    xxx

--- 1-Yotei.Tools.Generators/Yotei.Tools.WithGenerator.Tests/Tests/Test_AbstractHost.cs
+++ 1-Yotei.Tools.Generators/Yotei.Tools.WithGenerator.Tests/Tests/Test_AbstractHost.cs
@@ -2,7 +2,7 @@
 
 using static Yotei.Tools.Diagnostics.ConsoleEx;
 using static System.ConsoleColor;
-/*
+
 namespace Yotei.Tools.WithGenerator.Tests.Abstracts
 {
     using IFaces;
@@ -32,6 +32,27 @@ namespace Yotei.Tools.WithGenerator.Tests.Abstracts
         {
             // --------------------------------------------
             [With(InheritMembers = true)]
+            public abstract partial class AbstractFoo<T> : IFoo<T>
+            {
+                public AbstractFoo(string name) => Name = name;
+                protected AbstractFoo(ConcreteFoo<T> source) : this(source.Name) { }
+                public override string ToString() => Name ?? "-";
+                public string Name { get; set; }
+            }
+
+            // --------------------------------------------
+
+            [With(InheritMembers = true)]
+            public abstract partial class AbstractFromAbstract<T> : AbstractFoo<T>, IBar<T, T>
+            {
+                public AbstractFromAbstract(string name, string branch) : base(name) => Branch = branch;
+                protected AbstractFromAbstract(AbstractFromAbstract<T> source) : this(source.Name, source.Branch) { }
+                public override string ToString() => $"{(Name ?? "-")}, {(Branch ?? "-")}";
+                public string Branch { get; set; }
+            }
+
+            // --------------------------------------------
+            [With(InheritMembers = true)]
             public partial class ConcreteFoo<T> : IFoo<T>
             {
                 public ConcreteFoo(string name) => Name = name;
@@ -39,16 +60,6 @@ namespace Yotei.Tools.WithGenerator.Tests.Abstracts
                 public override string ToString() => Name ?? "-";
                 public string Name { get; set; }
             }
-
-            // --------------------------------------------
-            //[With(InheritMembers = true)]
-            //public abstract partial class AbstractFoo<T> : IFoo<T>
-            //{
-            //    public AbstractFoo(string name) => Name = name;
-            //    protected AbstractFoo(ConcreteFoo<T> source) : this(source.Name) { }
-            //    public override string ToString() => Name ?? "-";
-            //    public string Name { get; set; }
-            //}
         }
     }
 
@@ -56,6 +67,22 @@ namespace Yotei.Tools.WithGenerator.Tests.Abstracts
     //[Enforced]
     public static class Test
     {
+        //[Enforced]
+        [Fact]
+        public static void Test_AbstractFoo_HasWithMethod()
+        {
+            var type = typeof(TOther.AbstractFoo<int>);
+            var method = type.GetMethods().FirstOrDefault(x =>
+                x.Name == "WithName" &&
+                x.GetParameters().Length == 1 &&
+                x.GetParameters()[0].ParameterType == typeof(string));
+
+            Assert.NotNull(method);
+            Assert.True(method.IsAbstract);
+        }
+
+        // ------------------------------------------------
+
         //[Enforced]
         [Fact]
         public static void Test_ConcreteFoo_HasWithMethod()
@@ -72,117 +99,6 @@ namespace Yotei.Tools.WithGenerator.Tests.Abstracts
 
         // ------------------------------------------------
 
-        //[Enforced]
-        //[Fact]
-        //public static void Test_AbstractFoo_HasWithMethod()
-        //{
-        //    var type = typeof(TOther.AbstractFoo<int>);
-        //    var method = type.GetMethods().FirstOrDefault(x =>
-        //        x.Name == "WithName" &&
-        //        x.GetParameters().Length == 1 &&
-        //        x.GetParameters()[0].ParameterType == typeof(string));
-
-        //    Assert.NotNull(method);
-        //    Assert.True(method.IsAbstract);
-        //}
+        // ------------------------------------------------
     }
-}*/
-/*
-
-    // ====================================================
-    namespace TTypes
-    {
-        public partial class TOther
-        {
-            
-
-            // --------------------------------------------
-            [Cloneable]
-            public abstract partial class AbstractFromConcrete<K, T> : ConcreteFoo<T>, IOther.IBar<K, T>
-            {
-                public AbstractFromConcrete(string name, string branch) : base(name) => Branch = branch;
-                protected AbstractFromConcrete(AbstractFromConcrete<K, T> source) : this(source.Name, source.Branch) { }
-                public override string ToString() => $"{(Name ?? "-")}, {(Branch ?? "-")}";
-                public string Branch { get; set; }
-            }
-
-            // --------------------------------------------
-            [Cloneable]
-            public abstract partial class AbstractFromAbstract<K, T> : ConcreteFoo<T>, IOther.IBar<K, T>
-            {
-                public AbstractFromAbstract(string name, string branch) : base(name) => Branch = branch;
-                protected AbstractFromAbstract(AbstractFromAbstract<K, T> source) : this(source.Name, source.Branch) { }
-                public override string ToString() => $"{(Name ?? "-")}, {(Branch ?? "-")}";
-                public string Branch { get; set; }
-            }
-        }
-    }
-
-    // ====================================================
-    //[Enforced]
-    public static class Test
-    {
-        
-
-        // --------------------------------------------
-
-        //[Enforced]
-        [Fact]
-        public static void Test_AbstractFromConcrete_HasCloneMethod()
-        {
-            var type = typeof(TOther.AbstractFromConcrete<int, int>);
-            var method = type.GetMethods().FirstOrDefault(x =>
-                x.Name == "Clone" &&
-                x.GetParameters().Length == 0);
-
-            Assert.NotNull(method);
-            Assert.True(method.IsVirtual);
-        }
-
-        //[Enforced]
-        [Fact]
-        public static void Test_AbstractFromConcreteFoo_Has_ICloneable_Via_IBar()
-        {
-            var type = typeof(TOther.AbstractFromConcrete<int, int>);
-            var iface = typeof(ICloneable);
-
-            type.GetInterfaceMap(iface);
-        }
-
-        // --------------------------------------------
-
-        //[Enforced]
-        [Fact]
-        public static void Test_AbstractFromAbstract_HasCloneMethod()
-        {
-            var type = typeof(TOther.AbstractFromAbstract<int, int>);
-            var method = type.GetMethods().FirstOrDefault(x =>
-                x.Name == "Clone" &&
-                x.GetParameters().Length == 0);
-
-            Assert.NotNull(method);
-            Assert.True(method.IsVirtual);
-        }
-
-        //[Enforced]
-        [Fact]
-        public static void Test_AbstractFromAbstractFoo_Has_ICloneable_Via_IBar()
-        {
-            var type = typeof(TOther.AbstractFromAbstract<int, int>);
-            var iface = typeof(ICloneable);
-
-            type.GetInterfaceMap(iface);
-        }
-
-        //[Enforced]
-        [Fact]
-        public static void Test_Foo_Cloning()
-        {
-            var source = new TOther.ConcreteFoo<int>("Bond");
-            var target = source.Clone();
-
-            Assert.NotSame(target, source);
-            Assert.Equal("Bond", target.Name);
-        }
-    }
- */
+}
