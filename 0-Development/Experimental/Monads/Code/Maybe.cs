477ff01337408b3792b4b0401987cf31b120cd0b
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-04-24 09:24:02 +0200

    xxx

--- 0-Development/Experimental/Monads/Code/Maybe.cs
+++ 0-Development/Experimental/Monads/Code/Maybe.cs
@@ -1,12 +1,42 @@
-﻿namespace Experimental;
+﻿namespace Experimental.Monads;
 
 // ========================================================
-//[Enforced]
-//public static class Test_
-//{
-//    //[Enforced]
-//    [Fact]
-//    public static void Test_()
-//    {
-//    }
-//}
+/// <summary>
+/// Helpers for the <see cref="Maybe{T}"/> monad.
+/// </summary>
+public static class Maybe
+{
+    /// <summary>
+    /// Returns a new instance that carries the given value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static Maybe<T> Some<T>(T value) => new(value);
+
+    /// <summary>
+    /// Represents an invalid monad of an arbitrary type.
+    /// </summary>
+    public static Invalid None { get; } = new();
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Represents an arbitrary invalid monad.
+    /// </summary>
+    [SuppressMessage("", "IDE0060")]
+    public readonly struct Invalid : IComparable<Invalid>, IEquatable<Invalid>
+    {
+        public int CompareTo(Invalid _) => 0;
+        public bool Equals(Invalid _) => true;
+        public override bool Equals([NotNullWhen(true)] object? obj) => obj is Invalid;
+        public override int GetHashCode() => int.MinValue;
+
+        public static bool operator ==(Invalid x, Invalid y) => true;
+        public static bool operator !=(Invalid x, Invalid y) => false;
+        public static bool operator <(Invalid x, Invalid y) => false;
+        public static bool operator >(Invalid x, Invalid y) => false;
+        public static bool operator >=(Invalid x, Invalid y) => true;
+        public static bool operator <=(Invalid x, Invalid y) => true;
+    }
+}
