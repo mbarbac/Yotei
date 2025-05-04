2801bc388190ace2dcc4ec8dc7b0a3b8d41478c4
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-05-04 11:53:00 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Api/ICommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Yotei.ORM.Records;
+
+// ========================================================
+/// <summary>
+/// Represents a command that can be executed using a given connection.
+/// </summary>
+[Cloneable]
+public partial interface ICommand
+{
+}
