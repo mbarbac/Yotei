63e0ae9f2c7698e3b0251cfc3f4ae41d281c5dd8
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-05-04 10:13:01 +0200

    xxx

--- 2-Yotei.ORM/Yotei.ORM/2.Records/Api/ICommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Yotei.ORM.Records;
-
-// ========================================================
-[Cloneable]
-public partial interface ICommand
-{
-    ICommandInfo GetCommandInfo();
-}
