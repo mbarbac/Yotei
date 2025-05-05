74019ed4abf5cec91da6fa99a8bf89582ec6ba13
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-05-05 10:52:17 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM.Tests/2.Records/Test_MetadataEntry.cs
@@ -0,0 +1,23 @@
+﻿using static Yotei.Tools.Diagnostics.ConsoleEx;
+using static System.ConsoleColor;
+
+namespace Yotei.ORM.Tests.Records;
+
+// ========================================================
+//[Enforced]
+public static class Test_MetadataEntry
+{
+    //[Enforced]
+    [Fact]
+    public static void Test_Create()
+    {
+        var tag = new MetadataTag(false, ["one", "two"]);
+        var entry = new MetadataEntry(tag, null);
+
+        Assert.Same(tag, entry.Tag);
+        Assert.Null(entry.Value);
+
+        try { entry = new(null!, null); Assert.Fail(); }
+        catch (ArgumentNullException) { }
+    }
+}
