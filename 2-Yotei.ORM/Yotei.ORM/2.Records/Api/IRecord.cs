e12546cedf9d1a74388a239123faba1d62c967cf
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-05-09 20:05:41 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Api/IRecord.cs
@@ -0,0 +1,149 @@
+﻿namespace Yotei.ORM.Records;
+
+// ========================================================
+/// <summary>
+/// Represents the ordered collection of related values to be retrieved or to be persisted into
+/// an underlying database.
+/// </summary>
+[Cloneable]
+public partial interface IRecord : IEnumerable<object?>, IEquatable<IRecord>
+{
+    /// <summary>
+    /// The number of values carried by this instance.
+    /// </summary>
+    int Count { get; }
+
+    /// <summary>
+    /// Gets the value at the given index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    object? this[int index] { get; }
+
+    /// <summary>
+    /// Gets the value at the given index, casted to the given type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    [return: MaybeNull]
+    T This<T>(int index);
+
+    /// <summary>
+    /// Gets an array with the values in this record.
+    /// </summary>
+    /// <returns></returns>
+    object?[] ToArray();
+
+    /// <summary>
+    /// Gets a list with the values in this record.
+    /// </summary>
+    /// <returns></returns>
+    List<object?> ToList();
+
+    /// <summary>
+    /// Gets a list with the requested number of values starting from the given index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    List<object?> ToList(int index, int count);
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Returns a new instance with the given number of elements, starting from the given index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    IRecord GetRange(int index, int count);
+
+    /// <summary>
+    /// Returns a new instance where the value at the given index has been replaced by the new
+    /// given one.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    IRecord Replace(int index, object? value);
+
+    /// <summary>
+    /// Returns a new instance where the given value has been added to the original record.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    IRecord Add(object? value);
+
+    /// <summary>
+    /// Returns a new instance where the values from the given range have been added to the
+    /// original record.
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    IRecord AddRange(IEnumerable<object?> range);
+
+    /// <summary>
+    /// Returns a new instance where the given value has been inserted into the original record
+    /// at the given index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    IRecord Insert(int index, object? value);
+
+    /// <summary>
+    /// Returns a new instance where the values from the given range have been inserted into the
+    /// original record, starting at the given index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    IRecord InsertRange(int index, IEnumerable<object?> range);
+
+    /// <summary>
+    /// Returns a new instance where the original value at the given index has been removed.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    IRecord RemoveAt(int index);
+
+    /// <summary>
+    /// Returns a new instance where the given number of values, starting at the given index,
+    /// have been removed.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    IRecord RemoveRange(int index, int count);
+
+    /// <summary>
+    /// Returns a new instance where the first value that matches the given predicate has been
+    /// removed.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    IRecord Remove(Predicate<object?> predicate);
+
+    /// <summary>
+    /// Returns a new instance where the last value that matches the given predicate has been
+    /// removed.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    IRecord RemoveLast(Predicate<object?> predicate);
+
+    /// <summary>
+    /// Returns a new instance where all the values that match the given predicate have been
+    /// removed.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    IRecord RemoveAll(Predicate<object?> predicate);
+
+    /// <summary>
+    /// Returns a new instance where all the original values have been removed.
+    /// </summary>
+    /// <returns></returns>
+    IRecord Clear();
+}
