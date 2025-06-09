5044094c791713fde7469d5acf74d013d62c3b94
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-09 20:41:23 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/1.Core/Api/ICommandInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Yotei.ORM;
+
+// ========================================================
+/// <summary>
+/// Represents the information needed to run a command.
+/// <para>Instances of this type are intended to be immutable ones.</para>
+/// </summary>
+[Cloneable]
+public partial interface ICommandInfo
+{
+    /// <summary>
+    /// Returns a new builder based upon the contents of this instance.
+    /// </summary>
+    /// <returns></returns>
+    IBuilder CreateBuilder();
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/1.Core/Api/ICommandInfo_IBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace Yotei.ORM;
+
+partial interface ICommandInfo
+{
+    // ====================================================
+    /// <summary>
+    /// Represents a builder for <see cref="ICommandInfo"/> instances.
+    /// </summary>
+    [Cloneable]
+    public partial interface IBuilder
+    {
+        /// <summary>
+        /// Returns a new instance based upon the contents of this builder.
+        /// </summary>
+        /// <returns></returns>
+        ICommandInfo CreateInstance();
+    }
+}
