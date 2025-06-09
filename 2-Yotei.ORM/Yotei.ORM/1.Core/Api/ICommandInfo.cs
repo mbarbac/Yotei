5ebde136dac3555347ca557b34fdff29015b771c
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-09 17:34:26 +0200

    xxx

--- 2-Yotei.ORM/Yotei.ORM/1.Core/Api/ICommandInfo.cs
+++ 2-Yotei.ORM/Yotei.ORM/1.Core/Api/ICommandInfo.cs
@@ -1,3 +1,103 @@
 ﻿namespace Yotei.ORM;
 
 // ========================================================
+/// <summary>
+/// Represents the information needed to run a command.
+/// <para>Instances of this type are intended to be immutable ones.</para>
+/// </summary>
+[Cloneable]
+public partial interface ICommandInfo
+{
+    /// <summary>
+    /// Gets a new builder based upon the contents of this instance.
+    /// </summary>
+    /// <returns></returns>
+    IBuilder GetBuilder();
+
+    // ------------------------------------------------
+
+    /// <summary>
+    /// The engine this instance is associated with.
+    /// </summary>
+    IEngine Engine { get; }
+
+    /// <summary>
+    /// The captured command's text.
+    /// </summary>
+    string Text { get; }
+
+    /// <summary>
+    /// The captured command's parameters.
+    /// </summary>
+    IParameterList Parameters { get; }
+
+    /// <summary>
+    /// Determines if this instance is an empty one, or not.
+    /// </summary>
+    bool IsEmpty { get; }
+
+    // ------------------------------------------------
+
+    /// <summary>
+    /// Returns a new instance where the existing text has been inconditionally replaced by the
+    /// new given one, without any attempt of matching any specifications in the text with the
+    /// names of the existing parameters.
+    /// <br/> Incorrect usage of this method may render the returned instance unusable.
+    /// <br/> No bracket '{...}' specifications are allowed.
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    ICommandInfo ReplaceText(string? text);
+
+    /// <summary>
+    /// Returns a new instance where the existing collecion of parameters has been replaced
+    /// inconditionally by the one obtained from the given range of values, without any attempt
+    /// of matching their names with any text specification.
+    /// <br/> Incorrect usage of this method may render the returned instance unusable.
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    ICommandInfo ReplaceValues(params object?[]? range);
+
+    // ------------------------------------------------
+
+    /// <summary>
+    /// Returns a new instance where all the original contents have been cleared.
+    /// </summary>
+    /// <returns></returns>
+    ICommandInfo Clear();
+
+    /// <summary>
+    /// Returns a new instance where the contents from the given source have been added to the
+    /// original ones.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    ICommandInfo Add(ICommandInfo source);
+
+    /// <summary>
+    /// Returns a new instance where the contents from the given source have been added to the
+    /// original ones.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    ICommandInfo Add(IBuilder source);
+
+    /// <summary>
+    /// Returns a new instance where the given text and the collection of parameters obtained
+    /// from the given range of values, if any, have been added to the original contents.
+    /// <br/>- If <paramref name="text"/> is null, then the range of values is just captured
+    /// without any attempts of matching their names with bracket specifications. Similarly,
+    /// if <paramref name="range"/> is empty, then the text is captured without intercepting
+    /// any dangling specifications.
+    /// <br/>- Parameter specifications must always be bracket ones, either positional '{n}'
+    /// or named '{name}' ones, where name contains the name of the parameter or the name of
+    /// the unique property of an anonymous item. In both cases, 'name' may or may not start
+    /// with the engine parameters' prefix.
+    /// <br/>- No unused parameters are allowed, neither dangling bracket specifications.
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    ICommandInfo Add(string? text, params object?[]? range);
+}
--- 2-Yotei.ORM/Yotei.ORM/1.Core/Code/CommandInfo_Builder.cs
+++ 2-Yotei.ORM/Yotei.ORM/1.Core/Code/CommandInfo_Builder.cs
@@ -1,3 +1,158 @@
 ﻿namespace Yotei.ORM.Code;
 
-// ========================================================
+partial class CommandInfo
+{
+    // ====================================================
+    /// <inheritdoc cref="ICommandInfo.IBuilder"/>
+    [Cloneable]
+    public partial class Builder : ICommandInfo.IBuilder
+    {
+        readonly StringBuilder _Text;
+        readonly ParameterList.Builder _Parameters;
+
+        /// <summary>
+        /// Initializes a new empty instance.
+        /// </summary>
+        /// <param name="engine"></param>
+        public Builder(IEngine engine)
+        {
+            _Text = new();
+            _Parameters = new(engine);
+        }
+
+        /// <summary>
+        /// Initializes a new instance with the contents of the given source.
+        /// </summary>
+        /// <param name="source"></param>
+        public Builder(ICommandInfo source)
+        {
+            source.ThrowWhenNull();
+
+            _Text = new(source.Text);
+            _Parameters = new(source.Engine, source.Parameters);
+        }
+
+        /// <summary>
+        /// Initializes a new instance with the contents of the given source.
+        /// </summary>
+        /// <param name="source"></param>
+        public Builder(ICommandInfo.IBuilder source)
+        {
+            source.ThrowWhenNull();
+
+            _Text = new(source.Text);
+            _Parameters = new(source.Engine, source.Parameters);
+        }
+
+        /// <summary>
+        /// Initializes a new instance using the given text and the collection of parameters
+        /// obtained from the given range of values, if any.
+        /// <br/>- If <paramref name="text"/> is null, then the range of values is just captured
+        /// without any attempts of matching their names with bracket specifications. Similarly,
+        /// if <paramref name="range"/> is empty, then the text is captured without intercepting
+        /// any dangling specifications.
+        /// <br/>- Parameter specifications must always be bracket ones, either positional '{n}'
+        /// or named '{name}' ones, where name contains the name of the parameter or the name of
+        /// the unique property of an anonymous item. In both cases, 'name' may or may not start
+        /// with the engine parameters' prefix.
+        /// <br/>- No unused parameters are allowed, neither dangling bracket specifications.
+        /// </summary>
+        /// <param name="engine"></param>
+        /// <param name="text"></param>
+        /// <param name="range"></param>
+        public Builder(IEngine engine, string? text, params object?[]? range)
+            : this(engine)
+            => Add(text, range);
+
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        /// <param name="source"></param>
+        protected Builder(Builder source)
+        {
+            source.ThrowWhenNull();
+
+            _Text = new(source._Text.ToString());
+            _Parameters = new(source.Engine, source._Parameters);
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            if (_Parameters.Count == 0) return _Text.ToString();
+
+            var pars = $"[{string.Join(", ", _Parameters)}]";
+            return _Text.Length == 0 ? pars : $"{_Text} : {pars}";
+        }
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public virtual CommandInfo ToInstance()
+        {
+            throw null;
+        }
+        ICommandInfo ICommandInfo.IBuilder.ToInstance() => ToInstance();
+
+        /// <inheritdoc/>
+        public IEngine Engine => _Parameters.Engine;
+
+        StringComparison Comparison => Engine.CaseSensitiveNames
+            ? StringComparison.Ordinal
+            : StringComparison.OrdinalIgnoreCase;
+
+        string Prefix => Engine.ParameterPrefix;
+
+        /// <inheritdoc/>
+        public string Text => _Text.ToString();
+
+        /// <inheritdoc/>
+        public IParameterList Parameters
+        {
+            get => throw null;
+        }
+
+        /// <inheritdoc/>
+        public bool IsEmpty => _Text.Length == 0 && _Parameters.Count == 0;
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public virtual bool ReplaceText(string? text)
+        {
+            throw null;
+        }
+
+        /// <inheritdoc/>
+        public virtual bool ReplaceValues(params object?[]? range)
+        {
+            throw null;
+        }
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public virtual bool Clear()
+        {
+            throw null;
+        }
+
+        /// <inheritdoc/>
+        public virtual bool Add(ICommandInfo source)
+        {
+            throw null;
+        }
+
+        /// <inheritdoc/>
+        public virtual bool Add(ICommandInfo.IBuilder source)
+        {
+            throw null;
+        }
+
+        /// <inheritdoc/>
+        public virtual bool Add(string? text, params object?[]? range)
+        {
+            throw null;
+        }
+    }
+}
