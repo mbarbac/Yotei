618fecc0d1d4010654bc79878028802f738e8d37
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-09 19:32:06 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM.Tests/5.Tests_Generators.Collections/Code/ElementList[T]_Builder.cs
@@ -0,0 +1,95 @@
+﻿using IHost = Yotei.ORM.Tests.Generators.IElementList_T;
+using IItem = Yotei.ORM.Tests.Generators.IElement;
+
+namespace Yotei.ORM.Tests.Generators;
+
+partial class ElementList_T
+{
+    // ====================================================
+    /// <summary>
+    /// Represents a builder for <see cref="IHost"/> intances.
+    /// </summary>
+    [DebuggerDisplay("{ToDebugString(5)}")]
+    [Cloneable]
+    public partial class Builder : CoreList<IItem>, IHost.IBuilder
+    {
+        /// <summary>
+        /// Initializes a new empty instance.
+        /// </summary>
+        /// <param name="sensitive"></param>
+        public Builder(bool sensitive) => CaseSensitive = sensitive;
+
+        /// <summary>
+        /// Initializes a new empty instance with the given initial capacity.
+        /// </summary>
+        /// <param name="sensitive"></param>
+        /// <param name="capacity"></param>
+        public Builder(bool sensitive, int capacity) : this(sensitive) => Capacity = capacity;
+
+        /// <summary>
+        /// Initializes a new instance with the elements of the given range.
+        /// </summary>
+        /// <param name="sensitive"></param>
+        /// <param name="range"></param>
+        public Builder(bool sensitive, IEnumerable<IItem> range) : this(sensitive) => AddRange(range);
+
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        /// <param name="source"></param>
+        protected Builder(Builder source) : this(source.CaseSensitive) => AddRange(source);
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public override IItem ValidateItem(IItem item) => item.ThrowWhenNull();
+
+        /// <inheritdoc/>
+        public override bool ExpandItems => true;
+
+        /// <inheritdoc/>
+        public override bool IsValidDuplicate(IItem source, IItem item)
+            => ReferenceEquals(source, item)
+            ? true
+            : throw new DuplicateException("Duplicated element.").WithData(item);
+
+        /// <inheritdoc/>
+        public override IEqualityComparer<IItem> Comparer => _Comparer ??= new ItemComparer(CaseSensitive);
+        IEqualityComparer<IItem>? _Comparer = null;
+
+        readonly struct ItemComparer(bool CaseSensitive) : IEqualityComparer<IItem>
+        {
+            public bool Equals(IItem? x, IItem? y)
+            {
+                if (x is null && y is null) return true;
+                if (x is null || y is null) return false;
+
+                return string.Compare(x.Name, y.Name, !CaseSensitive) == 0;
+            }
+            public int GetHashCode([DisallowNull] IItem obj) => throw new NotImplementedException();
+        }
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public virtual ElementList_T CreateInstance() => new(CaseSensitive, this);
+        IHost IHost.IBuilder.CreateInstance() => CreateInstance();
+
+        /// <inheritdoc/>
+        public bool CaseSensitive
+        {
+            get => _CaseSensitive;
+            set
+            {
+                if (value == _CaseSensitive) return;
+
+                var range = ToList();
+                Clear();
+
+                _CaseSensitive = value;
+                AddRange(range);
+            }
+        }
+        bool _CaseSensitive;
+    }
+}
