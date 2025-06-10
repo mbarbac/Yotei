749e46bbb8b3b6bd6c0c39a95978d95ec3459cf6
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-10 20:12:49 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/4.Internals/StringTokenizers/Code/StrTokenizer.cs
@@ -0,0 +1,60 @@
+﻿namespace Yotei.ORM.Internals;
+
+// ========================================================
+/// <inheritdoc cref="IStrTokenizer"/>
+[InheritWiths]
+public abstract partial class StrTokenizer : IStrTokenizer
+{
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    public StrTokenizer() { }
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="source"></param>
+    protected StrTokenizer(StrTokenizer source)
+    {
+        Comparison = source.Comparison;
+    }
+
+    /// <inheritdoc/>
+    public StringComparison Comparison { get; init; } = StringComparison.CurrentCulture;
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public IStrToken Tokenize(string source, bool reduce = true)
+    {
+        var token = new StrTokenText(source);
+        return Tokenize(token, reduce);
+    }
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public IStrToken Tokenize(IStrToken source, bool reduce = true)
+    {
+        source.ThrowWhenNull();
+
+        // It may happen that the things to search for are split between adjacent text elements,
+        // so we need to combine them before extracting contents...
+
+        if (source is IStrTokenChain chain) source = chain.ReduceText();
+        source = source.TokenizeWith(Extract);
+
+        if (reduce) source = source.Reduce(Comparison);
+        return source;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Invoked to extract the appropriate tokens from the given source.
+    /// <br/> This method MUST NOT invoke any reduce action.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    protected abstract IStrToken Extract(string source);
+}
