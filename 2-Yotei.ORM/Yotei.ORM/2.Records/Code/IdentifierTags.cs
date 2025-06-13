91829dbd08ad2a0d2338ebf9ce758559e4bcacee
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-13 17:47:23 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Code/IdentifierTags.cs
@@ -0,0 +1,89 @@
+﻿using IHost = Yotei.ORM.IIdentifierTags;
+using IItem = Yotei.ORM.IMetadataTag;
+
+namespace Yotei.ORM.Code;
+
+// ========================================================
+/// <inheritdoc cref="IHost"/>
+[DebuggerDisplay("{ToDebugString(5)}")]
+[InvariantList<IItem>]
+public partial class IdentifierTags : IHost
+{
+    /// <inheritdoc/>
+    protected override Builder Items { get; }
+
+    /// <summary>
+    /// Invoked to create the initial repository of contents of this instance.
+    /// </summary>
+    protected virtual Builder OnInitialize(bool sensitiveTags) => new(sensitiveTags);
+
+    /// <summary>
+    /// Initializes a new empty instance.
+    /// </summary>
+    /// <param name="sensitiveTags"></param>
+    public IdentifierTags(bool sensitiveTags) => Items = OnInitialize(sensitiveTags);
+
+    /// <summary>
+    /// Initializes a new instance with the elements of the given range.
+    /// </summary>
+    /// <param name="sensitiveTags"></param>
+    /// <param name="range"></param>
+    public IdentifierTags(
+        bool sensitiveTags, IEnumerable<IItem> range) : this(sensitiveTags) => Items.AddRange(range);
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="source"></param>
+    protected IdentifierTags(IdentifierTags source) => Items = source.Items.Clone();
+
+    /// <inheritdoc/>
+    public override string ToString() => Items.ToString();
+
+    /// <inheritdoc/>
+    public virtual Builder CreateBuilder() => throw null;
+    IHost.IBuilder IHost.CreateBuilder() => CreateBuilder();
+
+    // ------------------------------------------------
+
+    /// <inheritdoc/>
+    public bool Equals(IHost? other) => throw null;
+
+    // ------------------------------------------------
+
+    /// <inheritdoc/>
+    public bool CaseSensitiveTags => Items.CaseSensitiveTags;
+
+    /// <inheritdoc/>
+    public IEnumerable<string> Names => Items.Names;
+
+    // ------------------------------------------------
+
+    /// <inheritdoc/>
+    public bool Contains(string name) => Items.IndexOf(name) >= 0;
+
+    /// <inheritdoc/>
+    public bool Contains(IEnumerable<string> range) => Items.Contains(range);
+
+    /// <inheritdoc/>
+    public int IndexOf(string name) => Items.IndexOf(name);
+
+    /// <inheritdoc/>
+    public int IndexOf(IEnumerable<string> range) => Items.IndexOf(range);
+
+    /// <inheritdoc/>
+    public int LastIndexOf(IEnumerable<string> range) => Items.LastIndexOf(range);
+
+    /// <inheritdoc/>
+    public List<int> IndexesOf(IEnumerable<string> range) => Items.IndexesOf(range);
+
+    // ------------------------------------------------
+
+    /// <inheritdoc/>
+    public virtual IdentifierTags Remove(string name) => throw null;
+    IHost IHost.Remove(string name) => Remove(name);
+
+    /// <inheritdoc/>
+    public virtual IdentifierTags Remove(IEnumerable<string> range) => throw null;
+    IHost IHost.Remove(IEnumerable<string> range) => Remove(range);
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Code/IdentifierTags_Builder.cs
@@ -0,0 +1,129 @@
+﻿using IHost = Yotei.ORM.IIdentifierTags;
+using IItem = Yotei.ORM.IMetadataTag;
+
+namespace Yotei.ORM.Code;
+
+partial class IdentifierTags
+{
+    // ====================================================
+    /// <inheritdoc cref="IHost.IBuilder"/>
+    [Cloneable]
+    public partial class Builder : CoreList<IItem>, IHost.IBuilder
+    {
+        /// <summary>
+        /// Initializes a new empty instance.
+        /// </summary>
+        /// <param name="sensitiveTags"></param>
+        public Builder(bool sensitiveTags) => CaseSensitiveTags = sensitiveTags;
+
+        /// <summary>
+        /// Initializes a new empty instance with the given initial capacity.
+        /// </summary>
+        /// <param name="sensitiveTags"></param>
+        /// <param name="capacity"></param>
+        public Builder(
+            bool sensitiveTags, int capacity) : this(sensitiveTags) => Capacity = capacity;
+
+        /// <summary>
+        /// Initializes a new instance with the elements of the given range.
+        /// </summary>
+        /// <param name="sensitiveTags"></param>
+        /// <param name="range"></param>
+        public Builder(
+            bool sensitiveTags, IEnumerable<IItem> range) : this(sensitiveTags) => AddRange(range);
+
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        /// <param name="source"></param>
+        protected Builder(Builder source) : this(source.CaseSensitiveTags) => AddRange(source);
+
+        /// <inheritdoc/>
+        public virtual IdentifierTags CreateInstance() => throw null;
+        IHost IHost.IBuilder.CreateInstance() => CreateInstance();
+
+        /// <inheritdoc/>
+        public override string ToString() => throw null;
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public override IItem ValidateItem(IItem item)
+        {
+            item.ThrowWhenNull();
+
+            if (CaseSensitiveTags != item.CaseSensitiveTags) throw new ArgumentException(
+                "Case Sensitive of the given element is not the same as this instance's one.")
+                .WithData(item)
+                .WithData(this);
+
+            return item;
+        }
+
+        /// <inheritdoc/>
+        public override bool ExpandItems => true;
+
+        /// <inheritdoc/>
+        public override bool IsValidDuplicate(IItem source, IItem item)
+            => throw new DuplicateException("Duplicated element.").WithData(item);
+
+        /// <inheritdoc/>
+        public override IEqualityComparer<IItem> Comparer => _Comparer ??= new ItemComparer();
+        IEqualityComparer<IItem>? _Comparer = null;
+
+        readonly struct ItemComparer() : IEqualityComparer<IItem>
+        {
+            public bool Equals(IItem? x, IItem? y)
+            {
+                if (x is null && y is null) return true;
+                if (x is null || y is null) return false;
+
+                return x.Equals(y);
+            }
+            public int GetHashCode([DisallowNull] IItem obj) => throw new NotImplementedException();
+        }
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public bool CaseSensitiveTags { get; }
+
+        /// <inheritdoc/>
+        public IEnumerable<string> Names
+        {
+            get
+            {
+                foreach (var tag in this)
+                    foreach (var name in tag) yield return name;
+            }
+        }
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public bool Contains(string name) => throw null;
+
+        /// <inheritdoc/>
+        public bool Contains(IEnumerable<string> range) => throw null;
+
+        /// <inheritdoc/>
+        public int IndexOf(string name) => throw null;
+
+        /// <inheritdoc/>
+        public int IndexOf(IEnumerable<string> range) => throw null;
+
+        /// <inheritdoc/>
+        public int LastIndexOf(IEnumerable<string> range) => throw null;
+
+        /// <inheritdoc/>
+        public List<int> IndexesOf(IEnumerable<string> range) => throw null;
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        public virtual int Remove(string name) => throw null;
+
+        /// <inheritdoc/>
+        public virtual int Remove(IEnumerable<string> range) => throw null;
+    }
+}
