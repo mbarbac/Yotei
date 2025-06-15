ee24210be7ae2bfd6c54392c859e833d67118481
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-15 10:43:29 +0200

    xxx

--- 2-Yotei.ORM/Yotei.ORM.Tests/4.Tests_Internals/DbVisitor/Test_DbTokenVisitor_Dynamic.cs
+++ 2-Yotei.ORM/Yotei.ORM.Tests/4.Tests_Internals/DbVisitor/Test_DbTokenVisitor_Dynamic.cs
@@ -197,61 +197,61 @@ public static class Test_DbTokenVisitor_Dynamic
     // ----------------------------------------------------
 
     //[Enforced]
-    //[Fact]
-    //public static void Test_Command_Direct()
-    //{
-    //    var engine = new FakeEngine();
-    //    var connection = new FakeConnection(engine);
-    //    var visitor = new DbTokenVisitor(connection);
-    //    ICommandInfo.IBuilder builder;
+    [Fact]
+    public static void Test_Command_Direct()
+    {
+        var engine = new FakeEngine();
+        var connection = new FakeConnection(engine);
+        var visitor = new DbTokenVisitor(connection);
+        ICommandInfo.IBuilder builder;
 
-    //    var command = new FakeCommand(connection,
-    //        "SELECT * FROM [Emp] WHERE [LastName] = {0}",
-    //        null!);
+        var command = new FakeCommand(connection,
+            "SELECT * FROM [Emp] WHERE [LastName] = {0}",
+            null!);
 
-    //    builder = visitor.Visit(x => command);
-    //    Assert.Equal("(SELECT * FROM [Emp] WHERE [LastName] = #0)", builder.Text);
-    //    Assert.Single(builder.Parameters);
-    //    Assert.Equal("#0", builder.Parameters[0].Name); Assert.Null(builder.Parameters[0].Value);
-    //}
-
-    //[Enforced]
-    //[Fact]
-    //public static void Test_Command_Invoke()
-    //{
-    //    var engine = new FakeEngine();
-    //    var connection = new FakeConnection(engine);
-    //    var visitor = new DbTokenVisitor(connection);
-    //    ICommandInfo.IBuilder builder;
-
-    //    var command = new FakeCommand(connection,
-    //        "SELECT * FROM [Emp] WHERE [LastName] = {0}",
-    //        null!);
-
-    //    builder = visitor.Visit(x => x(command));
-    //    Assert.Equal("(SELECT * FROM [Emp] WHERE [LastName] = #0)", builder.Text);
-    //    Assert.Single(builder.Parameters);
-    //    Assert.Equal("#0", builder.Parameters[0].Name); Assert.Null(builder.Parameters[0].Value);
-    //}
+        builder = visitor.Visit(x => command);
+        Assert.Equal("(SELECT * FROM [Emp] WHERE [LastName] = #0)", builder.Text);
+        Assert.Single(builder.Parameters);
+        Assert.Equal("#0", builder.Parameters[0].Name); Assert.Null(builder.Parameters[0].Value);
+    }
 
     //[Enforced]
-    //[Fact]
-    //public static void Test_Command_Invoke_WithAlias()
-    //{
-    //    var engine = new FakeEngine();
-    //    var connection = new FakeConnection(engine);
-    //    var visitor = new DbTokenVisitor(connection);
-    //    ICommandInfo.IBuilder builder;
+    [Fact]
+    public static void Test_Command_Invoke()
+    {
+        var engine = new FakeEngine();
+        var connection = new FakeConnection(engine);
+        var visitor = new DbTokenVisitor(connection);
+        ICommandInfo.IBuilder builder;
 
-    //    var command = new FakeCommand(connection,
-    //        "SELECT * FROM [Emp] WHERE [LastName] = {0}",
-    //        null!);
+        var command = new FakeCommand(connection,
+            "SELECT * FROM [Emp] WHERE [LastName] = {0}",
+            null!);
 
-    //    builder = visitor.Visit(x => x(command).As(x.Any));
-    //    Assert.Equal("(SELECT * FROM [Emp] WHERE [LastName] = #0) AS [Any]", builder.Text);
-    //    Assert.Single(builder.Parameters);
-    //    Assert.Equal("#0", builder.Parameters[0].Name); Assert.Null(builder.Parameters[0].Value);
-    //}
+        builder = visitor.Visit(x => x(command));
+        Assert.Equal("(SELECT * FROM [Emp] WHERE [LastName] = #0)", builder.Text);
+        Assert.Single(builder.Parameters);
+        Assert.Equal("#0", builder.Parameters[0].Name); Assert.Null(builder.Parameters[0].Value);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Command_Invoke_WithAlias()
+    {
+        var engine = new FakeEngine();
+        var connection = new FakeConnection(engine);
+        var visitor = new DbTokenVisitor(connection);
+        ICommandInfo.IBuilder builder;
+
+        var command = new FakeCommand(connection,
+            "SELECT * FROM [Emp] WHERE [LastName] = {0}",
+            null!);
+
+        builder = visitor.Visit(x => x(command).As(x.Any));
+        Assert.Equal("(SELECT * FROM [Emp] WHERE [LastName] = #0) AS [Any]", builder.Text);
+        Assert.Single(builder.Parameters);
+        Assert.Equal("#0", builder.Parameters[0].Name); Assert.Null(builder.Parameters[0].Value);
+    }
 
     // -----------------------------------------------------
 
