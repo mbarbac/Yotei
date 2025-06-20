35372fff90b8c5955ff634f32410f46844c843d0
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-20 17:28:49 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/4.Internals/Fragments/FragmentGroupBy.cs
@@ -0,0 +1,11 @@
+﻿namespace Yotei.ORM.Internals;
+
+/// <summary>
+/// Represents the ability of parsing GROUP BY clauses.
+/// <br/>- Standard syntax: 'x => x.Element'.
+/// </summary>
+/// <remarks>
+/// GROUP BY clauses accept complex specifications:
+/// <br/>- Example: 'GROUP BY EXTRACT(YEAR FROM date), ...'
+/// </remarks>
+public static partial class FragmentGroupBy { }
