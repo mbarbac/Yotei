c7edf43ad378df70d2dcdf8c7b462fb0c86378bc
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-24 15:11:43 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/4.Internals/Fragments/FragmentGroupBy.cs
@@ -0,0 +1,88 @@
+﻿namespace Yotei.ORM.Internals;
+
+/// <summary>
+/// Represents the ability of parsing GROUP BY clauses.
+/// <br/>- Standard syntax: 'x => x.Element'.
+/// </summary>
+/// <remarks>
+/// GROUP BY clauses accept complex specifications:
+/// <br/>- Example: 'GROUP BY EXTRACT(YEAR FROM date), ...'
+/// </remarks>
+public static partial class FragmentGroupBy
+{
+    // ====================================================
+    /// <summary>
+    /// Represents an entry in a collection of fragments used to build a GROUP BY clause.
+    /// </summary>
+    [Cloneable]
+    public partial class Entry : Fragment.Entry
+    {
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="master"></param>
+        /// <param name="body"></param>
+        public Entry(Master master, IDbToken body) : base(master, body) { }
+
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        /// <param name="source"></param>
+        protected Entry(Entry source) : base(source) { }
+    }
+
+    // ====================================================
+    /// <summary>
+    /// Represents the collection of fragments used to build a GROUP BY clause.
+    /// </summary>
+    [Cloneable]
+    public partial class Master : Fragment.Master
+    {
+        /// <inheritdoc/>
+        public override string CLAUSE { get; set; } = "GROUP BY";
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="command"></param>
+        public Master(ICommand command) : base(command) { }
+
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        /// <param name="source"></param>
+        protected Master(Master source) : base(source) { }
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        protected override Entry Validate(Fragment.Entry entry)
+        {
+            if (entry is not Entry valid) throw new ArgumentException(
+                $"Entry is not a valid {CLAUSE} one.")
+                .WithData(entry);
+
+            return valid;
+        }
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        protected override Entry OnCreate(DbTokenInvoke? head, IDbToken body, DbTokenInvoke? tail)
+        {
+            // Finishing...
+            var entry = new Entry(this, body);
+
+            if (head is not null) entry._Head = head;
+            if (tail is not null) entry._Tail = tail;
+            return entry;
+        }
+
+        // ------------------------------------------------
+
+        /// <inheritdoc/>
+        /// <remarks>We only need to provide a separator when the previous entry specifies a
+        /// connector.</remarks>
+        public override string? Separator(Fragment.Entry entry) => ", ";
+    }
+}
