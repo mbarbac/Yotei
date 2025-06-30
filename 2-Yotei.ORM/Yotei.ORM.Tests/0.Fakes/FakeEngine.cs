1c815dcdcd93da28e5c8b2f21ecac453e3ff3867
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-06-30 19:58:58 +0200

    xxx

--- 2-Yotei.ORM/Yotei.ORM.Tests/0.Fakes/FakeEngine.cs
+++ 2-Yotei.ORM/Yotei.ORM.Tests/0.Fakes/FakeEngine.cs
@@ -5,7 +5,7 @@
 [InheritWiths]
 public partial class FakeEngine : Engine
 {
-    public FakeEngine() : base() { }
+    public FakeEngine() : base() => KnownTags = new FakeKnownTags(CASESENSITIVETAGS);
     protected FakeEngine(FakeEngine source) : base(source) { }
     public override string ToString() => "FakeEngine";
 }
