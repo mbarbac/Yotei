36c2449a2b0e1448c0ef172dbc720a6d88e325a6
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-07-02 20:13:41 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/5.Internals/DbTokens/Code/DbToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Yotei.ORM.Internals;
+
+// ========================================================
+/// <summary>
+/// Helpers for <see cref="IDbToken"/> instances.
+/// </summary>
+public static class DbToken
+{
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/5.Internals/DbTokens/Code/DbTokenArgument.cs
@@ -0,0 +1,4 @@
+﻿namespace Yotei.ORM.Internals;
+
+// ========================================================
+public interface DbTokenArgument : IDbToken { }
