043b0d3a5bc8b7f860f5d63f651d5c23092a5f92
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-07-03 20:34:54 +0200

    xxx

--- 2-Yotei.ORM/Yotei.ORM.Tests/5.Tests_Internals/Tests_DbTokens/Test_DbTokenExtractor.cs
+++ 2-Yotei.ORM/Yotei.ORM.Tests/5.Tests_Internals/Tests_DbTokens/Test_DbTokenExtractor.cs
@@ -1,1 +1,77 @@
-﻿
+﻿#pragma warning disable IDE0018
+
+using Argument = Yotei.ORM.Internals.DbTokenArgument;
+using Literal = Yotei.ORM.Internals.DbTokenLiteral;
+using Chain = Yotei.ORM.Internals.DbTokenChain;
+using Identifier = Yotei.ORM.Internals.DbTokenIdentifier;
+using Setter = Yotei.ORM.Internals.DbTokenSetter;
+
+namespace Yotei.ORM.Tests.Internals.DbTokens;
+
+// ========================================================
+//[Enforced]
+public static class Test_Tree_DbTokenExtractor
+{
+    //[Enforced]
+    [Fact]
+    public static void Test_Tree_ExtractFirst_Argument()
+    {
+        var engine = new FakeEngine();
+        IDbToken source, item;
+        IDbToken? removed = null;
+
+        // Arguments ARE NOT extracted from trees...
+        source = DbLambdaParser.Parse(engine, x => x.One.Two);
+        item = source.ExtractFirst(x => x is Argument, out removed);
+        Assert.Same(source, item);
+        Assert.Null(removed);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Tree_ExtractFirst_Not_Found()
+    {
+        var engine = new FakeEngine();
+        IDbToken source, item;
+        IDbToken? removed = null;
+
+        source = DbLambdaParser.Parse(engine, x => x.One.Two);
+        item = source.ExtractFirst(x => x is Literal, out removed);
+        Assert.Same(source, item);
+        Assert.Null(removed);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Tree_ExtractFirst_Found_Not_Hosted_Header()
+    {
+        var engine = new FakeEngine();
+        IDbToken source, item;
+        IDbToken? removed = null;
+        Identifier id;
+        Setter setter;
+
+        source = DbLambdaParser.Parse(engine, x => (x.One = x.Two).Three.Four);
+        item = source.ExtractFirst(x => x is Setter, out removed);
+        Assert.NotSame(source, item);
+        id = Assert.IsType<Identifier>(item); Assert.Equal("x.[Three].[Four]", id.ToString());
+        Assert.NotNull(removed);
+        setter = Assert.IsType<Setter>(removed); Assert.Equal("(x.[One] = x.[Two])", setter.ToString());
+
+        item = source.ExtractFirst(x => x is Identifier id && id.Value == "[Three]", out removed);
+        Assert.NotSame(source, item);
+        id = Assert.IsType<Identifier>(item); Assert.Equal("(x.[One] = x.[Two]).[Four]", id.ToString());
+        Assert.NotNull(removed);
+        id = Assert.IsType<Identifier>(removed); Assert.Equal("x.[Three]", id.ToString());
+
+        item = source.ExtractFirst(x => x is Identifier id && id.Value == "[Four]", out removed);
+        Assert.NotSame(source, item);
+        id = Assert.IsType<Identifier>(item); Assert.Equal("(x.[One] = x.[Two]).[Three]", id.ToString());
+        Assert.NotNull(removed);
+        id = Assert.IsType<Identifier>(removed); Assert.Equal("x.[Four]", id.ToString());
+    }
+
+    // ----------------------------------------------------
+
+
+}
--- 2-Yotei.ORM/Yotei.ORM/5.Internals/DbTokens/DbTokenExtractor.cs
+++ 2-Yotei.ORM/Yotei.ORM/5.Internals/DbTokens/DbTokenExtractor.cs
@@ -1,1 +1,142 @@
-﻿
+﻿namespace Yotei.ORM.Internals;
+
+/// <br/>- If the source token is a chain instance, then this method behaves as the standard
+/// removal one.
+
+// ========================================================
+public static class DbTokenExtractor
+{
+    /// <summary>
+    /// Extracts from the given tree-like source the first ocurrence of a token that matches the
+    /// given predicate. If so, returns the resulting tree without that token, which is placed in
+    /// the out argument. Otherwise, returns the original source and the out argument is set to
+    /// <c>null</c>.
+    /// <br/>- If the source token is not a hosted one, then the actual tree to remove from will
+    /// be the left-most part of that source, if possible, and the result is a token of the same
+    /// type but with its left part with the matching token removed.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="predicate"></param>
+    /// <param name="removed"></param>
+    /// <returns></returns>
+    public static IDbToken ExtractFirst(
+        this IDbToken source, Predicate<IDbToken> predicate, out IDbToken? removed)
+    {
+        source.ThrowWhenNull();
+        predicate.ThrowWhenNull();
+        removed = null;
+
+        // Case: source is a hosted instance...
+        if (source is DbTokenHosted)
+        {
+            var arg = source.GetArgument() ?? throw new InvalidOperationException(
+                "Cannot obtain the dynamic argument of the given source tree.")
+                .WithData(source);
+
+            IDbToken cloned = source.Clone();
+            IDbToken? prev = null;
+
+            IDbToken? item = cloned;
+            while (item is not null)
+            {
+                if (item is not DbTokenArgument && predicate(item))
+                {
+                    removed = item.Clone();
+                    if (removed is DbTokenHosted xremoved) xremoved.Host = arg;
+
+                    if (prev is null) // Found last one in tree...
+                    {
+                        var host = (item as DbTokenHosted)!.Host;
+                        return host ?? arg;
+                    }
+                    else // Found an intermediate one...
+                    {
+                        var host = (item as DbTokenHosted)?.Host;
+                        ((DbTokenHosted)prev).Host = host ?? arg;
+                        return cloned;
+                    }
+                }
+
+                prev = item;
+                item = (item as DbTokenHosted)?.Host;
+            }
+        }
+
+        // Othwerwise...
+        return source;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Extracts from the given tree-like source the last ocurrence of a token that matches the
+    /// given predicate. If so, returns the resulting tree without that token, which is placed in
+    /// the out argument. Otherwise, returns the original source and the out argument is set to
+    /// <c>null</c>.
+    /// <br/>- If the source token is not a hosted one, then the actual tree to remove from will
+    /// be the left-most part of that source, if possible, and the result is a token of the same
+    /// type but with its left part with the matching token removed.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="predicate"></param>
+    /// <param name="removed"></param>
+    /// <returns></returns>
+    public static IDbToken ExtractLast(
+        this IDbToken source, Predicate<IDbToken> predicate, out IDbToken? removed)
+    {
+        throw null;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Extracts from the given tree-like source all the ocurrences of tokens that match the
+    /// given predicate. If so, returns the resulting tree without that tokens, which are placed
+    /// in the out argument. Otherwise, returns the original source and the out argument is set
+    /// to <c>null</c>.
+    /// <br/>- If the source token is not a hosted one, then the actual tree to remove from will
+    /// be the left-most part of that source, if possible, and the result is a token of the same
+    /// type but with its left part with the matching tokens removed.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="predicate"></param>
+    /// <param name="removed"></param>
+    /// <returns></returns>
+    public static IDbToken ExtractAll(
+        this IDbToken source, Predicate<IDbToken> predicate, out List<IDbToken> removed)
+    {
+        throw null;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Extracts from the given tree-like source the combined chain of head invoke operations.
+    /// If any is found, then returns the resulting tree without that head tokens, and sets the
+    /// out argument to an invoke one with the combined arguments, in order. Otherwise, returns
+    /// the original instance and sets the out argument to <c>null</c>.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="removed"></param>
+    /// <returns></returns>
+    public static IDbToken ExtractHeadInvokes(IDbToken source, out DbTokenInvoke? removed)
+    {
+        throw null;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Extracts from the given tree-like source the combined chain of tail invoke operations.
+    /// If any is found, then returns the resulting tree without that tail tokens, and sets the
+    /// out argument to an invoke one with the combined arguments, in order. Otherwise, returns
+    /// the original instance and sets the out argument to <c>null</c>.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="removed"></param>
+    /// <returns></returns>
+    public static IDbToken ExtractTailInvokes(IDbToken source, out DbTokenInvoke? removed)
+    {
+        throw null;
+    }
+}
