f0d64b8f5dd051abc8633e63503e15d35c4de435
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-07-31 09:37:49 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/5.Internals/Miscelanea/StrExtractor.cs
@@ -0,0 +1,131 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yotei.ORM.Internals;
+
+// ========================================================
+public static class StrExtractor
+{
+    /// <summary>
+    /// Tries to extract at the head of the given source any of the given specifications. If so,
+    /// returns true and sets the out main argument to the trimmed remaining source without the
+    /// found specification, and the out found argument to that specification. Otherwise returns
+    /// false and the out arguments are set to arbitrary values.
+    /// </summary>
+    public static bool ExtractHead(
+        this string source,
+        bool sensitive, bool isolated, out string main, out string found,
+        params string[] specs)
+    {
+        source.ThrowWhenNull();
+        specs.ThrowWhenNull();
+
+        found = string.Empty;
+        main = source.NullWhenEmpty(trim: true)!; if (main is null) return false;
+
+        for (int i = 0; i < specs.Length; i++)
+        {
+            var spec = specs[i].NotNullNotEmpty(trim: true);
+            var index = main.IndexOf(spec, sensitive);
+            if (index == 0)
+            {
+                if (isolated)
+                {
+                    var temp = main.FindIsolated(spec, 0, sensitive);
+                    if (temp != index) continue;
+                }
+
+                found = main[..spec.Length];
+                main = main.Remove(index, spec.Length).Trim();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Tries to extract at the tail of the given source any of the given specifications. If so,
+    /// returns true and sets the out main argument to the trimmed remaining source without the
+    /// found specification, and the out found argument to that specification. Otherwise returns
+    /// false and the out arguments are set to arbitrary values.
+    /// </summary>
+    public static bool ExtractTail(
+        this string source,
+        bool sensitive, bool isolated, out string main, out string found,
+        params string[] specs)
+    {
+        source.ThrowWhenNull();
+        specs.ThrowWhenNull();
+
+        found = string.Empty;
+        main = source.NullWhenEmpty(trim: true)!; if (main is null) return false;
+
+        for (int i = 0; i < specs.Length; i++)
+        {
+            var spec = specs[i].NotNullNotEmpty(trim: true);
+            var index = main.LastIndexOf(spec, sensitive);
+            if (index >= 0 && (index + spec.Length) == main.Length)
+            {
+                if (isolated)
+                {
+                    var ini = index - 1;
+                    if (ini < 0) ini = 0;
+
+                    var temp = main.FindIsolated(spec, ini, sensitive);
+                    if (temp != index) continue;
+                }
+
+                found = main[..spec.Length];
+                main = main.Remove(index, spec.Length).Trim();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Tries to extract the first ocurrence of any of the given specifications. If so, returns
+    /// true and sets the out head and tail out arguments to the trimmed remainings without the
+    /// found specification, and the out found argument to that specification. Otherwise returns
+    /// false and the out arguments are set to arbitrary values.
+    /// </summary>
+    public static bool ExtractFirst(
+        this string source,
+        bool sensitive, bool isolated, out string head, out string found, out string tail,
+        params string[] specs)
+    {
+        source.ThrowWhenNull();
+        specs.ThrowWhenNull();
+
+        found = string.Empty;
+        tail = string.Empty;
+        head = source.NullWhenEmpty(trim: true)!; if (head is null) return false;
+
+        throw null;
+
+        //for (int i = 0; i < specs.Length; i++)
+        //{
+        //    var spec = specs[i].NotNullNotEmpty(trim: true);
+        //    var index = main.IndexOf(spec, sensitive);
+        //    if (index == 0)
+        //    {
+        //        if (isolated)
+        //        {
+        //            var temp = main.FindIsolated(spec, 0, sensitive);
+        //            if (temp != index) continue;
+        //        }
+
+        //        found = main[..spec.Length];
+        //        main = main.Remove(index, spec.Length).Trim();
+        //        return true;
+        //    }
+        //}
+
+        //return false;
+    }
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/5.Internals/Miscelanea/StrFindIsolated.cs
@@ -0,0 +1,66 @@
+﻿namespace Yotei.ORM.Internals;
+
+// ========================================================
+public static class StrFindIsolated
+{
+    /// <summary>
+    /// The default collection of separators for isolated values.
+    /// </summary>
+    public static readonly ImmutableArray<char> SEPARATORS =
+        " ()[]{}<>\"'`´\\|!&@#=+-*/%^"
+        .ToImmutableArray();
+
+    /// <summary>
+    /// Returns the index of the first ocurrence of the given value in the given source string,
+    /// starting at the given index, or -1 if not found. Isolation is tested using both the given
+    /// head and tails separators that, if null, use default ones, and whether or not the value
+    /// appears at the start or end of the source sequence.
+    /// </summary>
+    public static int FindIsolated(
+        this string source, string value, int ini,
+        bool sensitive,
+        IEnumerable<char>? heads = null, IEnumerable<char>? tails = null)
+    {
+        var comparison = sensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
+        return FindIsolated(source, value, ini, comparison, heads, tails);
+    }
+
+    /// <summary>
+    /// Returns the index of the first ocurrence of the given value in the given source string,
+    /// starting at the given index, or -1 if not found. Isolation is tested using both the given
+    /// head and tails separators that, if null, use default ones, and whether or not the value
+    /// appears at the start or end of the source sequence.
+    /// </summary>
+    public static int FindIsolated(
+        this string source, string value, int ini,
+        StringComparison comparison,
+        IEnumerable<char>? heads = null, IEnumerable<char>? tails = null)
+    {
+        source = source.ThrowWhenNull();
+        value = value.NotNullNotEmpty();
+        heads ??= SEPARATORS;
+        tails ??= SEPARATORS;
+
+        if (source.Length == 0) return -1;
+        if (value.Length > source.Length) return -1;
+
+        var pos = source.IndexOf(value, ini, comparison);
+        if (pos >= 0)
+        {
+            if (pos > 0)
+            {
+                var c = source[pos - 1];
+                if (!heads.Contains(c)) return -1;
+            }
+
+            var len = pos + value.Length;
+            if (len < source.Length)
+            {
+                var c = source[len];
+                if (!tails.Contains(c)) return -1;
+            }
+        }
+
+        return pos;
+    }
+}
