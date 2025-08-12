456b56b8bb4b7b971f7d357559a489422d290c9b
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-08-12 18:23:45 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM.Tests/1.Tests_Core/Test_Command.cs
@@ -0,0 +1,35 @@
+﻿namespace Yotei.ORM.Tests;
+
+// ========================================================
+//[Enforced]
+public static class Test_Command
+{
+    //[Enforced]
+    [Fact]
+    public static void Test_Create_Empty()
+    {
+        var engine = new FakeEngine();
+        var connection = new FakeConnection(engine);
+
+        var command = new FakeCommand(connection);
+        Assert.True(command.GetCommandInfo().IsEmpty);
+        Assert.False(command.IsValid);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Create_Populated()
+    {
+        var engine = new FakeEngine();
+        var connection = new FakeConnection(engine);
+
+        var command = new FakeCommand(
+            connection, "SELECT * FROM [Emps] WHERE [Id] = {0}",
+            "007");
+
+        var info = command.GetCommandInfo();
+        Assert.Equal("SELECT * FROM [Emps] WHERE [Id] = #0", info.Text);
+        Assert.Single(info.Parameters);
+        Assert.Equal("#0", info.Parameters[0].Name); Assert.Equal("007", info.Parameters[0].Value);
+    }
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/1.Core/Code/Command.cs
@@ -0,0 +1,65 @@
+﻿namespace Yotei.ORM.Code;
+
+// ========================================================
+/// <inheritdoc cref="ICommand"/>
+[Cloneable]
+[InheritWiths]
+public abstract partial class Command : ICommand
+{
+    /// <summary>
+    /// Initializes a new empty instance.
+    /// </summary>
+    /// <param name="connection"></param>
+    public Command(IConnection connection) => Connection = connection;
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="source"></param>
+    protected Command(Command source)
+    {
+        source.ThrowWhenNull();
+
+        Connection = source.Connection;
+        Locale = source.Locale;
+    }
+
+    /// <inheritdoc/>
+    public override string ToString() => GetCommandInfo().ToString() ?? string.Empty;
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public IConnection Connection
+    {
+        get => _Connection;
+        init => _Connection = value.ThrowWhenNull();
+    }
+    IConnection _Connection = default!;
+
+    protected IEngine Engine => Connection.Engine;
+
+    /// <inheritdoc/>
+    public Locale Locale
+    {
+        get => _Locale;
+        init => _Locale = value.ThrowWhenNull();
+    }
+    Locale _Locale = new();
+
+    /// <inheritdoc/>
+    public abstract bool IsValid { get; }
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public abstract ICommandInfo GetCommandInfo();
+
+    /// <inheritdoc/>
+    public abstract ICommandInfo GetCommandInfo(bool iterable);
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public abstract ICommand Clear();
+}
