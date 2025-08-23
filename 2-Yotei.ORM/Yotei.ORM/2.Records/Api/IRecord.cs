4c6dc013ec180c52e7e6e4adac64743a3ddb6cf5
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-08-23 12:13:25 +0200

    xxx

--- 2-Yotei.ORM/Yotei.ORM/2.Records/Api/IRecord.cs
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Api/IRecord.cs
@@ -64,4 +64,169 @@ public partial interface IRecord : IEnumerable<object?>, IEquatable<IRecord>
     /// </summary>
     /// <returns></returns>
     List<object?> ToList();
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Returns a new instance with the given number of original elements, starting at the given
+    /// index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    IRecord GetRange(int index, int count);
+
+    /// <summary>
+    /// Returns a new instance where the value at the given index has been replaced by the new
+    /// given one.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    IRecord Replace(int index, object? value);
+
+    /// <summary>
+    /// Returns a new instance where the value and schema entry at the given index have been
+    /// replaced by the new given ones.
+    /// <br/> This method throws an exception if this instance is a schema-less one.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    /// <param name="entry"></param>
+    /// <returns></returns>
+    IRecord Replace(int index, object? value, ISchemaEntry entry);
+
+    /// <summary>
+    /// Returns a new instance where the given value has been added to the original instance.
+    /// <br/> This method throws an exception if this instance is a schema-full one.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    IRecord Add(object? value);
+
+    /// <summary>
+    /// Returns a new instance where the given value and schema entry have been added to the
+    /// original instance.
+    /// <br/> This method throws an exception if this instance is a schema-less one.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="entry"></param>
+    /// <returns></returns>
+    IRecord Add(object? value, ISchemaEntry entry);
+
+    /// <summary>
+    /// Returns a new instance where the values from the given range have been added to the
+    /// original instance.
+    /// <br/> This method throws an exception if this instance is a schema-full one.
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    IRecord AddRange(IEnumerable<object?> range);
+
+    /// <summary>
+    /// Returns a new instance where the values and schema entries from the given ranges have
+    /// been added to the original instance.
+    /// <br/> This method throws an exception if this instance is a schema-less one.
+    /// </summary>
+    /// <param name="range"></param>
+    /// <param name="entries"></param>
+    /// <returns></returns>
+    IRecord AddRange(IEnumerable<object?> range, IEnumerable<ISchemaEntry> entries);
+
+    /// <summary>
+    /// Returns a new instance where the given value has been inserted into the original instance,
+    /// at the given index.
+    /// <br/> This method throws an exception if this instance is a schema-full one.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    IRecord Insert(int index, object? value);
+
+    /// <summary>
+    /// Returns a new instance where the given value and schema entry have been inserted into the
+    /// original instance, at the given index.
+    /// <br/> This method throws an exception if this instance is a schema-less one.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    /// <param name="entry"></param>
+    /// <returns></returns>
+    IRecord Insert(int index, object? value, ISchemaEntry entry);
+
+    /// <summary>
+    /// Returns a new instance where the values from the given range have been inserted into
+    /// the original instamce, starting at the given index.
+    /// <br/> This method throws an exception if this instance is a schema-full one.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    IRecord InsertRange(int index, IEnumerable<object?> range);
+
+    /// <summary>
+    /// Returns a new instance where the values and schema entries from the given ranges have
+    /// been inserted into the original instance, starting at the given index.
+    /// <br/> This method throws an exception if this instance is a schema-less one.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="range"></param>
+    /// <param name="entries"></param>
+    /// <returns></returns>
+    IRecord InsertRange(int index, IEnumerable<object?> range, IEnumerable<ISchemaEntry> entries);
+
+    /// <summary>
+    /// Returns a new instance where the value and schema entry, if any, at the given index
+    /// have been removed from the original collection.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    IRecord RemoveAt(int index);
+
+    /// <summary>
+    /// Returns a new instance where the given number of values and schema entries, if any, have
+    /// been removed from the original collection, starting from the given index.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    IRecord RemoveRange(int index, int count);
+
+    /// <summary>
+    /// Returns a new instance where all the original contents have been cleared.
+    /// </summary>
+    /// <returns></returns>
+    IRecord Clear();
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Returns a new record with the changes detected at the target one when it is compared
+    /// against this instance, using a default comparer to compare their respective values, or
+    /// null if no changes are detected.
+    /// <br/> Both records must be schema-full instances.
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="orphanSources"></param>
+    /// <param name="orphanTargets"></param>
+    /// <returns></returns>
+    IRecord? GetChanges(
+        IRecord target,
+        bool orphanSources = false, bool orphanTargets = false);
+
+    /// <summary>
+    /// Returns a new record with the changes detected at the target one when it is compared
+    /// against this instance, using the given comparer to compare their respective values, or
+    /// null if no changes are detected.
+    /// <br/> Both records must be schema-full instances.
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="comparer"></param>
+    /// <param name="orphanSources"></param>
+    /// <param name="orphanTargets"></param>
+    /// <returns></returns>
+    IRecord? GetChanges(
+        IRecord target,
+        IEqualityComparer comparer,
+        bool orphanSources = false, bool orphanTargets = false);
 }
---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Code/Record.cs
@@ -0,0 +1,370 @@
+﻿namespace Yotei.ORM.Records.Code;
+
+// ========================================================
+/// <inheritdoc cref="IRecord"/>
+[Cloneable]
+[InheritWiths]
+[DebuggerDisplay("{Items.ToDebugString(5)}")]
+public partial class Record : IRecord
+{
+    protected virtual Builder Items { get; }
+
+    /// <summary>
+    /// Initializes a new empty schema-less instance.
+    /// </summary>
+    public Record() => Items = new();
+
+    /// <summary>
+    /// Initializes a new schema-less instance with the values of the given range.
+    /// </summary>
+    /// <param name="range"></param>
+    public Record(IEnumerable<object?> range) : this() => Items.AddRange(range);
+
+    /// <summary>
+    /// Initializes a new schema-full instance with all its values set to <c>null</c>.
+    /// </summary>
+    /// <param name="engine"></param>
+    public Record(IEngine engine) => Items = new(engine);
+
+    /// <summary>
+    /// Initializes a new schema-full instance with the values and schema entries of the
+    /// given ranges.
+    /// </summary>
+    /// <param name="engine"></param>
+    /// <param name="range"></param>
+    /// <param name="entries"></param>
+    public Record(
+        IEngine engine, IEnumerable<object?> range, IEnumerable<ISchemaEntry> entries)
+        :this(engine)
+        => Items.AddRange(range, entries);
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="source"></param>
+    protected Record(Record source) => Items = source.Items.Clone();
+
+    /// <inheritdoc/>
+    public IEnumerator<object?> GetEnumerator() => Items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    /// <inheritdoc/>
+    public override string ToString() => Items.ToString();
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public virtual bool Equals(IRecord? other)
+    {
+        if (ReferenceEquals(this, other)) return true;
+        if (other is null) return false;
+
+        if (Count != other.Count) return false;
+        for (int i = 0; i < Items.Count; i++)
+        {
+            var item = Items[i];
+            var equal = item.EqualsEx(item);
+            if (!equal) return false;
+        }
+
+        var schema = Schema;
+        if (schema is null) { if (other.Schema is not null) return false; }
+        else if (!schema.Equals(other.Schema)) return false;
+
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => Equals(obj as IRecord);
+
+    public static bool operator ==(Record? host, IRecord? item)
+    {
+        if (host is null && item is null) return true;
+        if (host is null || item is null) return false;
+
+        return host.Equals(item);
+    }
+
+    public static bool operator !=(Record? host, IRecord? item) => !(host == item);
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        var code = 0;
+        for (int i = 0; i < Count; i++) code = HashCode.Combine(code, Items[i]);
+
+        var schema = Schema;
+        if (schema is not null) code = HashCode.Combine(code, schema);
+
+        return code;
+    }
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public virtual IRecord.IBuilder CreateBuilder() => Items.Clone();
+
+    /// <inheritdoc/>
+    public ISchema? Schema
+    {
+        get => Items.Schema;
+        init => Items.Schema = value;
+    }
+
+    /// <inheritdoc/>
+    public int Count => Items.Count;
+
+    /// <inheritdoc/>
+    public object? this[int index] => Items[index];
+
+    /// <inheritdoc/>
+    public object? this[string identifier] => Items[identifier];
+
+    /// <inheritdoc/>
+    public bool TryGet(string identifier, out object? value) => Items.TryGet(identifier, out value);
+
+    /// <inheritdoc/>
+    public object?[] ToArray() => Items.ToArray();
+
+    /// <inheritdoc/>
+    public List<object?> ToList() => Items.ToList();
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public virtual IRecord GetRange(int index, int count)
+    {
+        var builder = CreateBuilder();
+        var done = builder.GetRange(index, count);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord Replace(int index, object? value)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Replace(index, value);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord Replace(int index, object? value, ISchemaEntry entry)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Replace(index, value, entry);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord Add(object? value)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Add(value);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord Add(object? value, ISchemaEntry entry)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Add(value, entry);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord AddRange(IEnumerable<object?> range)
+    {
+        var builder = CreateBuilder();
+        var done = builder.AddRange(range);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord AddRange(IEnumerable<object?> range, IEnumerable<ISchemaEntry> entries)
+    {
+        var builder = CreateBuilder();
+        var done = builder.AddRange(range, entries);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord Insert(int index, object? value)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Insert(index, value);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord Insert(int index, object? value, ISchemaEntry entry)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Insert(index, value, entry);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord InsertRange(int index, IEnumerable<object?> range)
+    {
+        var builder = CreateBuilder();
+        var done = builder.InsertRange(index, range);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord InsertRange(
+        int index, IEnumerable<object?> range, IEnumerable<ISchemaEntry> entries)
+    {
+        var builder = CreateBuilder();
+        var done = builder.InsertRange(index, range, entries);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord RemoveAt(int index)
+    {
+        var builder = CreateBuilder();
+        var done = builder.RemoveAt(index);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord RemoveRange(int index, int count)
+    {
+        var builder = CreateBuilder();
+        var done = builder.RemoveRange(index, count);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IRecord Clear()
+    {
+        var builder = CreateBuilder();
+        var done = builder.Clear();
+        return done ? builder.CreateInstance() : this;
+    }
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public IRecord? GetChanges(
+        IRecord target,
+        bool orphanSources = false, bool orphanTargets = false)
+    {
+        var comparer = new ObjectComparer();
+        return GetChanges(target, comparer, orphanSources, orphanTargets);
+    }
+
+    /// <inheritdoc/>
+    public IRecord? GetChanges(
+        IRecord target,
+        IEqualityComparer comparer,
+        bool orphanSources = false, bool orphanTargets = false)
+    {
+        target.ThrowWhenNull();
+        comparer.ThrowWhenNull();
+
+        var source = this;
+        var sourceSchema = source.Schema ?? throw new InvalidOperationException("Source schema cannot be null.").WithData(source);
+        var targetSchema = target.Schema ?? throw new InvalidOperationException("Target schema cannot be null.").WithData(target);
+
+        var engine = sourceSchema.Engine;
+        var sourceValues = source.ToList(); var sourceEntries = sourceSchema.ToList();
+        var targetValues = target.ToList(); var targetEntries = targetSchema.ToList();
+
+        var values = new List<object?>();
+        var entries = new Schema.Builder(engine);
+
+        // Source elements...
+        for (int i = 0; i < sourceValues.Count; i++)
+        {
+            if (targetValues.Count == 0) break;
+
+            var sentry = sourceEntries[i];
+            var index = FindIndex(targetEntries, sentry.Identifier);
+            if (index < 0) index = FindMatch(targetEntries, sentry.Identifier);
+            if (index < 0) continue;
+
+            var svalue = sourceValues[i];
+            var tvalue = targetValues[index];
+            if (!comparer.Equals(svalue, tvalue))
+            {
+                values.Add(tvalue);
+                entries.Add(sentry);
+            }
+
+            sourceValues.RemoveAt(i); targetValues.RemoveAt(index);
+            sourceEntries.RemoveAt(i); targetEntries.RemoveAt(index);
+            i--;
+        }
+
+        // Orphan sources...
+        if (orphanSources && sourceValues.Count > 0)
+        {
+            for (int i = 0; i < sourceValues.Count; i++)
+            {
+                values.Add(sourceValues[i]);
+                entries.Add(sourceEntries[i]);
+            }
+        }
+
+        // Orphan targets...
+        if (orphanTargets && targetValues.Count > 0)
+        {
+            for (int i = 0; i < targetValues.Count; i++)
+            {
+                values.Add(targetValues[i]);
+                entries.Add(targetEntries[i]);
+            }
+        }
+
+        // Finishing...
+        return
+            values.Count == 0 ? null :
+            entries.Count == 0 ? new Record(values) : new Record(engine, values, entries);
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Used as a default value comparer.
+    /// </summary>
+    readonly struct ObjectComparer : IEqualityComparer
+    {
+        readonly public new bool Equals(object? x, object? y) => x.EqualsEx(y);
+        readonly public int GetHashCode([DisallowNull] object? obj) => 0;
+    }
+
+    /// <summary>
+    /// Returns the index of the first entry in the list with the given identifier.
+    /// </summary>
+    static int FindIndex(List<ISchemaEntry> entries, IIdentifier identifier)
+    {
+        var engine = identifier.Engine;
+        var sensitive = engine.CaseSensitiveNames;
+
+        return entries.FindIndex(x =>
+        {
+            var iname = identifier.Value ?? string.Empty;
+            var xname = x.Identifier.Value ?? string.Empty;
+            return string.Compare(iname, xname, !sensitive) == 0;
+        });
+    }
+
+    /// <summary>
+    /// Returns the index of the first entry in the list that matches the given identifier.
+    /// </summary>
+    static int FindMatch(List<ISchemaEntry> entries, IIdentifier identifier)
+    {
+        var value = identifier.Value;
+
+        for (int i = 0; i < entries.Count; i++)
+        {
+            var entry = entries[i];
+            var same = entry.Identifier.Match(value);
+            if (same) return i;
+        }
+
+        return -1;
+    }
+}
