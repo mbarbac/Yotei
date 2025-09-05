1cadd6999954da925aea06c3a9c74f5684c018c2
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-09-05 19:43:16 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Api/IMetadataTag.cs
@@ -0,0 +1,109 @@
+﻿namespace Yotei.ORM.Records;
+
+// ========================================================
+/// <summary>
+/// Represents the collection of names by which a given metadata entry can be known. Names are
+/// not-null and not-empy strings. This type is never an empty collection, and either the first
+/// name used or the one explicitly selected become the default one.
+/// </summary>
+[Cloneable]
+public partial interface IMetadataTag : IEnumerable<string>, IEquatable<IMetadataTag>
+{
+    /// <summary>
+    /// Returns a new builder based upon the contents of this instance.
+    /// </summary>
+    /// <returns></returns>
+    IBuilder CreateBuilder();
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Determines if the names are case sensitive or not.
+    /// </summary>
+    bool CaseSensitiveTags { get; }
+
+    /// <summary>
+    /// Gets or sets the default name among the ones in this instance.
+    /// <br/> Note that the setter somehow breaks immutability, but only in the sense that this
+    /// value can change at will, if the selected one already belongs to this instance, but cannot
+    /// add a new value.
+    /// </summary>
+    string Default { get; set; }
+
+    /// <summary>
+    /// The number of actual names carried by this instance.
+    /// </summary>
+    int Count { get; }
+
+    /// <summary>
+    /// Determines if this instance carries the given name, or not.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    bool Contains(string name);
+
+    /// <summary>
+    /// Determines if this instance carries any name from the given range, or not.
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    bool Contains(IEnumerable<string> range);
+
+    /// <summary>
+    /// Gets an array with the names in this collection.
+    /// </summary>
+    /// <returns></returns>
+    string[] ToArray();
+
+    /// <summary>
+    /// Gets a list with the names in this collection.
+    /// </summary>
+    /// <returns></returns>
+    List<string> ToList();
+
+    /// <summary>
+    /// Trims the internal structures of this collection.
+    /// </summary>
+    void Trim();
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Returns a new instance where the existing old name has been replaced by the new given one.
+    /// </summary>
+    /// <param name="oldname"></param>
+    /// <param name="newname"></param>
+    /// <returns></returns>
+    IMetadataTag Replace(string oldname, string newname);
+
+    /// <summary>
+    /// Returns a new instance where the given name has been added to the original collection.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    IMetadataTag Add(string name);
+
+    /// <summary>
+    /// Returns a new instance where the names of the given range has been added to the original
+    /// collection.
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    IMetadataTag AddRange(IEnumerable<string> range);
+
+    /// <summary>
+    /// Returns a new instance where the given name has been removed from the original collection.
+    /// <br/> If the name was not found, returns the original instance.
+    /// <br/> If the name was the only one in the collection, an exception is thrown.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    IMetadataTag Remove(string name);
+
+    /// <summary>
+    /// Returns a new instance where all the original contents, except the default one, have
+    /// been removed.
+    /// </summary>
+    /// <returns></returns>
+    IMetadataTag Clear();
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Code/MetadataTag.cs
@@ -0,0 +1,160 @@
+﻿namespace Yotei.ORM.Records.Code;
+
+// ========================================================
+/// <inheritdoc cref="IMetadataTag"/>
+[Cloneable<IMetadataTag>]
+public partial class MetadataTag : IMetadataTag
+{
+    protected virtual Builder Items { get; }
+
+    /// <summary>
+    /// Initializes a new instance with the given default name.
+    /// </summary>
+    /// <param name="sensitive"></param>
+    /// <param name="name"></param>
+    public MetadataTag(bool sensitive, string name) => Items = new(sensitive, name);
+
+    /// <summary>
+    /// Initializes a new instance with the names of the given name. The first name becomes
+    /// the default one. If the range is empty, then an exception is thrown.
+    /// </summary>
+    /// <param name="sensitive"></param>
+    /// <param name="range"></param>
+    public MetadataTag(
+        bool sensitive, IEnumerable<string> range) => Items = new(sensitive, range);
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="source"></param>
+    protected MetadataTag(MetadataTag source) => Items = new(source.CaseSensitiveTags, source);
+
+    /// <inheritdoc/>
+    public override string ToString() => Items.ToString();
+
+    /// <inheritdoc/>
+    public IEnumerator<string> GetEnumerator() => Items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    /// <inheritdoc/>
+    public virtual IMetadataTag.IBuilder CreateBuilder() => Items.Clone();
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public virtual bool Equals(IMetadataTag? other)
+    {
+        if (ReferenceEquals(this, other)) return true;
+        if (other is null) return false;
+
+        if (CaseSensitiveTags != other.CaseSensitiveTags) return false;
+        if (Count != other.Count) return false;
+
+        var targets = new List<string>(other);
+        while (targets.Count > 0)
+        {
+            var target = targets[0];
+            var found = Contains(target);
+
+            if (!found) return false;
+            targets.RemoveAt(0);
+        }
+
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => Equals(obj as IMetadataTag);
+
+    public static bool operator ==(MetadataTag? host, IMetadataTag? item)
+    {
+        if (host is null && item is null) return true;
+        if (host is null || item is null) return false;
+
+        return host.Equals(item);
+    }
+
+    public static bool operator !=(MetadataTag? host, IMetadataTag? item) => !(host == item);
+
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        var code = 0;
+        code = HashCode.Combine(code, CaseSensitiveTags);
+        foreach (var name in Items) code = HashCode.Combine(code, name);
+        return code;
+    }
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public bool CaseSensitiveTags => Items.CaseSensitiveTags;
+
+    /// <inheritdoc/>
+    public string Default
+    {
+        get => Items.Default;
+        set => Items.Default = value;
+    }
+
+    /// <inheritdoc/>
+    public int Count => Items.Count;
+
+    /// <inheritdoc/>
+    public bool Contains(string name) => Items.Contains(name);
+
+    /// <inheritdoc/>
+    public bool Contains(IEnumerable<string> range) => Items.Contains(range);
+
+    /// <inheritdoc/>
+    public string[] ToArray() => Items.ToArray();
+
+    /// <inheritdoc/>
+    public List<string> ToList() => Items.ToList();
+
+    /// <inheritdoc/>
+    public void Trim() => Items.Trim();
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public virtual IMetadataTag Replace(string oldname, string newname)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Replace(oldname, newname);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IMetadataTag Add(string name)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Add(name);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IMetadataTag AddRange(IEnumerable<string> range)
+    {
+        var builder = CreateBuilder();
+        var done = builder.AddRange(range);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IMetadataTag Remove(string name)
+    {
+        var builder = CreateBuilder();
+        var done = builder.Remove(name);
+        return done ? builder.CreateInstance() : this;
+    }
+
+    /// <inheritdoc/>
+    public virtual IMetadataTag Clear()
+    {
+        var builder = CreateBuilder();
+        var done = builder.Clear();
+        return done ? builder.CreateInstance() : this;
+    }
+}
