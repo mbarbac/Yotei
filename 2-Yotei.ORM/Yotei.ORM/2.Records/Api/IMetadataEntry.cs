eddb823045441ed1ed777304d799c3c93f150be1
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-09-07 11:26:39 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Api/IMetadataEntry.cs
@@ -0,0 +1,30 @@
+﻿namespace Yotei.ORM.Records;
+
+// ========================================================
+/// <summary>
+/// Represents a metadata entry, which carries a metadata name and a value.
+/// <br/> Instances of this type are intended to be immutable ones.
+/// </summary>
+public partial interface IMetadataEntry : IEquatable<IMetadataEntry>
+{
+    /// <summary>
+    /// Determines if this instance is equal to the other given one, using the given comparison
+    /// mode to compare their metadata names.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <param name="caseSensitiveNames"></param>
+    /// <returns></returns>
+    bool Equals(IMetadataEntry? other, bool caseSensitiveNames);
+
+    /// <summary>
+    /// The name by which this metadata entry is known.
+    /// </summary>
+    [With]
+    string Name { get; }
+
+    /// <summary>
+    /// The value carried by this entry.
+    /// </summary>
+    [With]
+    object? Value { get; }
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/2.Records/Code/MetadataEntry.cs
@@ -0,0 +1,78 @@
+﻿namespace Yotei.ORM.Records.Code;
+
+// ========================================================
+/// <inheritdoc cref="IMetadataEntry"/>
+[InheritWiths<IMetadataEntry>]
+public partial class MetadataEntry : IMetadataEntry
+{
+    /// <summary>
+    /// Initialiazes a new instance.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="value"></param>
+    public MetadataEntry(string name, object? value)
+    {
+        Name = name;
+        Value = value;
+    }
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="source"></param>
+    protected MetadataEntry(MetadataEntry source)
+    {
+        source.ThrowWhenNull();
+
+        Name = source.Name;
+        Value = source.Value;
+    }
+
+    /// <inheritdoc/>
+    public override string ToString() => $"[{Name}='{Value.Sketch()}']";
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public virtual bool Equals(IMetadataEntry? other, bool caseSensitiveNames)
+    {
+        if (ReferenceEquals(this, other)) return true;
+        if (other is null) return false;
+
+        if (string.Compare(Name, other.Name, !caseSensitiveNames) != 0) return false;
+        if (!Value.EqualsEx(other.Value)) return false;
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public virtual bool Equals(IMetadataEntry? other) => Equals(other, true);
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => Equals(obj as IMetadataEntry);
+
+    public static bool operator ==(MetadataEntry? host, IMetadataEntry? item)
+    {
+        if (host is null && item is null) return true;
+        if (host is null || item is null) return false;
+
+        return host.Equals(item);
+    }
+
+    public static bool operator !=(MetadataEntry? host, IMetadataEntry? item) => !(host == item);
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => HashCode.Combine(Name, Value);
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public string Name
+    {
+        get => _Name;
+        init => _Name = value.NotNullNotEmpty();
+    }
+    string _Name = default!;
+
+    /// <inheritdoc/>
+    public object? Value { get; init; }
+}
