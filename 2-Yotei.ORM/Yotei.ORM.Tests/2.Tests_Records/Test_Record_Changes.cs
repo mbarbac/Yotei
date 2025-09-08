b6efd263b94e54098aff3909bbc9dbf3197916f5
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-09-08 12:36:53 +0200

    xxx

--- 2-Yotei.ORM/Yotei.ORM.Tests/2.Tests_Records/Test_Record_Changes.cs
+++ 2-Yotei.ORM/Yotei.ORM.Tests/2.Tests_Records/Test_Record_Changes.cs
@@ -1,12 +1,3 @@
 ﻿namespace Yotei.ORM.Tests.Records;
 
 // ========================================================
-//[Enforced]
-//public static class Test_
-//{
-//    //[Enforced]
-//    [Fact]
-//    public static void Test_()
-//    {
-//    }
-//}
