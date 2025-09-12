354448e556ed25ed2db6f18b0180aa9935d922a6
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-09-12 19:24:11 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/2.RecordsGate/Api/IEnumerableCommand[T].cs
@@ -0,0 +1,43 @@
+﻿namespace Yotei.ORM.Records;
+
+// ========================================================
+/// <summary>
+/// Represents a records-oriented command that, when executed using its associated connection,
+/// enumerates the arbitrary results produced by that execution, using the given delegate to
+/// convert from the original records to the type of those results.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[Cloneable]
+[InheritWiths]
+public partial interface IEnumerableCommand<T>
+    : ICommand
+    , IEnumerable<T?>, IAsyncEnumerable<T?>
+{
+    /// <summary>
+    /// Returns an object that can execute this command and enumerate the records produced by that
+    /// execution, if any.
+    /// </summary>
+    /// <returns></returns>
+    new ICommandEnumerator<T> GetEnumerator();
+
+    /// <summary>
+    /// Returns an object that can execute this command and enumerate the records produced by that
+    /// execution, if any.
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    new ICommandEnumerator<T> GetAsyncEnumerator(CancellationToken token = default);
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// The original command this instance wraps.
+    /// </summary>
+    IEnumerableCommand Command { get; }
+
+    /// <summary>
+    /// The delegate to use to convert the original records produced by the execution of this
+    /// command to the type of the arbitrary results this instance is created for.
+    /// </summary>
+    Func<IRecord, ISchema?, T> Converter { get; }
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/2.RecordsGate/Code/EnumerableCommand[T].cs
@@ -0,0 +1,88 @@
+﻿namespace Yotei.ORM.Records.Code;
+
+// ========================================================
+/// <inheritdoc cref="IEnumerableCommand{T}"/>
+[Cloneable(ReturnType = typeof(IEnumerableCommand<>))]
+[InheritWiths(ReturnType = typeof(IEnumerableCommand<>))]
+public partial class EnumerableCommand<T> : IEnumerableCommand<T>
+{
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    /// <param name="command"></param>
+    /// <param name="converter"></param>
+    public EnumerableCommand(IEnumerableCommand command, Func<IRecord, ISchema?, T> converter)
+    {
+        Command = command.ThrowWhenNull();
+        Converter = converter.ThrowWhenNull();
+    }
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="source"></param>
+    protected EnumerableCommand(EnumerableCommand<T> source)
+    {
+        source.ThrowWhenNull();
+
+        Command = source.Command.Clone();
+        Converter = source.Converter;
+    }
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public ICommandEnumerator<T> GetEnumerator() => new CommandEnumerator<T>(this);
+    IEnumerator<T?> IEnumerable<T?>.GetEnumerator() => GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    /// <inheritdoc/>
+    public ICommandEnumerator<T> GetAsyncEnumerator(CancellationToken token = default) => new CommandEnumerator<T>(this, token);
+    IAsyncEnumerator<T?> IAsyncEnumerable<T?>.GetAsyncEnumerator(
+        CancellationToken token) => GetAsyncEnumerator(token);
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public IEnumerableCommand Command { get; }
+
+    /// <inheritdoc/>
+    public Func<IRecord, ISchema?, T> Converter { get; }
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public IConnection Connection
+    {
+        get => Command.Connection;
+        init => throw new InvalidOperationException("Use the original command.");
+    }
+
+    /// <inheritdoc/>
+    public Locale Locale
+    {
+        get => Command.Locale;
+        init => throw new InvalidOperationException("Use the original command.");
+    }
+
+    /// <inheritdoc/>
+    public bool IsValid => Command.IsValid;
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public ICommandInfo GetCommandInfo() => Command.GetCommandInfo();
+
+    /// <inheritdoc/>
+    public ICommandInfo GetCommandInfo(bool iterable) => Command.GetCommandInfo(iterable);
+
+    // ----------------------------------------------------
+
+    /// <inheritdoc/>
+    public IEnumerableCommand<T> Clear()
+    {
+        Command.Clear();
+        return this;
+    }
+    ICommand ICommand.Clear() => Clear();
+}
