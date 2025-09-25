f82cb933baa6d193780a8366f22cd346db54c3b3
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-09-25 20:35:19 +0200

    xxx

---
+++ 0-Development/Runner/MenuBuilder/MenuVersions.cs
@@ -0,0 +1,153 @@
+﻿using static Yotei.Tools.ConsoleEx;
+using static System.ConsoleColor;
+
+namespace Runner;
+
+// ========================================================
+public class MenuVersions : ConsoleMenuEntry
+{
+    /// <inheritdoc/>
+    public override string Header() => "Manage Package Versions";
+
+    /// <inheritdoc/>
+    public override void Execute()
+    {
+        var option = 0; do
+        {
+            var root = Program.GetSolutionDirectory();
+            var projects = Program.FindProjects(root);
+            projects = projects.Where(x => x.GetVersion(out _)).ToList();
+            projects.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            if (projects.Count == 0)
+            {
+                WriteLine(true);
+                Write(true, Red, "No projects found from: "); WriteLine(true, root);
+                return;
+            }
+
+            WriteLine(true);
+            WriteLine(true, Green, Program.FatSeparator);
+            WriteLine(true, Green, Header());
+
+            var menu = new ConsoleMenu { new("Exit") };
+            var items = projects.Select(x => new ConsoleMenuEntry(x.NameVersion)).ToList();
+            menu.AddRange(items);
+            option = menu.Run(true, Green, Program.Timeout, option);
+
+            if (option > 0)
+            {
+                var project = projects[option - 1];
+                Execute(project);
+            }
+        }
+        while (option > 0);
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Invoked to manage the version of the given project.
+    /// </summary>
+    /// <param name="project"></param>
+    static void Execute(Project project)
+    {
+        WriteLine(true);
+        WriteLine(true, Green, Program.SlimSeparator);
+        Write(true, Green, "Project: "); WriteLine(true, project.NameVersion);
+
+        if (!project.GetVersion(out var original))
+        {
+            WriteLine(true);
+            WriteLine(true, Red, "Cannot obtain the version of this project.");
+            return;
+        }
+        string str = original;
+
+        WriteLine(true);
+        Write(true, Green, "New version: ");
+        var done = EditLine(true, Program.Timeout, str, out str!);
+        if (!done || str is null || str.Length == 0) return;
+
+        // Executing under a backup umbrella...
+        var backups = new BuildBackups();
+        var saved = backups.Add(project);
+        saved.AddRange(project);
+
+        try
+        {
+            // Setting and saving the project's version...
+            var updated = new SemanticVersion(str);
+            var reduced = updated.PreRelease.IsEmpty ? updated : updated with { PreRelease = "" };
+            var enlarged = updated.PreRelease.IsEmpty ? updated with { PreRelease = "v0001" } : updated;
+
+            if (!project.SetVersion(updated))
+            {
+                WriteLine(true);
+                WriteLine(true, Red, "Cannot set the new version of this project.");
+                return;
+            }
+            project.SaveContents();
+
+            // Updating references in all solution's projects...
+            WriteLine(true);
+            WriteLine(true, Green, "Updating references... ");
+
+            var root = Program.GetSolutionDirectory();
+            var items = Program.FindProjects(root);
+            var first = true;
+
+            foreach (var item in items)
+            {
+                // Pre-saving state...
+                var temps = item.Select(x => new ProjectLine(x)).ToList();
+                var modified = false;
+
+                // Iterating through item's lines...
+                var ulines = item.Where(x => x.IsNuReference()).ToList();
+                foreach (var uline in ulines)
+                {
+                    if (!uline.GetNuName(out var uname)) continue;
+                    if (!uline.GetNuVersion(out var uversion)) continue;
+                    if (string.Compare(project.Name, uname, ignoreCase: true) != 0) continue;
+
+                    if (!modified) // First-time modification...
+                    {
+                        if (first) WriteLine(true);
+                        Write(true, Green, "Modifying: "); WriteLine(true, item.NameExtension);
+                        first = false;
+
+                        saved = backups.Add(item);
+                        saved.Clear();
+                        saved.AddRange(temps);
+                        modified = true;
+                    }
+
+                    done = uversion.PreRelease.IsEmpty
+                        ? uline.SetNuVersion(reduced)
+                        : uline.SetNuVersion(enlarged);
+
+                    if (!done) throw new Exception($"Cannot modify line: {uline}");
+                }
+
+                // If modified, saved the new contents...
+                if (modified) item.SaveContents();
+            }
+
+        }
+        catch (Exception ex) // Intercepting failures and reverting to original state...
+        {
+            WriteLine(true);
+            WriteLine(true, Red, "Exception intercepted:");
+            WriteLine(true, ex.ToDisplayString());
+
+            WriteLine(true);
+            WriteLine(true, Red, "Reverting to previous state...");
+            backups.Restore();
+
+            WriteLine(true);
+            Write(true, Green, "Press [Enter] to continue...");
+            Console.ReadLine();
+        }
+    }
+}
--- 0-Development/Runner/Program.cs
+++ 0-Development/Runner/Program.cs
@@ -42,6 +42,7 @@ internal class Program
                 new("Exit"),
                 new MenuTester(breakOnError: true),
                 new MenuArtifacts(),
+                new MenuVersions(),
                 new MenuBuilder(),
                 //new("Examples", () =>
                 //{
@@ -70,6 +71,48 @@ internal class Program
 
             dir = dir.Parent!;
             if (dir == null) Environment.FailFast("Cannot find solution's directory.");
+        }
+    }
+
+    /// <summary>
+    /// Returns a list with the projects found starting at the given root directory, and its
+    /// child, provided none if part of the given exclusion branch, if any.
+    /// </summary>
+    /// <param name="root"></param>
+    /// <param name="exclude"></param>
+    /// <param name="comparison"></param>
+    /// <returns></returns>
+    static internal List<Project> FindProjects(
+        string root,
+        string? exclude = null,
+        StringComparison comparison = StringComparison.OrdinalIgnoreCase)
+    {
+        root = root.NotNullNotEmpty(true);
+        exclude = exclude?.NotNullNotEmpty(true);
+
+        var list = new List<Project>();
+        Populate(root);
+        return list;
+
+        /// <summary>
+        /// Recursively populates the list of projects, starting at the given path.
+        /// </summary>
+        void Populate(string path)
+        {
+            if (path.Contains(".git", comparison)) return;
+            if (path.Contains(".vs", comparison)) return;
+            if (path.Contains("\\bin\\", comparison)) return;
+            if (path.Contains("\\obj\\", comparison)) return;
+            if (exclude is not null && path.StartsWith(exclude, comparison)) return;
+
+            var dir = new DirectoryInfo(path);
+            if (!dir.Exists) return;
+
+            var files = dir.GetFiles("*.csproj");
+            foreach (var file in files) list.Add(new(file.FullName));
+
+            var dirs = dir.GetDirectories();
+            foreach (var temp in dirs) Populate(temp.FullName);
         }
     }
 
@@ -111,5 +154,71 @@ internal class Program
             catch (ArgumentException) { }
             catch (FileNotFoundException) { }
         }
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Invoked to capture the desired increase mode, if any.
+    /// </summary>
+    /// <param name="mode"></param>
+    /// <returns></returns>
+    static internal bool CaptureIncreaseMode(ref IncreaseMode mode)
+    {
+        var option = mode switch
+        {
+            IncreaseMode.Major => 1,
+            IncreaseMode.Minor => 2,
+            IncreaseMode.Patch => 3,
+            IncreaseMode.PreRelease => 4,
+            _ => 0,
+        };
+        option = new ConsoleMenu
+        {
+            new("Exit"),
+            new("Major"), new("Minor"), new("Patch"), new("PreRelease"),
+        }
+        .Run(true, Green, Timeout, option);
+
+        switch (option)
+        {
+            case 1: mode = IncreaseMode.Major; return true;
+            case 2: mode = IncreaseMode.Minor; return true;
+            case 3: mode = IncreaseMode.Patch; return true;
+            case 4: mode = IncreaseMode.PreRelease; return true;
+        }
+        return false;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Invoked to capture the desired build mode, if any.
+    /// </summary>
+    /// <param name="mode"></param>
+    /// <returns></returns>
+    static internal bool CaptureBuildMode(ref BuildMode mode)
+    {
+        var option = mode switch
+        {
+            BuildMode.Debug => 1,
+            BuildMode.Local => 2,
+            BuildMode.Release => 3,
+            _ => 0,
+        };
+        option = new ConsoleMenu
+        {
+            new("Exit"),
+            new("Debug"), new("Local"), new("Release"),
+        }
+        .Run(true, Green, Timeout, option);
+
+        switch (option)
+        {
+            case 1: mode = BuildMode.Debug; return true;
+            case 2: mode = BuildMode.Local; return true;
+            case 3: mode = BuildMode.Release; return true;
+        }
+        return false;
     }
 }
