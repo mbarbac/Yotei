a7f2b689aa94edc87ce75fdc8bddeae82d9004fd
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-10-02 21:38:55 +0200

    xxx

---
+++ 0-Experimental/Experimental.Tests/__.cs
@@ -0,0 +1,14 @@
+﻿namespace Experimental.Tests;
+
+// =============================================================
+//[Enforced]
+//public static class Test_
+//{
+//    //[Enforced]
+//    [Fact]
+//    public static void Test_()
+//    {
+//    }
+//}
+
+public interface IFake { }
---
+++ 0-Experimental/Experimental/__.cs
@@ -0,0 +1,5 @@
+﻿namespace Experimental;
+
+// =============================================================
+
+public interface IFake { }
