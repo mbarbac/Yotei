d4c540909005f6811c13bc60d8358f26a5cc59cb
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-10-08 12:22:29 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM.Tests/__.cs
@@ -0,0 +1,14 @@
+﻿namespace Yotei.ORM.Tests;
+
+// ========================================================
+//[Enforced]
+//public static class Test_
+//{
+//    //[Enforced]
+//    [Fact]
+//    public static void Parse_()
+//    {
+//    }
+//}
+
+public interface IFake { }
---
+++ 4-Yotei.ORM.SqlServer/Yotei.ORM.SqlServer/__.cs
@@ -0,0 +1,5 @@
+﻿namespace Yotei.ORM.SqlServer;
+
+// ========================================================
+
+public interface IFake { }
