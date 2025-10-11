3b5143fca4129a95e9c3544a8d186f13b2237737
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-10-11 12:23:20 +0200

    xxx

---
+++ 1-Yotei.Tools.Generators/Yotei.Tools.CoreGenerator/Core/Candidates/Code/ErrorCandidate.cs
@@ -0,0 +1,23 @@
+﻿namespace Yotei.Tools.CoreGenerator;
+
+// ========================================================
+/// <summary>
+/// Represents an error condition while transforming a syntax node into a candidate for source
+/// code generation.
+/// </summary>
+internal sealed class ErrorCandidate : ICandidate
+{
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    /// <param name="diagnostic"></param>
+    public ErrorCandidate(Diagnostic diagnostic) => throw null;
+
+    /// <inheritdoc/>
+    public override string ToString() => Diagnostic.GetMessage();
+
+    /// <summary>
+    /// The diagnostic that describes the error situation.
+    /// </summary>
+    public Diagnostic Diagnostic { get; }
+}
---
+++ 1-Yotei.Tools.Generators/Yotei.Tools.CoreGenerator/Core/Candidates/ICandidate.cs
@@ -0,0 +1,7 @@
+﻿namespace Yotei.Tools.CoreGenerator;
+
+// ========================================================
+/// <summary>
+/// Represents a capture candidate for source code generation.
+/// </summary>
+internal interface ICandidate { }
