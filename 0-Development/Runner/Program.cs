a5a832d6d31c4831e3e22d0458c938752ad8be02
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-10-24 19:52:02 +0200

    xxx

--- 0-Development/Runner/Program.cs
+++ 0-Development/Runner/Program.cs
@@ -42,7 +42,7 @@ internal class Program
                 new("Exit"),
                 new MenuTester(breakOnError: true),
                 new MenuArtifacts(),
-                new("Manage Packages"),
+                new MenuPackages(),
             }
             .Run(MenuOptions);
         }
