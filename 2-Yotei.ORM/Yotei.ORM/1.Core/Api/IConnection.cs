eab9239fdc2bae52831c72692077967738c0eae6
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-10-30 20:32:04 +0100

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/1.Core/Api/IConnection.cs
@@ -0,0 +1,56 @@
+﻿namespace Yotei.ORM;
+
+// ========================================================
+/// <summary>
+/// Represents a connection with an underlying database.
+/// </summary>
+public interface IConnection
+{
+    /// <summary>
+    /// The descriptor of the database engine this instance connects to.
+    /// </summary>
+    IEngine Engine { get; }
+
+    /// <summary>
+    /// The number of times this instance tries to recover from a transient connection error
+    /// before throwing an exception. If the value of this property is cero, connection is only
+    /// tried once.
+    /// </summary>
+    int Retries { get; set; }
+
+    /// <summary>
+    /// The amount of time this instance waits before the next attempt to recover from a transient
+    /// connection error. If it is a zero-alike one, then there will be no wait.
+    /// </summary>
+    TimeSpan RetryInterval { get; set; }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Determines if this connection is active, or not.
+    /// </summary>
+    bool IsOpen { get; }
+
+    /// <summary>
+    /// Opens the connection with the underlying database.
+    /// </summary>
+    void Open();
+
+    /// <summary>
+    /// Opens the connection with the underlying database.
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    ValueTask OpenAsync(CancellationToken token = default);
+
+    /// <summary>
+    /// Closes the connection with the underlying database.
+    /// </summary>
+    void Close();
+
+    /// <summary>
+    /// Closes the connection with the underlying database.
+    /// </summary>
+    /// <returns></returns>
+    ValueTask CloseAsync();
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/1.Core/Api/IEngine.cs
@@ -0,0 +1,54 @@
+﻿namespace Yotei.ORM;
+
+// ========================================================
+/// <summary>
+/// Represents the descriptor of an underlying database engine.
+/// <br/> Instances of this type are intended to be immutable ones.
+/// </summary>
+public partial interface IEngine : IEquatable<IEngine>
+{
+    /// <summary>
+    /// Determines if this engine ignores the case of identifier-alike names and reserved keywords.
+    /// </summary>
+    [With] bool IgnoreCase { get; }
+
+    /// <summary>
+    /// The literal used by this engine to represents NULL values.
+    /// </summary>
+    [With] string NullValueLiteral { get; }
+
+    /// <summary>
+    /// Whether this engine treats the parameters of database commands as positional ones, or not,
+    /// and so treats them by name.
+    /// </summary>
+    [With] bool PositionalParameters { get; }
+
+    /// <summary>
+    /// The literal used by this engine to prepend the parameters of database commands.
+    /// </summary>
+    [With] string ParameterPrefix { get; }
+
+    /// <summary>
+    /// Determines if this engine provides native pagination capabilities, or rather they have to
+    /// be emulated by the framework.
+    /// </summary>
+    [With] bool NativePaging { get; }
+
+    /// <summary>
+    /// Determines if this engine wraps the identifier-alike elements in database commands with
+    /// the specified terminators, or not.
+    /// </summary>
+    [With] bool UseTerminators { get; }
+
+    /// <summary>
+    /// The left terminator used by this engine to wrap identifier-alike elements in database
+    /// commands, if terminators are used.
+    /// </summary>
+    [With] char LeftTerminator { get; }
+
+    /// <summary>
+    /// The right terminator used by this engine to wrap identifier-alike elements in database
+    /// commands, if terminators are used.
+    /// </summary>
+    [With] char RightTerminator { get; }
+}
