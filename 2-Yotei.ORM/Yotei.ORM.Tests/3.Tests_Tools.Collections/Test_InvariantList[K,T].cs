dae400fde3305e133d94160a865359279336bd6a
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-11-02 00:24:43 +0100

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM.Tests/3.Tests_Tools.Collections/Test_InvariantList[K,T].cs
@@ -0,0 +1,12 @@
+﻿namespace Yotei.ORM.Tests.Tools.Collections;
+
+// ========================================================
+//[Enforced]
+public class Test_InvariantList_KT
+{
+    //[Enforced]
+    [Fact]
+    public void Test()
+    {
+    }
+}
