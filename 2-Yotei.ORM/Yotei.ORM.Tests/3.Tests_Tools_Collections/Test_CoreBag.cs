757bc12426f7983efc82491a4170c86c3d4f87d2
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-11-26 18:55:06 +0100

    xxx

--- 2-Yotei.ORM/Yotei.ORM.Tests/3.Tests_Tools_Collections/Test_CoreBag.cs
+++ 2-Yotei.ORM/Yotei.ORM.Tests/3.Tests_Tools_Collections/Test_CoreBag.cs
@@ -1,31 +1,72 @@
 ﻿#pragma warning disable xUnit2017
+using System.Xml.Schema;
 
 namespace Yotei.ORM.Tests.Collections;
 
 // ========================================================
 //[Enforced]
-public static class Test_CoreBag
+public static partial class Test_CoreBag
 {
     public interface IElement { }
 
-    public class Element(string name) : IElement
+    public class Named(string name) : IElement
     {
         public string Name { get; set; } = name;
         public override string ToString() => Name ?? "-";
     }
+    readonly static Named xone = new("one");
+    readonly static Named xtwo = new("two");
+    readonly static Named xthree = new("three");
+    readonly static Named xfour = new("four");
 
-    readonly struct MyComparer(bool Sensitive) : IEqualityComparer<IElement>
+    [Cloneable(ReturnType = typeof(ICoreBag<IElement>))]
+    public partial class Chain : CoreBag<IElement>, IElement
     {
-        public bool Equals(IElement? x, IElement? y)
+        public Chain(bool sensitive) : base()
         {
-            if (x is null && y is null) return true;
-            if (x is null || y is null) return false;
-
-            return x is Element xnamed && y is Element ynamed
-                ? string.Compare(xnamed.Name, ynamed.Name, !Sensitive) == 0
-                : ReferenceEquals(x, y);
+            Sensitive = sensitive;
+            Validate = static x => x.ThrowWhenNull();
+            FlattenElements = true;
+            AreEqual = (x, y) => new MyComparer(Sensitive).Equals(x, y); // Creating new comparer on the stack for testing purposes only...
+            IsValidDuplicate = (x, y) => throw new DuplicateException();
         }
-        public int GetHashCode(IElement obj) => throw new NotImplementedException();
+
+        public Chain(bool sensitive, IEnumerable<IElement> range)
+            : this(sensitive)
+            => AddRange(range);
+
+        protected Chain(Chain source) : base(source) => Sensitive = source.Sensitive;
+
+        public bool Sensitive
+        {
+            get;
+            set
+            {
+                if (field == value) return;
+
+                if (Count == 0) field = value;
+                else
+                {
+                    var range = ToArray();
+                    Clear();
+                    field = value; AddRange(range);
+                }
+            }
+        }
+
+        readonly struct MyComparer(bool Sensitive) : IEqualityComparer<IElement>
+        {
+            public bool Equals(IElement? x, IElement? y)
+            {
+                if (x is null && y is null) return true;
+                if (x is null || y is null) return false;
+
+                return x is Named xnamed && y is Named ynamed
+                    ? string.Compare(xnamed.Name, ynamed.Name, !Sensitive) == 0
+                    : ReferenceEquals(x, y);
+            }
+            public int GetHashCode(IElement obj) => throw new NotImplementedException();
+        }
     }
 
     // ----------------------------------------------------
@@ -34,7 +75,7 @@ public static class Test_CoreBag
     [Fact]
     public static void Test_Create_Empty()
     {
-        var source = new CoreBag<IElement>();
+        var source = new Chain(false);
         Assert.Empty(source);
     }
 
@@ -42,68 +83,23 @@ public static class Test_CoreBag
     [Fact]
     public static void Test_Create_Range()
     {
-        var xone = new Element("one");
-        var xtwo = new Element("two");
-        var xthree = new Element("three");
-
-        var source = new CoreBag<IElement>([]);
+        var source = new Chain(false, []);
         Assert.Empty(source);
 
-        source = new CoreBag<IElement>([xone, xtwo, xthree]);
+        source = new Chain(false, [xone, xtwo, xthree]);
         Assert.Equal(3, source.Count);
-        Assert.True(source.Contains(xone));
+        Assert.True(source.Contains(xone)); Assert.True(source.Contains(new Named("ONE")));
         Assert.True(source.Contains(xtwo));
         Assert.True(source.Contains(xthree));
+        Assert.False(source.Contains(xfour));
 
-        source = new CoreBag<IElement>([xone, xone]);
-        Assert.Equal(2, source.Count);
-        Assert.True(source.Contains(xone));
-        Assert.True(source.Contains(xone));
+        source = new Chain(true, [xone, xtwo, xthree]);
+        Assert.True(source.Contains(xone)); Assert.False(source.Contains(new Named("ONE")));
 
-        try
-        {
-            source = new CoreBag<IElement>([xone, xone])
-            { IsValidDuplicate = (x, y) => throw new DuplicateException(), };
-            Assert.Fail();
-        }
-        catch (DuplicateException) { }
-    }
-
-    //[Enforced]
-    [Fact]
-    public static void Test_Create_Range_NoSensitive()
-    {
-        var xone = new Element("one");
-        var xtwo = new Element("two");
-        var xthree = new Element("three");
-
-        var source = new CoreBag<IElement>([])
-        {
-            AreEqual = (x, y) => new MyComparer(false).Equals(x, y),
-            IsValidDuplicate = (x, y) => throw new DuplicateException(),
-        };
-        Assert.Empty(source);
-
-        source = new CoreBag<IElement>([xone, xtwo, xthree])
-        {
-            IsValidDuplicate = (x, y) => throw new DuplicateException(),
-            AreEqual = (x, y) => new MyComparer(false).Equals(x, y),
-        };
-        Assert.Equal(3, source.Count);
-        Assert.True(source.Contains(xone));
-        Assert.True(source.Contains(xtwo));
-        Assert.True(source.Contains(xthree));
-
-        try
-        {
-            source = new CoreBag<IElement>([xone, new Element("ONE")])
-            {
-                AreEqual = (x, y) => new MyComparer(false).Equals(x, y),
-                IsValidDuplicate = (x, y) => throw new DuplicateException(),
-            };
-            Assert.Fail();
-        }
-        catch (DuplicateException) { }
+        try { source = new Chain(false, null!); Assert.Fail(); } catch (ArgumentNullException) { }
+        try { source = new Chain(false, [xone, null!]); Assert.Fail(); } catch (ArgumentNullException) { }
+        try { source = new Chain(false, [xone, xone]); Assert.Fail(); } catch (DuplicateException) { }
+        try { source = new Chain(false, [xone, new Named("ONE")]); Assert.Fail(); } catch (DuplicateException) { }
     }
 
     // ----------------------------------------------------
@@ -112,26 +108,20 @@ public static class Test_CoreBag
     [Fact]
     public static void Test_Clone()
     {
-        var xone = new Element("one");
-        var xtwo = new Element("two");
-        var xthree = new Element("three");
-        var source = new CoreBag<IElement>([xone, xtwo, xthree])
-        {
-            AreEqual = (x, y) => new MyComparer(false).Equals(x, y),
-            IsValidDuplicate = (x, y) => throw new DuplicateException(),
-        };
-
+        var source = new Chain(false, [xone, xtwo, xthree]);
         var target = source.Clone();
+
         Assert.NotSame(source, target);
         Assert.Equal(3, target.Count);
         Assert.True(target.Contains(xone));
         Assert.True(target.Contains(xtwo));
         Assert.True(target.Contains(xthree));
 
-        Assert.Same(source.Validate, ((CoreBag<IElement>)target).Validate);
-        Assert.Equal(source.FlattenElements, ((CoreBag<IElement>)target).FlattenElements);
-        Assert.Same(source.IsValidDuplicate, ((CoreBag<IElement>)target).IsValidDuplicate);
-        Assert.Same(source.AreEqual, ((CoreBag<IElement>)target).AreEqual);
+        Assert.Same(source.Validate, ((Chain)target).Validate);
+        Assert.Equal(source.FlattenElements, ((Chain)target).FlattenElements);
+        Assert.Same(source.AreEqual, ((Chain)target).AreEqual);
+        Assert.Same(source.IsValidDuplicate, ((Chain)target).IsValidDuplicate);
+        Assert.Equal(source.Sensitive, ((Chain)target).Sensitive);
     }
 
     // ----------------------------------------------------
@@ -140,37 +130,16 @@ public static class Test_CoreBag
     [Fact]
     public static void Test_Find()
     {
-        var xone = new Element("one");
-        var xtwo = new Element("two");
-        var xthree = new Element("three");
-        var source = new CoreBag<IElement>([xone, xtwo, xthree])
-        {
-            AreEqual = (x, y) => new MyComparer(false).Equals(x, y),
-            IsValidDuplicate = (x, y) => throw new DuplicateException(),
-        };
+        var source = new Chain(false, [xone, xtwo, xthree]);
 
         Assert.False(source.Find(x => x is null, out var item));
         Assert.Null(item);
 
-        Assert.True(source.Find(x => x is Element named && named.Name.Contains('e'), out item));
+        Assert.True(source.Find(x => x is Named named && named.Name.Contains('e'), out item));
         Assert.True(ReferenceEquals(xone, item) || ReferenceEquals(xthree, item));
-    }
-
-    //[Enforced]
-    [Fact]
-    public static void Test_FindAll()
-    {
-        var xone = new Element("one");
-        var xtwo = new Element("two");
-        var xthree = new Element("three");
-        var source = new CoreBag<IElement>([xone, xtwo, xthree])
-        {
-            AreEqual = (x, y) => new MyComparer(false).Equals(x, y),
-            IsValidDuplicate = (x, y) => throw new DuplicateException(),
-        };
 
         Assert.True(source.FindAll(
-            x => x is Element named && named.Name.Contains('e'),
+            x => x is Named named && named.Name.Contains('e'),
             out var items));
 
         Assert.Equal(2, items.Count);
@@ -184,11 +153,7 @@ public static class Test_CoreBag
     [Fact]
     public static void Test_Add()
     {
-        var xone = new Element("one");
-        var xtwo = new Element("two");
-        var xthree = new Element("three");
-        var source = new CoreBag<IElement>([xone, xtwo]);
-
+        var source = new Chain(false, [xone, xtwo]);
         var done = source.Add(xthree);
         Assert.Equal(1, done);
         Assert.Equal(3, source.Count);
@@ -196,13 +161,154 @@ public static class Test_CoreBag
         Assert.True(source.Contains(xtwo));
         Assert.True(source.Contains(xthree));
 
-        source = new CoreBag<IElement>([xone, xtwo])
-        {
-            Validate = (x) => x.ThrowWhenNull(),
-            AreEqual = (x, y) => new MyComparer(false).Equals(x, y),
-            IsValidDuplicate = (x, y) => throw new DuplicateException(),
-        };
-        try { source.Add(null!); Assert.Fail(); }
-        catch (ArgumentNullException) { }
+        try { source.Add(null!); Assert.Fail(); } catch (ArgumentNullException) { }
+        try { source.Add(xone); Assert.Fail(); } catch (DuplicateException) { }
+        try { source.Add(new Named("ONE")); Assert.Fail(); } catch (DuplicateException) { }
+
+        source = new Chain(false, [xone, xtwo]) { IsValidDuplicate = (_, _) => true };
+        done = source.Add(xone);
+        Assert.Equal(1, done);
+        Assert.True(source.Contains(xone));
+        Assert.True(source.Contains(xtwo));
+        Assert.True(source.FindAll(x => ReferenceEquals(x, xone), out var items));
+        Assert.Equal(2, items.Count);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Add_Flatten()
+    {
+        var source = new Chain(false, [xone, xtwo]);
+        var done = source.Add(new Chain(false));
+        Assert.Equal(0, done);
+
+        done = source.Add(new Chain(false, [xthree, xfour]));
+        Assert.Equal(2, done);
+        Assert.Equal(4, source.Count);
+        Assert.True(source.Contains(xone));
+        Assert.True(source.Contains(xtwo));
+        Assert.True(source.Contains(xthree));
+        Assert.True(source.Contains(xfour));
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Add_Range()
+    {
+        var source = new Chain(false, [xone, xtwo]);
+        var done = source.AddRange([]);
+        Assert.Equal(0, done);
+
+        done = source.AddRange([xthree, xfour]);
+        Assert.Equal(2, done);
+        Assert.Equal(4, source.Count);
+        Assert.True(source.Contains(xone));
+        Assert.True(source.Contains(xtwo));
+        Assert.True(source.Contains(xthree));
+        Assert.True(source.Contains(xfour));
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Remove_Item()
+    {
+        var source = new Chain(false, [xone, xtwo, xthree]);
+        var done = source.Remove(xfour);
+        Assert.Equal(0, done);
+
+        done = source.Remove(xtwo);
+        Assert.Equal(1, done);
+        Assert.Equal(2, source.Count);
+        Assert.True(source.Contains(xone));
+        Assert.True(source.Contains(xthree));
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Remove_Item_Flatten()
+    {
+        var source = new Chain(false, [xone, xtwo, xthree]);
+        var done = source.Remove(new Chain(false));
+        Assert.Equal(0, done);
+
+        done = source.Remove(new Chain(false, [xtwo, xfour]));
+        Assert.Equal(1, done);
+        Assert.Equal(2, source.Count);
+        Assert.True(source.Contains(xone));
+        Assert.True(source.Contains(xthree));
+
+        source = new Chain(false) { IsValidDuplicate = (_, _) => true };
+        source.AddRange([xone, xtwo, xone]);
+        done = source.Remove(new Chain(false, [xtwo, xfour]));
+        Assert.Equal(1, done);
+        Assert.Equal(2, source.Count);
+        Assert.True(source.Contains(xone));
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Remove_Item_All()
+    {
+        var source = new Chain(false) { IsValidDuplicate = (_, _) => true };
+        source.AddRange([xone, xtwo, xthree, xone]);
+
+        var done = source.RemoveAll(xone, out var items);
+        Assert.Equal(2, done);
+        Assert.Equal(2, source.Count);
+        Assert.True(source.Contains(xtwo));
+        Assert.True(source.Contains(xthree));
+        Assert.Equal(2, items.Count);
+        
+        Assert.Contains(xone, items);
+        Assert.Contains(xone, items);
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Remove_Predicate()
+    {
+        var source = new Chain(false, [xone, xtwo, xthree]);
+        var done = source.Remove(x => x is Named named && named.Name.Contains('e'), out var item);
+        
+        Assert.Equal(1, done);
+        Assert.Equal(2, source.Count);
+        Assert.True(source.Contains(xtwo));
+        Assert.True(ReferenceEquals(xone, item) || ReferenceEquals(xthree, item));
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Remove_Predicate_All()
+    {
+        var source = new Chain(false, [xone, xtwo, xthree]);
+        var done = source.RemoveAll(x => x is Named named && named.Name.Contains('e'), out var items);
+        
+        Assert.Equal(2, done);
+        Assert.Single(source);
+        Assert.True(source.Contains(xtwo));
+        Assert.Equal(2, items.Count);
+        
+        Assert.Contains(xone, items);
+        Assert.Contains(xthree, items);
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Clear()
+    {
+        var source = new Chain(false);
+        var done = source.Clear();
+        Assert.Equal(0, done);
+
+        source = new Chain(false, [xone, xtwo, xthree]);
+        done = source.Clear();
+        Assert.Equal(3, done);
+        Assert.Empty(source);
     }
 }
