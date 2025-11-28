26dbf4af5e233a53a2f1794f24385ace92dd6b80
Moises Barba Cebeira <mbarbac@hotmail.com>
2025-11-28 20:08:44 +0100

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM.Tests/3.Tests_Tools_Collections/Test_CoreBag.cs
@@ -0,0 +1,366 @@
+﻿#pragma warning disable xUnit2017
+
+namespace Yotei.ORM.Tests.Collections;
+
+// ========================================================
+//[Enforced]
+public static partial class Test_CoreBag
+{
+    public interface IElement { }
+
+    public class Named(string name) : IElement
+    {
+        public string Name { get; set; } = name;
+        public override string ToString() => Name ?? "-";
+    }
+    readonly static Named xone = new("one");
+    readonly static Named xtwo = new("two");
+    readonly static Named xthree = new("three");
+    readonly static Named xfour = new("four");
+
+    [Cloneable(ReturnType = typeof(ICoreBag<IElement>))]
+    public partial class Chain : CoreBag<IElement>, IElement
+    {
+        // Custom behaviors for testing purposes...
+        public Chain(bool sensitive) : base()
+        {
+            Sensitive = sensitive;
+            Validate = static x => x.ThrowWhenNull(); // Nulls not accepted...
+            FlattenElements = true; // Flatten input elements...
+            AreEqual = (x, y) => new MyComparer(Sensitive).Equals(x, y); // On-stack just for testing...
+            IsValidDuplicate = static (_, _) => throw new DuplicateException(); // No duplicates...
+        }
+
+        public Chain(bool sensitive, IEnumerable<IElement> range)
+            : this(sensitive)
+            => AddRange(range);
+
+        protected Chain(Chain source) : base(source) => Sensitive = source.Sensitive;
+
+        public bool Sensitive { get; }
+        readonly struct MyComparer(bool Sensitive) : IEqualityComparer<IElement>
+        {
+            public bool Equals(IElement? x, IElement? y)
+            {
+                if (x is null && y is null) return true;
+                if (x is null || y is null) return false;
+
+                return x is Named xnamed && y is Named ynamed
+                    ? string.Compare(xnamed.Name, ynamed.Name, !Sensitive) == 0
+                    : ReferenceEquals(x, y);
+            }
+            public int GetHashCode(IElement obj) => throw new NotImplementedException();
+        }
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Create_Empty()
+    {
+        var chain = new Chain(false);
+        Assert.Empty(chain);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Create_Range()
+    {
+        var chain = new Chain(false, []);
+        Assert.Empty(chain);
+
+        chain = new Chain(false, [xone, xtwo, xthree]);
+        Assert.Equal(3, chain.Count);
+        Assert.True(chain.Contains(xone));
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+
+        chain = new Chain(true, [xone, new Named("ONE")]);
+        Assert.Equal(2, chain.Count);
+        Assert.True(chain.Contains(xone));
+        Assert.True(chain.Find(x => x is Named named && named.Name == "ONE"));
+
+        try { _ = new Chain(false, null!); Assert.Fail(); } catch (ArgumentNullException) { }
+        try { _ = new Chain(false, [xone, null!]); Assert.Fail(); } catch (ArgumentNullException) { }
+        try { _ = new Chain(false, [xone, xone]); Assert.Fail(); } catch (DuplicateException) { }
+        try { _ = new Chain(false, [xone, new Named("ONE")]); Assert.Fail(); } catch (DuplicateException) { }
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Clone()
+    {
+        var source = new Chain(false, [xone, xtwo, xthree]);
+        var target = source.Clone();
+
+        Assert.Equal(3, target.Count);
+        Assert.True(target.Contains(xone));
+        Assert.True(target.Contains(xtwo));
+        Assert.True(target.Contains(xthree));
+
+        Assert.Same(source.Validate, ((Chain)target).Validate);
+        Assert.Equal(source.FlattenElements, ((Chain)target).FlattenElements);
+        Assert.Same(source.AreEqual, ((Chain)target).AreEqual);
+        Assert.Same(source.GetDuplicates, ((Chain)target).GetDuplicates);
+        Assert.Same(source.IsValidDuplicate, ((Chain)target).IsValidDuplicate);
+        Assert.Equal(source.Sensitive, ((Chain)target).Sensitive);
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Find_Values()
+    {
+        var chain = new CoreBag<int>([1, 2, 3, 4]);
+
+        Assert.False(chain.Find(x => x > 10, out var value));
+        Assert.True(chain.FindAll(x => x is > 1 and < 4, out var values));
+        Assert.Equal(2, values.Count);
+        Assert.Contains(2, values);
+        Assert.Contains(3, values);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Find_Predicate()
+    {
+        var chain = new Chain(false) { IsValidDuplicate = (_, _) => true };
+        chain.AddRange([xone, xtwo, xone, xthree]);
+
+        Assert.False(chain.Find(x => x is null, out var item));
+        Assert.Null(item);
+
+        Assert.True(chain.Find(x => x is Named named && named.Name.Contains('e'), out item));
+        Assert.True(ReferenceEquals(xone, item) || ReferenceEquals(xthree, item));
+
+        Assert.True(chain.FindAll(x => x is Named named && named.Name.Contains('e'), out var items));
+        Assert.Equal(3, items.Count);
+        Assert.Contains(xone, items);
+        Assert.Contains(xthree, items);
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Add()
+    {
+        var chain = new Chain(false, [xone, xtwo]);
+        var done = chain.Add(xthree);
+        Assert.Equal(1, done);
+        Assert.Equal(3, chain.Count);
+        Assert.True(chain.Contains(xone));
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+
+        try { chain.Add(null!); Assert.Fail(); } catch (ArgumentNullException) { }
+        try { chain.Add(xone); Assert.Fail(); } catch (DuplicateException) { }
+        try { chain.Add(new Named("ONE")); Assert.Fail(); } catch (DuplicateException) { }
+
+        chain = new Chain(false, [xone, xtwo]) { IsValidDuplicate = (_, _) => true };
+        done = chain.Add(xone);
+        Assert.Equal(1, done);
+        Assert.Equal(3, chain.Count);
+        Assert.True(chain.Contains(xone));
+        Assert.True(chain.Contains(xtwo));
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Add_Flatten()
+    {
+        var chain = new Chain(false, [xone, xtwo]);
+        var done = chain.Add(new Chain(false));
+        Assert.Equal(0, done);
+
+        done = chain.Add(new Chain(false, [xthree, xfour]));
+        Assert.Equal(2, done);
+        Assert.Equal(4, chain.Count);
+        Assert.True(chain.Contains(xone));
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+        Assert.True(chain.Contains(xfour));
+
+        try { chain.Add(new Chain(false, [new Named("ONE")])); Assert.Fail(); }
+        catch (DuplicateException) { }
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Add_Range()
+    {
+        var chain = new Chain(false, [xone, xtwo]);
+        var done = chain.AddRange([]);
+        Assert.Equal(0, done);
+
+        done = chain.AddRange([xthree, xfour]);
+        Assert.Equal(2, done);
+        Assert.Equal(4, chain.Count);
+        Assert.True(chain.Contains(xone));
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+        Assert.True(chain.Contains(xfour));
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Add_Range_Flatten()
+    {
+        var chain = new Chain(false, [xone, xtwo]);
+        var done = chain.AddRange([new Chain(false, [xthree, xfour]), new Named("FIVE")]);
+        Assert.Equal(3, done);
+        Assert.Equal(5, chain.Count);
+        Assert.True(chain.Contains(xone));
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+        Assert.True(chain.Contains(xfour));
+        Assert.True(chain.Find(x => x is Named named && named.Name == "FIVE"));
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Remove_Item()
+    {
+        var done = 0;
+        var chain = new Chain(false) { IsValidDuplicate = (_, _) => true };
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.Remove(xfour, out var items);
+        Assert.Equal(0, done);
+        Assert.Empty(items);
+        Assert.Equal(4, chain.Count);
+        Assert.True(chain.Contains(xone));
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.Remove(xone, out items);
+        Assert.Equal(1, done);
+        Assert.Single(items);
+        Assert.Contains(xone, items);
+        Assert.Equal(3, chain.Count);
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+        Assert.True(chain.Contains(xone));
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.Remove(new Named("ONE"), out items);
+        Assert.Equal(1, done);
+        Assert.Single(items);
+        Assert.Contains(xone, items);
+        Assert.Equal(3, chain.Count);
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+        Assert.True(chain.Contains(xone));
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_RemoveAll_Item()
+    {
+        var done = 0;
+        var chain = new Chain(false) { IsValidDuplicate = (_, _) => true };
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.RemoveAll(xone, out var items);
+        Assert.Equal(2, done);
+        Assert.Equal(2, items.Count);
+        Assert.Contains(xone, items);
+        Assert.Equal(2, chain.Count);
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.RemoveAll(new Named("ONE"), out items);
+        Assert.Equal(2, done);
+        Assert.Equal(2, items.Count);
+        Assert.Contains(xone, items);
+        Assert.Equal(2, chain.Count);
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Remove_Item_Flatten()
+    {
+        var done = 0;
+        var chain = new Chain(false) { IsValidDuplicate = (_, _) => true };
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.Remove(new Chain(false));
+        Assert.Equal(0, done);
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.Remove(new Chain(false, [xone, xthree]), out var items);
+        Assert.Equal(2, done);
+        Assert.Equal(2, items.Count);
+        Assert.Contains(xone, items);
+        Assert.Contains(xthree, items);
+        Assert.Equal(2, chain.Count);
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xone));
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.RemoveAll(new Chain(false, [xone, xthree]), out items);
+        Assert.Equal(3, done);
+        Assert.Equal(3, items.Count);
+        Assert.Contains(xone, items);
+        Assert.Contains(xthree, items);
+        Assert.Single(chain);
+        Assert.True(chain.Contains(xtwo));
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Remove_Predicate()
+    {
+        var done = 0;
+        var chain = new Chain(false) { IsValidDuplicate = (_, _) => true };
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.Remove(x => x is Named named && named.Name.Contains('e'), out var item);
+        Assert.Equal(1, done);
+        Assert.Same(xone, item);
+        Assert.Equal(3, chain.Count);
+        Assert.True(chain.Contains(xtwo));
+        Assert.True(chain.Contains(xthree));
+        Assert.True(chain.Contains(xone));
+
+        chain.Clear(); chain.AddRange([xone, xtwo, xthree, xone]);
+        done = chain.RemoveAll(x => x is Named named && named.Name.Contains('e'), out var items);
+        Assert.Equal(3, done);
+        Assert.Equal(3, items.Count);
+        Assert.Contains(xone, items);
+        Assert.Contains(xthree, items);
+        Assert.Same(xone, items[2]);
+        Assert.Single(chain);
+        Assert.True(chain.Contains(xtwo));
+    }
+
+    // ----------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Clear()
+    {
+        var chain = new Chain(false);
+        var done = chain.Clear();
+        Assert.Equal(0, done);
+
+        chain = new Chain(false, [xone, xtwo, xthree]);
+        done = chain.Clear();
+        Assert.Equal(3, done);
+        Assert.Empty(chain);
+    }
+}
--- 2-Yotei.ORM/Yotei.ORM/3.Tools/Collections/Api/ICoreBag.cs
+++ 2-Yotei.ORM/Yotei.ORM/3.Tools/Collections/Api/ICoreBag.cs
@@ -2,13 +2,11 @@
 
 // ========================================================
 /// <summary>
-/// Represents a list-alike collection of elements.
+/// Represents a bag-alike collection of elements with no ordering guarantees.
 /// </summary>
 /// <typeparam name="T"></typeparam>
 [Cloneable]
-public partial interface ICoreList<T>
-    : IList<T>, IReadOnlyList<T>, IList
-    , ICollection<T>, IReadOnlyCollection<T>, ICollection
+public partial interface ICoreBag<T> : ICollection<T>, IReadOnlyCollection<T>, ICollection
 {
     /// <summary>
     /// Invoked to return a validated element before using it in this collection.
@@ -47,65 +45,12 @@ public partial interface ICoreList<T>
     new int Count { get; }
 
     /// <summary>
-    /// Gets or sets the element at the given index.
-    /// <br/> Values may be intercepted as determined by this the rules in this instance.
-    /// </summary>
-    /// <param name="index"></param>
-    /// <returns></returns>
-    new T this[int index] { get; set; }
-
-    /// <summary>
     /// Determines if this collection contains at least one ocurrence of the given value, as
     /// determined by the rules in this instance.
     /// </summary>
     /// <param name="item"></param>
     /// <returns></returns>
     new bool Contains(T item);
-
-    /// <summary>
-    /// Returns the index of the first ocurrence of the given element, as determined by the rules
-    /// in this instance, or -1 if any.
-    /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
-    new int IndexOf(T item);
-
-    /// <summary>
-    /// Returns the index of the last ocurrence of the given element, as determined by the rules
-    /// in this instance, or -1 if any.
-    /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
-    int LastIndexOf(T item);
-
-    /// <summary>
-    /// Returns the indexes of all ocurrences of the given element, as determined by the rules in
-    /// this instance.
-    /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
-    List<int> IndexesOf(T item);
-
-    /// <summary>
-    /// Returns the index of the first element that matches the given predicate, or -1 if any.
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    int IndexOf(Predicate<T> predicate);
-
-    /// <summary>
-    /// Returns the index of the last element that matches the given predicate, or -1 if any.
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    int LastIndexOf(Predicate<T> predicate);
-
-    /// <summary>
-    /// Returns the indexes of all the ocurrences of elements that match the given predicate.
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    List<int> IndexesOf(Predicate<T> predicate);
 
     /// <summary>
     /// Tries to find the first ocurrence of an element that matches the given predicate and, if
@@ -124,24 +69,6 @@ public partial interface ICoreList<T>
     /// <param name="found"></param>
     /// <returns></returns>
     bool Find(Predicate<T> predicate, out T item);
-
-    /// <summary>
-    /// Tries to find the last ocurrence of an element that matches the given predicate and, if
-    /// so and if the given delegate is not null, invokes that delegate with the found element.
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <param name="found"></param>
-    /// <returns></returns>
-    bool FindLast(Predicate<T> predicate, Action<T>? found = null);
-
-    /// <summary>
-    /// Tries to find the last ocurrence of an element that matches the given predicate and, if
-    /// so, returns the found element in the out argument.
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <param name="found"></param>
-    /// <returns></returns>
-    bool FindLast(Predicate<T> predicate, out T item);
 
     /// <summary>
     /// Tries to find all the ocurrences of elements that match the given predicate and, if so
@@ -196,90 +123,7 @@ public partial interface ICoreList<T>
     /// <returns></returns>
     int AddRange(IEnumerable<T> range);
 
-    /// <summary>
-    /// Inserts into this collection the given element at the given index.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="item"></param>
-    /// <returns></returns>
-    new int Insert(int index, T item);
-
-    /// <summary>
-    /// Inserts into this collection the elements of the given range starting at the given index.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="range"></param>
-    /// <returns></returns>
-    int InsertRange(int index, IEnumerable<T> range);
-
     // ----------------------------------------------------
-
-    /// <summary>
-    /// Replaces the element at the given index with the given one. If it is an empty collection
-    /// of elements, and if this instance flattens input elements, then no replacement is made.
-    /// If the given delegate is not null, it is invoked with the removed element.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="item"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    int Replace(int index, T item, Action<T>? removed = null);
-
-    /// <summary>
-    /// Replaces the element at the given index with the given one. If it is an empty collection
-    /// of elements, and if this instance flattens input elements, then no replacement is made.
-    /// Returns the removed element, if any, in the out argument.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="item"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    int Replace(int index, T item, out T removed);
-
-    /// <summary>
-    /// Removes from this collection the element at the given index. If the given delegate is not
-    /// null, it is invoked with the removed element.
-    /// <br/> Returns whether the element has been removed or not.
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    bool RemoveAt(int index, Action<T>? removed = null);
-
-    /// <summary>
-    /// Removes from this collection the element at the given index. Returns the removed element,
-    /// if any, in the out argument.
-    /// <br/> Returns whether the element has been removed or not.
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    bool RemoveAt(int index, out T removed);
-
-    /// <summary>
-    /// Removes from this collection the given number of elements, starting at the given index.
-    /// If the given delegate is not null, it is invoked with the removed elements.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="count"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    int RemoveRange(int index, int count, Action<T>? removed = null);
-
-    /// <summary>
-    /// Removes from this collection the given number of elements, starting at the given index.
-    /// Returns the removed elements, if any, in the out argument.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="count"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    int RemoveRange(int index, int count, out List<T> removed);
 
     /// <summary>
     /// Removes from this collection the first ocurrence of the given element, as determined by
@@ -302,28 +146,6 @@ public partial interface ICoreList<T>
     /// <param name="items"></param>
     /// <returns></returns>
     int Remove(T item, out List<T> items);
-
-    /// <summary>
-    /// Removes from this collection the last ocurrence of the given element, as determined by
-    /// the rules in this instance. If it is an empty collection of elements, and if this instance
-    /// flattens input elements, then each element of that collection is removed instead. If the
-    /// given delegate is not null, it is invoked with the removed elements.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="item"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    int RemoveLast(T item, Action<T>? removed = null);
-
-    /// <summary>
-    /// Removes from this collection the last ocurrence of the given element, as determined by
-    /// the rules in this instance. If so, returns the removed elements in the out argument.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="item"></param>
-    /// <param name="items"></param>
-    /// <returns></returns>
-    int RemoveLast(T item, out List<T> items);
 
     /// <summary>
     /// Removes from this collection all the ocurrences of the given element, as determined by the
@@ -366,25 +188,6 @@ public partial interface ICoreList<T>
     /// <param name="removed"></param>
     /// <returns></returns>
     int Remove(Predicate<T> predicate, out T removed);
-
-    /// <summary>
-    /// Removes from this collection the last element that matches the given predicate. If the
-    /// given delegate is not null, it is invoked with the removed element.
-    /// <br/> Returns the number of changes made.
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    int RemoveLast(Predicate<T> predicate, Action<T>? removed = null);
-
-    /// <summary>
-    /// Removes from this collection the last element that matches the given predicate. Returns
-    /// the removed element, if any, in the out argument.
-    /// </summary>
-    /// <param name="predicate"></param>
-    /// <param name="removed"></param>
-    /// <returns></returns>
-    int RemoveLast(Predicate<T> predicate, out T removed);
 
     /// <summary>
     /// Removes from this collection all the elements that match the given predicate. If the
