e6d934e5af0220c107e14e559ec95373038c6fa8
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-01-08 19:54:10 +0100

    xxx

--- 1-Yotei.Tools/Yotei.Tools/SystemEx2/IsNullable.cs
+++ 1-Yotei.Tools/Yotei.Tools/SystemEx2/IsNullable.cs
@@ -4,11 +4,12 @@
 /// <summary>
 /// Used to decorate types for which nullability information shall be persisted.
 /// <br/> Nullable annotations on reference or on generic types are just syntactic sugar used by
-/// the compiler but, in most circumstances, not persisted in metadata or in custom attributes, or
-/// just prevented by the compiler. By contrast, the compiler translates annotated value types into
+/// the compiler but in general they are not persisted in metadata or in custom attributes, or the
+/// compiler just prevents them. By contrast, the compiler translates annotated value types into
 /// <see cref="Nullable{T}"/> instances.
 /// <br/> The <see cref="IsNullable{T}"/> and <see cref="IsNullableAttribute"/> types can be used
 /// as far-from-perfect workarounds when there is the need to specify this nullability information.
+/// Both approaches have drawbacks that must be considered carefully.
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public class IsNullable<T> { }
