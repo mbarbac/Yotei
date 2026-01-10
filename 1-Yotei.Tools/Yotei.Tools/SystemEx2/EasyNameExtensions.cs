0b516b0ffcf6addcc81adebe830a4c9a09daf8eb
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-01-10 12:25:03 +0100

    xxx

---
+++ 1-Yotei.Tools/Yotei.Tools/SystemEx2/EasyNameExtensions.cs
@@ -0,0 +1,133 @@
+﻿namespace Yotei.Tools;
+
+// ========================================================
+public static class EasyNameExtensions
+{
+    /// <summary>
+    /// Obtains a C#-alike representation of the given element, using default options.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public static string EasyName(
+        this Type source) => source.EasyName(EasyNameOptions.Default);
+
+    /// <summary>
+    /// Obtains a C#-alike representation of the given element, using the given options.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static string EasyName(this Type source, EasyNameOptions options)
+    {
+        source.ThrowWhenNull();
+        options.ThrowWhenNull();
+
+        var types = source.GetGenericArguments();
+        return EasyName(source, types, options);
+    }
+
+    /// <summary>
+    /// Invoked to obtain a C#-alike representation of the given type element, once the details
+    /// of its closed generic arguments have been captured, to prevent loosing them in recursive
+    /// calls.
+    /// </summary>
+    static string EasyName(this Type source, Type[] types, EasyNameOptions options)
+    {
+        var isgen = source.IsGenericParameter || source.IsGenericTypeParameter || source.IsGenericMethodParameter;
+        var host = source.DeclaringType;
+        var args = source.GetGenericArguments();
+        var used = host is null ? 0 : host.GetGenericArguments().Length;
+        var need = args.Length - used;
+        string? str;
+
+        // Shortcut hide name...
+        if (options.HideTypeName) return string.Empty;
+
+        // Shortcut wrapped nullability...
+        if (options.TypeNullableStyle is not EasyNullableStyle.KeepWrappers && (
+            (need == 1 && source.Name.StartsWith("Nullable`1")) ||
+            (need == 1 && source.Name.StartsWith("IsNullable`1"))))
+        {
+            var type = types[used];
+            str = type.EasyName(options);
+
+            if (options.TypeNullableStyle is EasyNullableStyle.UseAnnotations &&
+                !str.EndsWith('?'))
+                str += '?';
+
+            return str;
+        }
+
+        // Processing...
+        var sb = new StringBuilder();
+
+        // Variance mask...
+        if (options.UseTypeVariance && source.IsGenericParameter) // or otherwise an error!
+        {
+            var gpa = source.GenericParameterAttributes;
+            var variance = gpa & GenericParameterAttributes.VarianceMask;
+
+            if ((variance & GenericParameterAttributes.Covariant) != 0) sb.Append("out ");
+            if ((variance & GenericParameterAttributes.Contravariant) != 0) sb.Append("in ");
+        }
+
+        // Namespace...
+        if (options.UseTypeNamespace && host == null && !isgen)
+        {
+            str = source.Namespace;
+            if (str is not null && str.Length > 0) { sb.Append(str); sb.Append('.'); }
+        }
+
+        // Host...
+        if (options.UseHost && host != null && !isgen)
+        {
+            str = host.EasyName(types, options);
+            if (str is not null && str.Length > 0) { sb.Append(str); sb.Append('.'); }
+        }
+
+        // Name (if 'name&' is a 'ref' type, such is handled elsewhere)...
+        str = source.Name;
+        var index = str.IndexOf('`'); if (index >= 0) str = str[..index];
+        if (str.Length > 0 && str[^1] == '&') str = str[..^1];
+        sb.Append(str);
+
+        // Generic arguments...
+        if (options.UseGenericArguments && need > 0)
+        {
+            sb.Append('<'); for (var i = 0; i < need; i++)
+            {
+                var arg = types[used + i];
+                str = arg.EasyName(options);
+                if (i > 0) sb.Append(str.Length > 0 ? ", " : ",");
+                sb.Append(str);
+            }
+            sb.Append('>');
+        }
+
+        // Nullability...
+        if (options.TypeNullableStyle is not EasyNullableStyle.None &&
+            sb.Length > 0 &&
+            sb[^1] != '?')
+        {
+            if (options.TypeNullableStyle is EasyNullableStyle.KeepWrappers && (
+                source.Name.StartsWith("Nullable`1") ||
+                source.Name.StartsWith("IsNullable`1"))) goto END;
+
+            // Nullable attribute (API)...
+            var nullability = source.GetCustomAttribute<NullableAttribute>();
+            if (nullability != null && nullability.NullableFlags.Length > 0)
+            {
+                var flag = nullability.NullableFlags[0];
+                if (flag == 2) { sb.Append('?'); goto END; }
+            }
+
+            // IsNullable attribute...
+            if (source.GetCustomAttribute<IsNullableAttribute>() != null)
+            { sb.Append('?'); goto END; }
+        }
+
+        // Finishing...
+        END:
+        return sb.ToString();
+    }
+}
---
+++ 1-Yotei.Tools/Yotei.Tools/SystemEx2/EasyNameOptions.cs
@@ -0,0 +1,157 @@
+﻿namespace Yotei.Tools;
+
+// ========================================================
+/// <summary>
+/// Determines the style to use with the nullable annotations of type elements.
+/// </summary>
+public enum EasyNullableStyle { None, UseAnnotations, KeepWrappers }
+
+// ========================================================
+/// <summary>
+/// Determines the style to use with the names of constructor elements.
+/// </summary>
+public enum EasyConstructorStyle { HostName, HostAndTechName }
+
+// ========================================================
+/// <summary>
+/// Determines the style to use with the names of indexed properties.
+/// </summary>
+public enum EasyIndexedStyle { ThisName, TechName }
+
+// ========================================================
+/// <summary>
+/// Provides options for the 'EasyName' family of methods.
+/// </summary>
+public record EasyNameOptions
+{
+    /// <summary>
+    /// Determines if the variance specifiers ('in', 'out') of the generic type element are
+    /// used, or not.
+    /// </summary>
+    public bool UseTypeVariance { get; init; }
+
+    /// <summary>
+    /// Determines if the namespace of the type element is used, or not.
+    /// </summary>
+    public bool UseTypeNamespace { get; init; }
+
+    /// <summary>
+    /// Determines if the host type of the element is used, or not.
+    /// <br/> This setting is not used with constructor elements.
+    /// </summary>
+    public bool UseHost { get; init; }
+
+    /// <summary>
+    /// Determines if the name of the type element is hidden, or not.
+    /// <br/> If this setting is enabled, it shortcuts all other ones.
+    /// </summary>
+    public bool HideTypeName { get; init; }
+
+    /// <summary>
+    /// Determines the style to use with the nullable annotations of the type elements.
+    /// </summary>
+    public EasyNullableStyle TypeNullableStyle { get; init; }
+
+    /// <summary>
+    /// Determines the style to use with the names of constructor elements.
+    /// </summary>
+    public EasyConstructorStyle ConstructorStyle { get; init; }
+
+    /// <summary>
+    /// Determines the style to use with the names of indexed property elements.
+    /// </summary>
+    public EasyIndexedStyle IndexedStyle { get; init; }
+
+    /// <summary>
+    /// Determines if the generic arguments of the element are used, or not.
+    /// </summary>
+    public bool UseGenericArguments { get; init; }
+
+    /// <summary>
+    /// Determines if the return type of the element is used, or not.
+    /// <br/> This setting is only used with regular methods, properties and fields.
+    /// </summary>
+    public bool UseReturnType { get; init; }
+
+    /// <summary>
+    /// Determines if the member element brackets (if any) are used, even if no usage of member
+    /// arguments is requested.
+    /// </summary>
+    public bool UseBrackets { get; init; }
+
+    /// <summary>
+    /// Determines if the modifiers ('ref', 'out', etc.) of the member arguments are used, or not.
+    /// </summary>
+    public bool UseArgumentModifiers { get; init; }
+
+    /// <summary>
+    /// Determines if the types of the member arguments are used, or not.
+    /// </summary>
+    public bool UseArgumentTypes { get; init; }
+
+    /// <summary>
+    /// Determines the nullable style to use with the member argument types.
+    /// </summary>
+    public EasyNullableStyle ArgumentNullableStyle { get; init; }
+
+    /// <summary>
+    /// Determines if the names of the member arguments are used, or not.
+    /// </summary>
+    public bool UseArgumentNames { get; init; }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// A shared instance with default settings.
+    /// </summary>
+    public static EasyNameOptions Default { get; } = new EasyNameOptions(Mode.Default);
+
+    /// <summary>
+    /// A shared instance with full settings.
+    /// </summary>
+    public static EasyNameOptions Full { get; } = new EasyNameOptions(Mode.Full);
+
+    /// <summary>
+    /// Initializes a new empty instance.
+    /// </summary>
+    public EasyNameOptions() : this(Mode.Empty) { }
+
+    /// <summary>
+    /// Determines the mode used to initialize this instance.
+    /// </summary>
+    enum Mode { Empty, Default, Full };
+    EasyNameOptions(Mode mode)
+    {
+        switch (mode)
+        {
+            case Mode.Empty:
+                break;
+
+            default:
+            case Mode.Default:
+                TypeNullableStyle = EasyNullableStyle.UseAnnotations;
+                ConstructorStyle = EasyConstructorStyle.HostName;
+                IndexedStyle = EasyIndexedStyle.ThisName;
+                UseGenericArguments = true;
+                UseArgumentModifiers = true;
+                UseArgumentTypes = true;
+                ArgumentNullableStyle = EasyNullableStyle.UseAnnotations;
+                break;
+
+            case Mode.Full:
+                UseTypeVariance = true;
+                UseTypeNamespace = true;
+                UseHost = true;
+                TypeNullableStyle = EasyNullableStyle.KeepWrappers;
+                ConstructorStyle = EasyConstructorStyle.HostAndTechName;
+                IndexedStyle = EasyIndexedStyle.TechName;
+                UseGenericArguments = true;
+                UseReturnType = true;
+                UseArgumentModifiers = true;
+                UseArgumentTypes = true;
+                ArgumentNullableStyle = EasyNullableStyle.KeepWrappers;
+                UseArgumentNames = true;
+                break;
+        }
+    }
+}
