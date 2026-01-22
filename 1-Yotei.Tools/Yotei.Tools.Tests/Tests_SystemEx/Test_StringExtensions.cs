9067b5853605f6b9ac4e34292c68ee8bde178e85
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-01-22 13:05:20 +0100

    xxx

---
+++ 1-Yotei.Tools/Yotei.Tools.Tests/Tests_SystemEx/Test_StringExtensions.cs
@@ -0,0 +1,196 @@
+﻿namespace Yotei.Tools.Tests;
+
+// ========================================================
+//[Enforced]
+public static class Test_StringExtensions
+{
+    // [Enforced]
+    [Fact]
+    public static void Test_NullWhenEmpty_With_Trim()
+    {
+        string? source = null;
+        string? result = source.NullWhenEmpty(true);
+        Assert.Null(result);
+
+        source = "";
+        result = source.NullWhenEmpty(true);
+        Assert.Null(result);
+
+        source = " ";
+        result = source.NullWhenEmpty(true);
+        Assert.Null(result);
+    }
+
+    // [Enforced]
+    [Fact]
+    public static void Test_NullWhenEmpty_No_Trim()
+    {
+        string? source = null;
+        string? result = source.NullWhenEmpty(false);
+        Assert.Null(result);
+
+        source = "";
+        result = source.NullWhenEmpty(false);
+        Assert.Null(result);
+
+        source = " ";
+        result = source.NullWhenEmpty(false); // spaces not removed!
+        Assert.NotNull(result);
+        Assert.Equal(" ", result);
+    }
+
+    // ----------------------------------------------------
+
+    // [Enforced]
+    [Fact]
+    public static void Test_NotNullNotEmpty_With_Trim()
+    {
+        string? source = null;
+        try { source.NotNullNotEmpty(true); Assert.Fail(); }
+        catch (ArgumentNullException) { }
+
+        source = string.Empty;
+        try { source.NotNullNotEmpty(true); Assert.Fail(); }
+        catch (EmptyException) { }
+
+        source = " ";
+        try { source.NotNullNotEmpty(true); Assert.Fail(); }
+        catch (EmptyException) { }
+    }
+
+    // [Enforced]
+    [Fact]
+    public static void Test_NotNullNotEmpty_No_Trim()
+    {
+        string? source = null;
+        try { source.NotNullNotEmpty(false); Assert.Fail(); }
+        catch (ArgumentNullException) { }
+
+        source = string.Empty;
+        try { source.NotNullNotEmpty(false); Assert.Fail(); }
+        catch (EmptyException) { }
+
+        source = " ";
+        Assert.Equal(" ", source.NotNullNotEmpty(false));
+    }
+
+    // --------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_RemoveDiacritics()
+    {
+        string value, temp;
+
+        value = "é"; temp = value.RemoveDiacritics(); Assert.Equal("e", temp);
+        value = "É"; temp = value.RemoveDiacritics(); Assert.Equal("E", temp);
+        value = "ñ"; temp = value.RemoveDiacritics(); Assert.Equal("n", temp);
+        value = "Ñ"; temp = value.RemoveDiacritics(); Assert.Equal("N", temp);
+        value = "ç"; temp = value.RemoveDiacritics(); Assert.Equal("c", temp);
+        value = "Ç"; temp = value.RemoveDiacritics(); Assert.Equal("C", temp);
+        value = "ü"; temp = value.RemoveDiacritics(); Assert.Equal("u", temp);
+        value = "Ü"; temp = value.RemoveDiacritics(); Assert.Equal("U", temp);
+        value = "ô"; temp = value.RemoveDiacritics(); Assert.Equal("o", temp);
+        value = "Ô"; temp = value.RemoveDiacritics(); Assert.Equal("O", temp);
+        value = "à"; temp = value.RemoveDiacritics(); Assert.Equal("a", temp);
+        value = "À"; temp = value.RemoveDiacritics(); Assert.Equal("A", temp);
+    }
+
+    // --------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Wrap_Empty_Source()
+    {
+        var head = '[';
+        var tail = ']';
+
+        var source = (string?)null;
+        var result = source.Wrap(head, tail, true);
+        Assert.Null(result);
+
+        source = string.Empty;
+        result = source.Wrap(head, tail, true);
+        Assert.Equal(string.Empty, result);
+
+        source = " ";
+        result = source.Wrap(head, tail, true);
+        Assert.Equal(string.Empty, result);
+
+        result = source.Wrap(head, tail, false);
+        Assert.Equal("[ ]", result);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_Wrap_Regular_Source()
+    {
+        var head = '[';
+        var tail = ']';
+
+        var source = "   ";
+        var result = source.Wrap(head, tail, true);
+        Assert.Equal(string.Empty, result);
+
+        source = "   ";
+        result = source.Wrap(head, tail, false);
+        Assert.Equal("[   ]", result);
+
+        source = " Hello World ";
+        result = source.Wrap(head, tail, true);
+        Assert.Equal("[Hello World]", result);
+    }
+
+    // --------------------------------------------------
+
+    //[Enforced]
+    [Fact]
+    public static void Test_UnWrap_Empty_Source()
+    {
+        var head = '[';
+        var tail = ']';
+
+        var source = (string?)null;
+        var result = source!.Unwrap(head, tail, true);
+        Assert.Null(result);
+
+        source = string.Empty;
+        result = source.Unwrap(head, tail, true);
+        Assert.Equal(string.Empty, result);
+
+        source = " [  ] ";
+        result = source.Unwrap(head, tail, true);
+        Assert.Equal(string.Empty, result);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_UnWrap_One_Character_Source()
+    {
+        var ch = '\'';
+
+        var source = "'";
+        var result = source.Unwrap(ch, true);
+        Assert.Equal("'", result);
+
+        source = " ' ";
+        result = source.Unwrap(ch, true);
+        Assert.Equal("'", result);
+    }
+
+    //[Enforced]
+    [Fact]
+    public static void Test_UnWrap_Regular_Source()
+    {
+        var head = '[';
+        var tail = ']';
+
+        var source = "[ Whatever ]";
+        var result = source.Unwrap(head, tail, true);
+        Assert.Equal("Whatever", result);
+
+        source = " [ [ Whatever ] ] ";
+        result = source.Unwrap(head, tail, true);
+        Assert.Equal("Whatever", result);
+    }
+}
---
+++ 1-Yotei.Tools/Yotei.Tools/SystemEx/StringExtensions.cs
@@ -0,0 +1,158 @@
+﻿namespace Yotei.Tools;
+
+// Note: cannot use extension block when 'CallerArgumentExpression' is used.
+
+// ========================================================
+public static class StringExtensions
+{
+    /// <summary>
+    /// Returns <see langword="null"/> if the source string is null, or if it is empty after trimmed,
+    /// if such is requested. Otherwise, returns either the original string or the trimmed one.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="trim"></param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string? NullWhenEmpty(this string? source, bool trim) => trim switch
+    {
+        true => string.IsNullOrWhiteSpace(source) ? null : source.Trim(),
+        false => string.IsNullOrEmpty(source) ? null : source,
+    };
+
+    /// <summary>
+    /// Trims the source string, if requested, and then throws a <see cref="ArgumentNullException"/>
+    /// if the result is <see langword="null"/>, or a <see cref="EmptyException"/> is it is an empty
+    /// one. Otherwise, returns either the original string or the trimmed one.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="trim"></param>
+    /// <param name="description"></param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string NotNullNotEmpty(
+        this string? source, bool trim,
+        [CallerArgumentExpression(nameof(source))] string? description = null)
+    {
+        if (string.IsNullOrWhiteSpace(description)) description = nameof(source);
+
+        ArgumentNullException.ThrowIfNull(source, description);
+
+        source = source.NullWhenEmpty(trim);
+        if (source is null || source.Length == 0) throw new EmptyException(description);
+        return source;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Returns a new string where its diacritics marks have been removed.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public static string RemoveDiacritics(this string source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        source = source!.Normalize(NormalizationForm.FormD);
+        var sb = new StringBuilder();
+        foreach (var c in source)
+        {
+            var uc = CharUnicodeInfo.GetUnicodeCategory(c);
+            if (uc != UnicodeCategory.NonSpacingMark) sb.Append(c);
+        }
+        return sb.ToString();
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Wraps the given source string with the given head and tail characters, provided it is not
+    /// null and not empty. If trimming is requested, the source string is trimmed before validating
+    /// it.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="head"></param>
+    /// <param name="tail"></param>
+    /// <param name="trim"></param>
+    /// <returns></returns>
+    public static string? Wrap(this string? source, char head, char tail, bool trim)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(head);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(tail);
+
+        if (source is not null)
+        {
+            var span = source.AsSpan();
+            if (trim) span = span.Trim();
+
+            if (span.Length > 0 &&
+                span[0] != head &&
+                span[^1] != tail)
+                return $"{head}{span}{tail}";
+
+            if (span.Length != source.Length) return span.ToString();
+        }
+        return source;
+    }
+
+    /// <summary>
+    /// Wraps the given source string with the given character, provided it is not null and not empty.
+    /// If trimming is requested, the source string is trimmed before validating it.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="c"></param>
+    /// <param name="trim"></param>
+    /// <returns></returns>
+    public static string? Wrap(this string? source, char c, bool trim) => source.Wrap(c, c, trim);
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Removes from the given source string the given head and tail characters, provided they both
+    /// appear paired. If trimming is requested, the source string is trimmed before validating it.
+    /// If recursive is requested, then the method is iterated while needed.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="head"></param>
+    /// <param name="tail"></param>
+    /// <param name="trim"></param>
+    /// <param name="recursive"></param>
+    /// <returns></returns>
+    public static string? Unwrap(
+        this string? source, char head, char tail, bool trim, bool recursive = true)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(head);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(tail);
+
+        if (source is not null)
+        {
+            var span = source.AsSpan();
+
+            while (true)
+            {
+                if (trim) span = span.Trim();
+                if (span.Length < 2) break;
+                if (span[0] != head || span[^1] != tail) break;
+
+                span = span[1..^1];
+                if (!recursive) break;
+            }
+            if (span.Length != source.Length) return span.ToString();
+        }
+        return source;
+    }
+
+    /// <summary>
+    /// Removes from the given source string the given character, provided it appears both as the
+    /// head and tail character of that string. If trimming is requested, the source string is trimmed
+    /// before validating it. If recursive is requested, then the method is iterated while needed.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="c"></param>
+    /// <param name="trim"></param>
+    /// <param name="recursive"></param>
+    /// <returns></returns>
+    public static string? Unwrap(
+        this string? source, char c, bool trim, bool recursive = true)
+        => source.Unwrap(c, c, trim, recursive);
+}
