cc668427e4d0193437d33bb58ff69e60c962143f
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-01-29 19:37:43 +0100

    xxx

---
+++ 1-Yotei.Tools.Generators/00-CoreGenerator/Yotei.Tools.CoreGenerator/RoslynEx/SymbolExtensions.cs
@@ -0,0 +1,94 @@
+﻿namespace Yotei.Tools.CoreGenerator;
+
+// ========================================================
+internal static class SymbolExtensions
+{
+    extension(ISymbol symbol)
+    {
+        /// <summary>
+        /// Gets the symbol of the containing type or of the nearest enclosing namespace, or
+        /// null if any.
+        /// </summary>
+        public ISymbol? ContainingTypeOrNamespace =>
+            (ISymbol?)symbol.ContainingType ??
+            (ISymbol?)symbol.ContainingNamespace;
+
+        // ------------------------------------------------
+
+        /// <summary>
+        /// Gets the first known syntax location where this symbol is found. This property might
+        /// return '<c>null</c>' if this symbol was declared in metadata or if it was implicitly
+        /// declared.
+        /// </summary>
+        public Location? FirstLocation =>
+            symbol.Locations.FirstOrDefault() ??
+            symbol.GetSyntaxNodes().FirstOrDefault()?.GetLocation();
+
+        // ------------------------------------------------
+
+        /// <summary>
+        /// Gets the collection of syntax nodes where this symbol was declared in source code. If
+        /// the symbol was declared in metadata or it was implicitly declared, then it may return
+        /// an empty array.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<SyntaxNode> GetSyntaxNodes() => symbol
+            .DeclaringSyntaxReferences
+            .Select(x => x.GetSyntax());
+
+        // ------------------------------------------------
+
+        /// <summary>
+        /// Determines if this symbol has any attribute whose class is the given one.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public bool HasAttributes(Type type) => symbol.GetAttributes(type).Any();
+
+        /// <summary>
+        /// Returns the collection of attributes that decorates the given symbol and whose class is
+        /// the given one.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public IEnumerable<AttributeData> GetAttributes(Type type)
+        {
+            ArgumentNullException.ThrowIfNull(symbol);
+            ArgumentNullException.ThrowIfNull(type);
+
+            foreach (var at in symbol.GetAttributes())
+            {
+                if (at.AttributeClass is not null &&
+                    at.AttributeClass.Match(type)) yield return at;
+            }
+        }
+
+        /// <summary>
+        /// Determines if this symbol has any attribute whose class is among the given ones.
+        /// </summary>
+        /// <param name="types"></param>
+        /// <returns></returns>
+        public bool HasAttributes(IEnumerable<Type> types) => symbol.GetAttributes(types).Any();
+
+        /// <summary>
+        /// Returns the collection of attributes that decorates the given symbol and whose class is
+        /// any of the given ones.
+        /// </summary>
+        /// <param name="types"></param>
+        /// <returns></returns>
+        public IEnumerable<AttributeData> GetAttributes(IEnumerable<Type> types)
+        {
+            ArgumentNullException.ThrowIfNull(symbol);
+            ArgumentNullException.ThrowIfNull(types);
+
+            foreach (var at in symbol.GetAttributes())
+            {
+                foreach (var type in types)
+                {
+                    if (at.AttributeClass is not null &&
+                    at.AttributeClass.Match(type)) yield return at;
+                }
+            }
+        }
+    }
+}
