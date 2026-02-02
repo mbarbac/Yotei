0d9eaf77e7cf6ac2e4aa86bfed2fb3fe26a85050
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-02-02 17:56:38 +0100

    xxx

--- 1-Yotei.Tools.Generators/00-CoreGenerator/Yotei.Tools.CoreGenerator/Core/Candidates/ICandidate.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Yotei.Tools.CoreGenerator;
-
-// ========================================================
-/// <summary>
-/// Represents a source code generation candidate, or an error condition.
-/// </summary>
-internal interface ICandidate { }
