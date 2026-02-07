f1f7e54f250557d6e234bc9d3a06da850dad174d
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-02-07 12:25:15 +0100

    xxx

---
+++ 1-Yotei.Tools.Generators/00-CoreGenerator/Yotei.Tools.CoreGenerator/TreeEx/_EasyConstant.cs
@@ -0,0 +1,28 @@
+﻿namespace Yotei.Tools.CoreGenerator;
+
+// ========================================================
+internal record EasyConstant
+{
+}
+
+// ========================================================
+internal static partial class EasyNameExtensions
+{
+    /// <summary>
+    /// Returns a display string for the given element using default options.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public static string EasyName(this TypedConstant source) => source.EasyName(new());
+
+    /// <summary>
+    /// Returns a display string for the given element using the given options.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static string EasyName(this TypedConstant source, EasyConstant options)
+    {
+        throw null;
+    }
+}
---
+++ 1-Yotei.Tools.Generators/00-CoreGenerator/Yotei.Tools.CoreGenerator/TreeEx/_EasyField.cs
@@ -0,0 +1,28 @@
+﻿namespace Yotei.Tools.CoreGenerator;
+
+// ========================================================
+internal record EasyField
+{
+}
+
+// ========================================================
+internal static partial class EasyNameExtensions
+{
+    /// <summary>
+    /// Returns a display string for the given element using default options.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public static string EasyName(this IFieldSymbol source) => source.EasyName(new());
+
+    /// <summary>
+    /// Returns a display string for the given element using the given options.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static string EasyName(this IFieldSymbol source, EasyField options)
+    {
+        throw null;
+    }
+}
