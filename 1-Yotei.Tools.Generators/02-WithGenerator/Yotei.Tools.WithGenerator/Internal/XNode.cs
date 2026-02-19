889b4972ee4790e757f4c3fe86dc3aba5ad50790
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-02-19 19:27:53 +0100

    xxx

--- 1-Yotei.Tools.Generators/02-WithGenerator/Yotei.Tools.WithGenerator/Internal/XNode.cs
+++ 1-Yotei.Tools.Generators/02-WithGenerator/Yotei.Tools.WithGenerator/Internal/XNode.cs
@@ -3,9 +3,314 @@
 // ========================================================
 internal static class XNode
 {
+    /// <summary>
+    /// Tries to find the value of the 'UseVirtual' setting at the given type, if it is not null,
+    /// or at the types in the given chains, in that order. Uses either the attribute found at a
+    /// decorated member, or the one decorating a suitable host. If found, returns its value in
+    /// the out argument along with in which member (or null if any) and host type that attribute
+    /// was found.
+    /// </summary>
+    public static bool FindUseVirtual<T>(
+        string membername,
+        out bool value,
+        out T? member,
+        out INamedTypeSymbol? host,
+        INamedTypeSymbol? type,
+        params IEnumerable<INamedTypeSymbol>[] chains) where T : ISymbol
+    {
+        var found = Finder.Find((type, out info) =>
+        {
+            // Members take priority...
+            if (FindDecoratedMember<T>(membername, out var member, out var attr, type) &&
+                attr.HasUseVirtual(out var value))
+            {
+                info = new(value, member, type);
+                return true;
+            }
 
+            // Otherwise at type's level...
+            if (type.HasInheritsWithAttribute(out var at) &&
+                at.HasUseVirtual(out value))
+            {
+                info = new(value, default, type);
+                return true;
+            }
 
+            // Try next...
+            info = null!;
+            return false;
+        },
+        out FindUseVirtualInfo<T> info, type, chains);
 
+        value = found && info.Value;
+        member = found ? info.Member : default;
+        host = found ? info.Host : default;
+        return found;
+    }
+    record FindUseVirtualInfo<T>(bool Value, T? Member, INamedTypeSymbol? Host) where T : ISymbol;
 
-    
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Tries to find the value of the 'ReturnType' setting at the given type, if it is not null,
+    /// or at the types in the given chains, in that order. Uses either the attribute found at a
+    /// decorated member, or the one decorating a suitable host. If found, returns its value in
+    /// the out argument along with in which member (or null if any) and host type that attribute
+    /// was found.
+    /// </summary>
+    public static bool FindReturnType<T>(
+        string membername,
+        [NotNullWhen(true)] out INamedTypeSymbol? value,
+        out bool nullable,
+        out INamedTypeSymbol? host,
+        INamedTypeSymbol? type,
+        params IEnumerable<INamedTypeSymbol>[] chains) where T : ISymbol
+    {
+        var found = Finder.Find((type, out info) =>
+        {
+            // Members take priority...
+            if (FindDecoratedMember<T>(membername, out var member, out var attr, type) &&
+                attr.HasReturnType(out var value, out var nullable))
+            {
+                info = new(value, nullable, type);
+                return true;
+            }
+
+            // Otherwise at type's level...
+            if (type.HasInheritsWithAttribute(out var at) &&
+                at.HasReturnType(out value, out nullable))
+            {
+                info = new(value, nullable, type);
+                return true;
+            }
+
+            // Try next...
+            info = null!;
+            return false;
+        },
+        out FindReturnTypeInfo<T> info, type, chains);
+
+        value = found ? info.Value : default;
+        nullable = found && info.Nullable;
+        host = found ? info.Host : default;
+        return found;
+    }
+    record FindReturnTypeInfo<T>(
+        INamedTypeSymbol Value, bool Nullable, INamedTypeSymbol? Host)
+        where T : ISymbol;
+
+    /// <summary>
+    /// Returns the appropriate options to emit the return type, based upon whether it is the
+    /// same as the given host one, or not.
+    /// </summary>
+    public static EasyTypeSymbol ReturnOptions(INamedTypeSymbol hosttype, INamedTypeSymbol rtype)
+    {
+        return SymbolEqualityComparer.Default.Equals(hosttype, rtype)
+            ? EasyTypeSymbol.Default
+            : EasyTypeSymbol.Full with { NullableStyle = IsNullableStyle.None };
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Tries to find a property or field member with the given name in the given type, if it is
+    /// not null, or in the types in the given chains, in that order.
+    /// </summary>
+    public static bool FindMember<T>(
+        string membername,
+        [NotNullWhen(true)] out T? value,
+        INamedTypeSymbol? type,
+        params IEnumerable<INamedTypeSymbol>[] chains) where T : ISymbol
+    {
+        return Finder.Find((type, out value) =>
+        {
+            value = type.GetMembers().OfType<T>().FirstOrDefault(x => x.Name == membername);
+            return value != null;
+        },
+        out value, type, chains);
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Tries to find a decorated property or field member with the given name in the given type,
+    /// if it is not null, or in the types in the given chains, in that order.
+    /// </summary>
+    public static bool FindDecoratedMember<T>(
+        string membername,
+        [NotNullWhen(true)] out T? value,
+        [NotNullWhen(true)] out AttributeData? attr,
+        INamedTypeSymbol? type,
+        params IEnumerable<INamedTypeSymbol>[] chains) where T : ISymbol
+    {
+        var found = Finder.Find((type, out info) =>
+        {
+            var member = type.GetMembers().OfType<T>().FirstOrDefault(x => x.Name == membername);
+            if (member != null)
+            {
+                var ats = member.GetAttributes([typeof(WithAttribute), typeof(WithAttribute<>)]).ToArray();
+                if (ats.Length == 1)
+                {
+                    info = new(member, ats[0]);
+                    return true;
+                }
+            }
+
+            info = null!;
+            return false;
+        },
+        out FindMemberInfo<T> info, type, chains);
+
+        value = found ? info.Member : default;
+        attr = found ? info.Attribute : default;
+        return found;
+    }
+    record FindMemberInfo<T>(T Member, AttributeData Attribute) where T : ISymbol;
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Tries to find a method with the given name and parameter type in the given type, if it is
+    /// not null, or in the types in the given chains, in that order.
+    /// </summary>
+    public static bool FindMethod(
+        string methodname,
+        ITypeSymbol paramtype,
+        [NotNullWhen(true)] out IMethodSymbol? value,
+        INamedTypeSymbol? type,
+        params IEnumerable<INamedTypeSymbol>[] chains)
+    {
+        methodname = methodname.NotNullNotEmpty(trim: true);
+        ArgumentNullException.ThrowIfNull(paramtype);
+
+        return Finder.Find((type, out value) =>
+        {
+            value = type.GetMembers().OfType<IMethodSymbol>().FirstOrDefault(x =>
+                x.Name == methodname &&
+                x.Parameters.Length == 1 &&
+                paramtype.IsAssignableTo(x.Parameters[0].Type));
+
+            return value != null;
+        },
+        out value, type, chains);
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Determines if the given attribute carries a 'UseVirtual' value and, if so, returns it in
+    /// the out arguments.
+    /// </summary>
+    public static bool HasUseVirtual(
+        this AttributeData at,
+        out bool value)
+    {
+        if (at.FindNamedArgument("UseVirtual", out var arg))
+        {
+            if (!arg.Value.IsNull && arg.Value.Value is bool temp)
+            {
+                value = temp;
+                return true;
+            }
+        }
+        value = false;
+        return false;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Determines if the given attribute carries a 'ReturnType' value and, if so, returns it and
+    /// whether it is a nullable one in the out arguments.
+    /// </summary>
+    public static bool HasReturnType(
+        this AttributeData at,
+        [NotNullWhen(true)] out INamedTypeSymbol? value, out bool nullable)
+    {
+        ArgumentNullException.ThrowIfNull(at);
+        ArgumentNullException.ThrowIfNull(at.AttributeClass);
+
+        // Generic attribute...
+        if (at.AttributeClass.Arity == 1)
+        {
+            value = (INamedTypeSymbol)at.AttributeClass.TypeArguments[0];
+            value = value.UnwrapNullable(out nullable);
+            return true;
+        }
+
+        // Not-generic attribute...
+        else if (at.AttributeClass.Arity == 0)
+        {
+            if (at.FindNamedArgument("ReturnType", out var arg))
+            {
+                if (!arg.Value.IsNull && arg.Value.Value is INamedTypeSymbol temp)
+                {
+                    value = temp.UnwrapNullable(out nullable);
+                    return true;
+                }
+            }
+        }
+
+        // Default...
+        value = null;
+        nullable = false;
+        return false;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Determines if the given member is decorated with a <see cref="WithAttribute"/> and, if so,
+    /// returns it in the out argument.
+    /// </summary>
+    public static bool HasWithAttribute<T>(
+        this T member,
+        [NotNullWhen(true)] out AttributeData? value) where T : ISymbol
+    {
+        value = member.GetAttributes([typeof(WithAttribute)]).FirstOrDefault();
+        if (value is not null) return true;
+
+        value = member.GetAttributes([typeof(WithAttribute<>)]).FirstOrDefault();
+        if (value is not null) return true;
+
+        return false;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Determines if the given type is decorated with a <see cref="InheritsWithAttribute"/> and,
+    /// if so, returns it in the out argument.
+    /// </summary>
+    public static bool HasInheritsWithAttribute(
+        this INamedTypeSymbol type,
+        [NotNullWhen(true)] out AttributeData? value)
+    {
+        value = type.GetAttributes([typeof(InheritsWithAttribute)]).FirstOrDefault();
+        return value != null;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// The version of the generator for documentation purposes.
+    /// </summary>
+    static string VersionDoc => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+    /// <summary>
+    /// A string with the 'GeneratedCode' attribute of the generator for documentation purposes.
+    /// </summary>
+    static string AttributeDoc => $$"""
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("{{nameof(WithGenerator)}}", "{{VersionDoc}}")]
+        """;
+
+    /// <summary>
+    /// Emits documentation.
+    /// </summary>
+    public static void EmitDocumentation(ISymbol symbol, CodeBuilder cb) => cb.AppendLine($$"""
+        /// <summary>
+        /// Emulates the '<see langword="with"/>' keyword for the '{{symbol.Name}}' member.
+        /// </summary>
+        {{AttributeDoc}}
+        """);
 }
--- 1-Yotei.Tools.Generators/02-WithGenerator/Yotei.Tools.WithGenerator/Internal/XPropertyNode.cs
+++ 1-Yotei.Tools.Generators/02-WithGenerator/Yotei.Tools.WithGenerator/Internal/XPropertyNode.cs
@@ -1,4 +1,6 @@
-﻿namespace Yotei.Tools.WithGenerator;
+﻿using Microsoft.CodeAnalysis.Operations;
+
+namespace Yotei.Tools.WithGenerator;
 
 // ========================================================
 /// <summary>
@@ -10,7 +12,6 @@ internal class XPropertyNode : PropertyNode
     /// <inheritdoc/>
     /// </summary>
     /// <param name="symbol"></param>
-    [SuppressMessage("", "IDE0290")]
     public XPropertyNode(IPropertySymbol symbol) : base(symbol)
     {
         MethodName = $"With{Symbol.Name}";
@@ -22,8 +23,9 @@ internal class XPropertyNode : PropertyNode
     /// </summary>
     public bool IsInherited { get; init; }
 
-    string MethodName = default!;
-    string ArgumentName = default!;
+    readonly string MethodName = default!;
+    readonly string ArgumentName = default!;
+    AttributeData? Attribute = default!;
 
     // ----------------------------------------------------
 
@@ -41,7 +43,42 @@ internal class XPropertyNode : PropertyNode
         if (Symbol.IsIndexer) { Symbol.ReportError(TreeError.IndexerNotSupported, context); r = false; }
         if (!Symbol.HasGetter) { Symbol.ReportError(TreeError.NoGetter, context); r = false; }
         if (!Symbol.HasSetter && !host.IsInterface) { Symbol.ReportError(TreeError.NoSetter, context); r = false; }
+
+        if (!IsInherited) // Member captured per-se...
+        {
+            if (Attributes.Count == 0) { Symbol.ReportError(TreeError.NoAttributes, context); r = false; }
+            if (Attributes.Count > 1) { Symbol.ReportError(TreeError.TooManyAttributes, context); r = false; }
+            Attribute = Attributes[0];
+        }
+        else // Captured because its host inherits members...
+        {
+            var ats = host.GetAttributes([typeof(InheritsWithAttribute), typeof(InheritsWithAttribute<>)]).ToList();
+            if (Attributes.Count == 0) { host.ReportError(TreeError.NoAttributes, context); r = false; }
+            if (ats.Count > 1) { host.ReportError(TreeError.TooManyAttributes, context); r = false; }
+            Attribute = ats[0];
+        }
+
         return r;
+    }
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="cb"></param>
+    public override void Emit(SourceProductionContext context, CodeBuilder cb)
+    {
+        var host = ParentNode!.Symbol;
+
+        // Intercepting explicitly implemented...
+        if (XNode.FindMethod(MethodName, Symbol.Type, out _, host)) return;
+
+        // Dispatching...
+        if (host.IsInterface) EmitHostInterface(context, cb);
+        else if (host.IsAbstract) EmitHostAbstract(context, cb);
+        else EmitHostRegular(context, cb);
     }
 
     // ----------------------------------------------------
@@ -49,9 +86,56 @@ internal class XPropertyNode : PropertyNode
     /// <summary>
     /// Invoked when the host is an interface.
     /// </summary>
+    [SuppressMessage("", "IDE0060")]
     void EmitHostInterface(SourceProductionContext context, CodeBuilder cb)
     {
-        throw null;
+        var host = ParentNode!.Symbol;
+        var type = XNode.FindReturnType<IPropertySymbol>(
+            Symbol.Name,
+            out var xtype, out var xnullable, out _, host, host.AllInterfaces) ? xtype : host;
+
+        var roptions = XNode.ReturnOptions(host, type);
+        var rtype = type.EasyName(roptions);
+        var rnull = xnullable ? "?" : string.Empty;
+        var modifiers = GetInterfaceModifiers();
+        var argtype = Symbol.Type.EasyName(EasyTypeSymbol.Full);
+
+        XNode.EmitDocumentation(Symbol, cb);
+        cb.AppendLine($"{modifiers}{rtype}{rnull}");
+        cb.AppendLine($"{MethodName}({argtype} {ArgumentName});");
+    }
+
+    /// <summary>
+    /// Invoked to obtain the appropriate method modifiers.
+    /// <br/> If not null, ends with a space separator.
+    /// </summary>
+    string? GetInterfaceModifiers()
+    {
+        var host = ParentNode!.Symbol;
+
+        var found = Finder.Find((type, out value) =>
+        {
+            // Member in the base type...
+            if (XNode.FindDecoratedMember<IPropertySymbol>(Symbol.Name, out _, out var at, type))
+            {
+                value = "new ";
+                return true;
+            }
+
+            // Method in the base type...
+            if (XNode.FindMethod(MethodName, Symbol.Type, out _, type))
+            {
+                value = "new ";
+                return true;
+            }
+
+            // Try next...
+            value = default;
+            return false;
+        },
+        out string? value, null, host.AllInterfaces);
+
+        return found ? value : null;
     }
 
     // ----------------------------------------------------
@@ -61,7 +145,64 @@ internal class XPropertyNode : PropertyNode
     /// </summary>
     void EmitHostAbstract(SourceProductionContext context, CodeBuilder cb)
     {
-        throw null;
+        var host = ParentNode!.Symbol;
+        var type = XNode.FindReturnType<IPropertySymbol>(
+            Symbol.Name,
+            out var xtype, out var xnullable, out _, host, host.AllInterfaces) ? xtype : host;
+
+        var roptions = XNode.ReturnOptions(host, type);
+        var rtype = type.EasyName(roptions);
+        var rnull = xnullable ? "?" : string.Empty;
+        var modifiers = GetAbstractModifiers();
+        var argtype = Symbol.Type.EasyName(EasyTypeSymbol.Full);
+
+        XNode.EmitDocumentation(Symbol, cb);
+        cb.AppendLine($"{modifiers}{rtype}{rnull}");
+        cb.AppendLine($"{MethodName}({argtype} {ArgumentName});");
+
+        EmitExplicitInterfaces(context, cb);
+    }
+
+    /// <summary>
+    /// Invoked to obtain the appropriate method modifiers.
+    /// <br/> If not null, ends with a space separator.
+    /// </summary>
+    string? GetAbstractModifiers()
+    {
+        var host = ParentNode!.Symbol;
+
+        var found = Finder.Find((type, out value) =>
+        {
+            // Method in the base type...
+            while (XNode.FindMethod(MethodName, Symbol.Type, out var method, type))
+            {
+                var dec = method.DeclaredAccessibility; if (dec == Accessibility.Private) break;
+                var str = dec.ToAccessibilityString(); if (str == null) break;
+
+                if (type.IsInterface) { value = $"{str} abstract "; return true; }
+
+                var methodvirt = method.IsVirtual || method.IsAbstract || method.IsOverride;
+                value = methodvirt ? $"{str} abstract override " : $"{str} abstract new ";
+                return true;
+            }
+
+            // Member requested in the base type...
+            if (XNode.FindDecoratedMember<IPropertySymbol>(Symbol.Name, out var member, out var at, type))
+            {
+                if (type.IsInterface) { value = $"public abstract "; return true; }
+
+                var virt = at.HasUseVirtual(out var temp) && temp;
+                value = virt ? $"public abstract override " : $"public abstract new ";
+                return true;
+            }
+
+            // Try next...
+            value = default;
+            return false;
+        },
+        out string? value, null, host.AllInterfaces);
+
+        return found ? value : "public abstract ";
     }
 
     // ----------------------------------------------------
@@ -71,63 +212,133 @@ internal class XPropertyNode : PropertyNode
     /// </summary>
     void EmitHostRegular(SourceProductionContext context, CodeBuilder cb)
     {
-        throw null;
+        var host = ParentNode!.Symbol;
+        var ctor = host.FindCopyConstructor(strict: false);
+        if (ctor == null) { host.ReportError(TreeError.NoCopyConstructor, context); return; }
+
+        var type = XNode.FindReturnType<IPropertySymbol>(
+            Symbol.Name,
+            out var xtype, out var xnullable, out _, host, host.AllInterfaces) ? xtype : host;
+
+        var roptions = XNode.ReturnOptions(host, type);
+        var rtype = type.EasyName(roptions);
+        var rnull = xnullable ? "?" : string.Empty;
+        var modifiers = GetRegularModifiers();
+        var argtype = Symbol.Type.EasyName(EasyTypeSymbol.Full);
+
+        XNode.EmitDocumentation(Symbol, cb);
+        cb.AppendLine($"{modifiers}{rtype}{rnull}");
+        cb.AppendLine($"{MethodName}({argtype} {ArgumentName})");
+        cb.AppendLine("{");
+        cb.IndentLevel++;
+        {
+            var hostname = host.EasyName();
+            cb.AppendLine($"var v_host = new {hostname}(this)");
+            cb.AppendLine("{");
+            cb.IndentLevel++;
+            {
+                cb.AppendLine($"{Symbol.Name} = {ArgumentName}");
+            }
+            cb.IndentLevel--;
+            cb.AppendLine("};");
+            cb.AppendLine("return v_host;");
+        }
+        cb.IndentLevel--;
+        cb.AppendLine("}");
+
+        EmitExplicitInterfaces(context, cb);
     }
 
-    // ----------------------------------------------------
-
     /// <summary>
-    /// Tries to find a decorated member in the given type, if not null, and in the given type chains.
+    /// Invoked to obtain the appropriate method modifiers.
+    /// <br/> If not null, ends with a space separator.
     /// </summary>
-    public bool FindMember(
-        [NotNullWhen(true)] out IPropertySymbol? value,
-        [NotNullWhen(true)] out AttributeData? attr,
-        INamedTypeSymbol? type, IEnumerable<INamedTypeSymbol>[] chains)
+    string? GetRegularModifiers()
     {
+        var host = ParentNode!.Symbol;
+        var issealed = Symbol.IsSealed || host.IsSealed;
+        var hostvirt = XNode.FindUseVirtual<IPropertySymbol>(
+            Symbol.Name,
+            out var temp, out _, out _,
+            host, host.AllBaseTypes, host.AllInterfaces) || true; // Default value...
+
         var found = Finder.Find((type, out value) =>
         {
-            var member = type
-            .GetMembers().OfType<IPropertySymbol>().FirstOrDefault(x => x.Name == Symbol.Name);
-
-            if (member != null)
+            // Method in the base type...
+            while (XNode.FindMethod(MethodName, Symbol.Type, out var method, type))
             {
-                var ats = member.GetAttributes([typeof(WithAttribute), typeof(WithAttribute<>)]).ToArray();
-                if (ats.Length == 1)
+                var dec = method.DeclaredAccessibility; if (dec == Accessibility.Private) break;
+                var str = dec.ToAccessibilityString(); if (str == null) break;
+
+                if (type.IsInterface)
                 {
-                    value = new(member, ats[0]);
+                    value = issealed || !hostvirt ? $"{str} " : $"{str} virtual ";
                     return true;
                 }
+
+                var methodvirt = method.IsVirtual || method.IsAbstract || method.IsOverride;
+                value = issealed || !methodvirt ? $"{str} new " : $"{str} override ";
+                return true;
             }
 
-            value = null;
+            // Member requested in the base type...
+            if (XNode.FindDecoratedMember<IPropertySymbol>(Symbol.Name, out var member, out var at, type))
+            {
+                if (type.IsInterface)
+                {
+                    value = issealed || !hostvirt ? $"public " : $"public virtual ";
+                    return true;
+                }
+
+                var virt = at.HasUseVirtual(out var temp) && temp;
+                value = virt ? $"public override " : $"public new ";
+                return true;
+            }
+
+            // Try next...
+            value = default;
             return false;
         },
-        out FindMemberTemp? temp, type, chains);
+        out string? value, null, host.AllInterfaces);
 
-        value = found ? null : temp!.Element;
-        attr = found ? null : temp!.Attribute;
-        return found;
+        return found
+            ? value
+            : (issealed || !hostvirt) ? "public " : "public virtual ";
     }
-    record FindMemberTemp(IPropertySymbol? Element, AttributeData Attribute);
 
     // ----------------------------------------------------
 
-    /// <summary>
-    /// Tries to find a compatible in the given type, if not null, and in the given type chains.
-    /// </summary>
-    public bool FindMethod(
-        [NotNullWhen(true)] out IMethodSymbol? value,
-        INamedTypeSymbol? type, IEnumerable<INamedTypeSymbol>[] chains)
-    {
-        return Finder.Find((type, out value) =>
-        {
-            value = type.GetMembers().OfType<IMethodSymbol>().FirstOrDefault(x =>
-                x.Name == MethodName &&
-                x.Parameters.Length == 1 &&
-                Symbol.Type.IsAssignableTo(x.Parameters[0].Type));
+    record ExplicitInfo(
+        INamedTypeSymbol RType, bool RNullable,
+        INamedTypeSymbol IFace,
+        INamedTypeSymbol MType);
 
-            return value != null;
-        },
-        out value, type, chains);
+    /// <summary>
+    /// Invoked to emit the interfaces that need explicit implementation, if any.
+    /// </summary>
+    void EmitExplicitInterfaces(SourceProductionContext context, CodeBuilder cb)
+    {
+        var items = GetExplicitInterfaces();
+        foreach (var item in items)
+        {
+            var rtype = item.RType.EasyName(EasyTypeSymbol.Full);
+            if (item.RNullable && !rtype.EndsWith('?')) rtype += '?';
+            var iface = item.IFace.EasyName(EasyTypeSymbol.Full with { NullableStyle = IsNullableStyle.None });
+            var mtype = item.MType.EasyName(EasyTypeSymbol.Full);
+
+            cb.AppendLine();
+            cb.AppendLine($"{rtype}");
+            cb.AppendLine($"{iface}.{MethodName}({mtype} value)");
+            cb.AppendLine($"=> {MethodName}(value);");
+        }
+    }
+
+    /// <summary>
+    /// Obtains the list of interfaces that need explicit implementation.
+    /// </summary>
+    /// <returns></returns>
+    List<ExplicitInfo> GetExplicitInterfaces()
+    {
+        throw null;
     }
 }
