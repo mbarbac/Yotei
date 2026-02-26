f5dce5562fefaa1628ec3c4cb30ceba74dea2c37
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-02-26 15:06:00 +0100

    xxx

--- 1-Yotei.Tools.Generators/00-CoreGenerator/Yotei.Tools.CoreGenerator/Tree/Nodes/INode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Yotei.Tools.CoreGenerator;
-
-// ========================================================
-/// <summary>
-/// Represents a source code generation element, or an error condition that will be reported at
-/// code generation time.
-/// </summary>
-internal interface INode { }
