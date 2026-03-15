a427f49022fdbfeb6ce1926debe3c92a4f27dc33
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-03-15 17:52:14 +0100

    xxx

--- 1-Yotei.Tools/Yotei.Tools/SystemEx2/IsNullableStyle.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Yotei.Tools;
-
-// ========================================================
-/// <summary>
-/// Determines the style to use with nullability annotations.
-/// </summary>
-public enum IsNullableStyle { None, UseAnnotations, KeepWrappers }
