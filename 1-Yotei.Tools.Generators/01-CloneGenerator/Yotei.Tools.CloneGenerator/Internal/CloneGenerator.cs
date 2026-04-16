cbbc14b8779fc4a55927d1097ba552f1e37990d2
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-04-16 11:23:23 +0200

    xxx

--- 1-Yotei.Tools.Generators/01-CloneGenerator/Yotei.Tools.CloneGenerator/Internal/CloneGenerator.cs
+++ 1-Yotei.Tools.Generators/01-CloneGenerator/Yotei.Tools.CloneGenerator/Internal/CloneGenerator.cs
@@ -4,35 +4,7 @@
 /// <summary>
 /// <inheritdoc/>
 /// </summary>
-[Generator(LanguageNames.CSharp)]
+//[Generator(LanguageNames.CSharp)]
 public class CloneGenerator : TreeGenerator
 {
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    protected override bool EmitFilesInFolders => false;
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    /// <param name="context"></param>
-    protected override void OnInitialize(IncrementalGeneratorPostInitializationContext context)
-    {
-        string rname;
-        var rfolder = "Public/";
-        var nspace = GetType().Namespace;
-
-        rname = "CloneableAttribute.cs";
-        AddResourceContents(context, $"{rfolder}{rname}", nspace, rname, EmitFilesInFolders);
-
-        rname = "CloneableAttribute[T].cs";
-        AddResourceContents(context, $"{rfolder}{rname}", nspace, rname, EmitFilesInFolders);
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    protected override List<Type> TypeAttributes { get; } = [
-        typeof(CloneableAttribute),
-        typeof(CloneableAttribute<>),];
 }
