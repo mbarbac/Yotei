fe268c6e13f486eeb66f86ccd4f5ce89385f5956
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-04-24 11:05:05 +0200

    xxx

--- 1-Yotei.Tools/Yotei.Tools.Shared/System/IEnumerableExtensions.cs
+++ 1-Yotei.Tools/Yotei.Tools.Shared/System/IEnumerableExtensions.cs
@@ -3,31 +3,40 @@
 // ========================================================
 public static class IEnumerableExtensions
 {
-    /// <summary>
-    /// Executes the given action for each element in the given enumeration.
-    /// </summary>
-    /// <param name="action"></param>
-    public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+    extension<T>(IEnumerable<T> source)
     {
-        ArgumentNullException.ThrowIfNull(items);
-        ArgumentNullException.ThrowIfNull(action);
+        /// <summary>
+        /// Determines if the enumeration is an empty one.
+        /// <br/> Note that this property may have side effects as it tries to obtain the first
+        /// element in the enumeration.
+        /// </summary>
+        public bool IsEmpty => !source.ThrowWhenNull().Any();
 
-        foreach (var item in items) action(item);
-    }
+        /// <summary>
+        /// Executes the given action for each element in the enumeration.
+        /// </summary>
+        /// <param name="action"></param>
+        public void ForEach(Action<T> action)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(action);
 
-    /// <summary>
-    /// Executes the given action for each element in the given enumeration that matches the
-    /// given predicate.
-    /// </summary>
-    /// <param name="items"></param>
-    /// <param name="action"></param>
-    public static void ForEach<T>(
-        this IEnumerable<T> items, Predicate<T> predicate, Action<T> action)
-    {
-        ArgumentNullException.ThrowIfNull(items);
-        ArgumentNullException.ThrowIfNull(predicate);
-        ArgumentNullException.ThrowIfNull(action);
+            foreach (var item in source) action(item);
+        }
 
-        foreach (var item in items) if (predicate(item)) action(item);
+        /// <summary>
+        /// Executes the given action for each element in the enumeration that meets the given
+        /// predicate.
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="action"></param>
+        public void ForEach(Predicate<T> predicate, Action<T> action)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            ArgumentNullException.ThrowIfNull(action);
+
+            foreach (var item in source) if (predicate(item)) action(item);
+        }
     }
 }
