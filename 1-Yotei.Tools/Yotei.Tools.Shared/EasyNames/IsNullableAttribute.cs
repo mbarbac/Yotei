698cbd07ab991cefcd2b5ee507fb379ea6a90e4a
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-04-28 17:14:54 +0200

    xxx

--- 1-Yotei.Tools/Yotei.Tools.Shared/EasyNames/IsNullableAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yotei.Tools;
-
-// ========================================================
-/// <summary>
-/// <inheritdoc cref="IsNullable{T}"/>
-/// </summary>
-[AttributeUsage(AttributeTargets.All)]
-public class IsNullableAttribute : Attribute
-{ }
