8871a245e2e374953de70ac9ca278169e9f71fd8
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-09-04 21:40:42 +0200

    xxx

---
+++ 0-Experimental/Experimental.Tests/Test_ExtendedProperties/Test_ExtendedProperty.cs
@@ -0,0 +1,29 @@
+﻿namespace Experimental.ExtendedProperties.Tests;
+
+// ========================================================
+//[Enforced]
+public static class Test_ExtendedProperty
+{
+    //[Enforced]
+    [Fact]
+    public static void Test_Extended_Property()
+    {        
+        Assert.Equal(0, PersonExtensions.Size);
+        try
+        {
+            var person = new Person("James"); Assert.Null(person.Nickname);
+            person.Nickname = "007"; Assert.Equal("007", person.Nickname);
+
+            person = new Person("Paul"); Assert.Null(person.Nickname);
+            person.Nickname = "008"; Assert.Equal("008", person.Nickname);
+
+            Assert.Equal(2, PersonExtensions.Size);
+
+            person = null;
+            GC.Collect(); GC.WaitForPendingFinalizers();
+            GC.Collect(); GC.WaitForPendingFinalizers();
+        }
+        finally { }
+        Assert.Equal(2, PersonExtensions.Size);
+    }
+}
---
+++ 0-Experimental/Experimental/ExtendedProperties/ExtendedProperties.cs
@@ -0,0 +1,21 @@
+﻿namespace Experimental.ExtendedProperties;
+
+// ========================================================
+public sealed record Person(string Name);
+
+// ========================================================
+public static class PersonExtensions
+{
+    static readonly ConditionalWeakTable<Person, StrongBox<string?>> _Nicknames = [];
+    public static int Size => _Nicknames.Count();
+    public static void Clear() => _Nicknames.Clear();
+
+    extension(Person person)
+    {
+        public string? Nickname
+        {
+            get => _Nicknames.TryGetValue(person, out var box) ? box.Value : null;
+            set => _Nicknames.AddOrUpdate(person, new(value));
+        }
+    }
+}
