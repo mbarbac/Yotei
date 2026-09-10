7fa37365ebb4b33a21f66d6c1202ab02c6d563db
Moises Barba Cebeira <mbarbac@hotmail.com>
2026-09-10 19:49:29 +0200

    xxx

---
+++ 2-Yotei.ORM/Yotei.ORM/1-Core/Api/IValueConverter.cs
@@ -0,0 +1,48 @@
+﻿namespace Yotei.ORM;
+
+// ========================================================
+/// <summary>
+/// Represents the ability of converting instances from a source type to a target one.
+/// <br/> Converters are typically used when there is no standard conversion from application
+/// level types to database ones, and viceversa.
+/// <br/> Instances of this type are intended to be immutable ones.
+/// </summary>
+public interface IValueConverter
+{
+    /// <summary>
+    /// The source type to convert values from.
+    /// </summary>
+    Type SourceType { get; }
+
+    /// <summary>
+    /// The target type to convert values to.
+    /// </summary>
+    Type TargetType { get; }
+
+    /// <summary>
+    /// Tries to convert the given value from the source type to the target one, using the given
+    /// locale if provided and needed.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="locale"></param>
+    /// <returns></returns>
+    object? Convert(object? value, Locale? locale = null);
+}
+
+// ========================================================
+/// <summary>
+/// <inheritdoc cref="IValueConverter"/>
+/// </summary>
+/// <typeparam name="TSource"></typeparam>
+/// <typeparam name="TTarget"></typeparam>
+public interface IValueConverter<TSource, TTarget> : IValueConverter
+{
+    /// <summary>
+    /// <inheritdoc cref="IValueConverter.Convert(object?, Locale?)"/>
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="locale"></param>
+    /// <returns></returns>
+    [return: MaybeNull]
+    TTarget Convert([AllowNull] TSource value, Locale? locale = null);
+}
---
+++ 2-Yotei.ORM/Yotei.ORM/1-Core/Api/IValueConverterList.cs
@@ -0,0 +1,70 @@
+﻿namespace Yotei.ORM;
+
+// ========================================================
+/// <summary>
+/// Representes a collection of value converters that tries to convert a given value using any
+/// of the converters registered into it.
+/// </summary>
+[Cloneable]
+public partial interface IValueConverterList : IEnumerable<IValueConverter>
+{
+    /// <summary>
+    /// Gets the number of converters registered into this instance.
+    /// </summary>
+    int Count { get; }
+
+    /// <summary>
+    /// Enumerates the source types of the converters registered into this instance.
+    /// </summary>
+    IEnumerable<Type> SourceTypes { get; }
+
+    /// <summary>
+    /// Tries to find a converter registered into this instance for the given source type, or
+    /// returns null if such cannot be found.
+    /// <br/> If no converter can be found in the default strict mode, and if <paramref name="relax"/>
+    /// is requested, then the first converter from whose source type the given one implements or
+    /// inherits from is then returned.
+    /// </summary>
+    /// <param name="sourceType"></param>
+    /// <param name="relax"></param>
+    /// <returns></returns>
+    IValueConverter? Find(Type sourceType, bool relax = false);
+
+    /// <summary>
+    /// <inheritdoc cref="Find(Type, bool)"/>
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <param name="relax"></param>
+    /// <returns></returns>
+    IValueConverter? Find<TSource>(bool relax = false);
+
+    // ----------------------------------------------------
+
+    /// <summary>
+    /// Adds to this collection the given converter. If its source type is already registered
+    /// into this collection, an exception is thrown.
+    /// </summary>
+    /// <param name="converter"></param>
+    void Add(IValueConverter converter);
+
+    /// <summary>
+    /// Adds to this collection the given converter. If its source type is already registered
+    /// into this collection, then the existing converter is replaced by the newly given one.
+    /// </summary>
+    /// <param name="converter"></param>
+    void AddOrReplace(IValueConverter converter);
+
+    /// <summary>
+    /// Removes from this collection the converter registered for the given source type, if any.
+    /// </summary>
+    /// <param name="sourceType"></param>
+    /// <returns></returns>
+    bool Remove(Type sourceType);
+
+    /// <summary>
+    /// Clears this instance.
+    /// </summary>
+    void Clear();
+
+    // ----------------------------------------------------
+}
